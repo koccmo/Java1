d50f7bbb586dbfa36da00af11e42b655e28b64b2
javaguruPresenter1 <javaguru.presenter@inbox.lv>
2020-05-26 07:38:57 +0300

    Student folder example

---
+++ src/student_firstname_secondname/lesson_1/homework/day_1/HelloWorld.java
@@ -0,0 +1,4 @@
+package student_firstname_secondname.lesson_1.homework.day_1;
+
+public class HelloWorld {
+}
---
+++ src/student_firstname_secondname/lesson_1/homework/day_2/HelloWorld.java
@@ -0,0 +1,4 @@
+package student_firstname_secondname.lesson_1.homework.day_2;
+
+public class HelloWorld {
+}
