bed2bca7fbafd1790fa70523bd7c51c5a181aecd
pavels-s <pavels.sarkels@gmail.com>
2020-05-27 22:27:12 +0300

    Lesson_1

---
+++ src/student_pavel_sharkel/lesson_1/Hello_name.java
@@ -0,0 +1,9 @@
+package student_pavel_sharkel.lesson_1;
+
+public class Hello_name {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Pavel Sharkel");
+    }
+}
