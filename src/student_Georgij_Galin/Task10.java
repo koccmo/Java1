a1a99167f93c40c60910a3087cfda4b02fcfee7e
Georgij Galin <65977707+GeorgijGalin21@users.noreply.github.com>
2020-05-27 22:33:44 +0300

    Hello Georgij Galin

---
+++ src/student_Georgij_Galin/Task10.java
@@ -0,0 +1,17 @@
+package student_Georgij_Galin;
+
+public class Task10 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello!");
+        System.out.println("Georgij Galin");
+    }
+
+}
+
+
+
+
+
+
--- src/student_igors_bartkevics/lesson_1/homeworks/Task_10.java
+++ src/student_igors_bartkevics/lesson_1/homeworks/Task_10.java
@@ -2,10 +2,9 @@ package student_igors_bartkevics.lesson_1.homeworks;
 
 class Task_10 {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
         System.out.println("Hello!");
         System.out.println("Igors Bartkevics");
     }
-
 }
