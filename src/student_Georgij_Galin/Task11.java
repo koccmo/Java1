f03ce52b6578b309a2a926e29ae47fc0ba9db311
Georgij Galin <65977707+GeorgijGalin21@users.noreply.github.com>
2020-05-27 23:12:17 +0300

    Task 11

---
+++ src/student_Georgij_Galin/Task11.java
@@ -0,0 +1,11 @@
+package student_Georgij_Galin;
+
+public class Task11 {
+
+    public static void main(String[] args) {
+
+        System.out.println(74 + 36);
+        System.out.println(50 / 3);
+    }
+
+}
