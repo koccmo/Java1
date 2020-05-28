b6723600de02520dc797ab7b469dc0339f7f805b
AlexaMaksimovic <lexaferret@inbox.lv>
2020-05-28 16:26:34 +0300

    - lesson 1, task 9 - lesson 1, task 10 - lesson 1, task 11

---
+++ src/student_aleksandra_maksimovic/homework/day_4/Task_10.java
@@ -0,0 +1,9 @@
+package student_aleksandra_maksimovic.homework.day_4;
+
+public class Task_10 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Aleksandra Maksimovic");
+    }
+}
+
---
+++ src/student_aleksandra_maksimovic/homework/day_4/Task_9.java
@@ -0,0 +1,7 @@
+package student_aleksandra_maksimovic.homework.day_4;
+
+public class Task_9 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
