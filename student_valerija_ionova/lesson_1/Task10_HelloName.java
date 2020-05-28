9103e40afaebce2258bca65f5a0ace0296d4c476
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-05-28 20:18:22 +0300

    Delete Task10_HelloName.java
    muchenie

--- student_valerija_ionova/lesson_1/Task10_HelloName.java
+++
@@ -1,8 +0,0 @@
-package student_valerija_ionova.lesson_1;
-
-public class Task10_HelloName {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Valerija");
-    }
-}
