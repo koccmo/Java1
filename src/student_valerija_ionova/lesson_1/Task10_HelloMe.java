73a6cd8118414c713bf4db3c5833215a4b01fd5b
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-05-28 20:54:35 +0300

    Muchenie

---
+++ src/student_valerija_ionova/lesson_1/Task10_HelloMe.java
@@ -0,0 +1,9 @@
+package student_valerija_ionova.lesson_1;
+
+public class Task10_HelloMe {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("Valerija!");
+    }
+}
---
+++ src/student_valerija_ionova/lesson_1/Task11_delenie.java
@@ -0,0 +1,7 @@
+package student_valerija_ionova.lesson_1;
+
+public class Task11_delenie {
+    public static void main(String[] args) {
+        System.out.println(50/3);
+    }
+}
