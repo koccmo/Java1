6ae69cb86078d803f9c5a77e55bf100474a65041
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-05-28 19:17:44 +0300

    Muchenie

---
+++ student_valerija_ionova/lesson_1/Task10_HelloName.java
@@ -0,0 +1,8 @@
+package student_valerija_ionova.lesson_1;
+
+public class Task10_HelloName {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Valerija");
+    }
+}
---
+++ student_valerija_ionova/lesson_1/Task9_HelloWorld.java
@@ -0,0 +1,7 @@
+package student_valerija_ionova.lesson_1;
+
+public class Task9_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
