454b82283173148d7ff01c40dc9fcb135d5067eb
Vladimir Kobenko <vladimirs.kobenko@gmail.com>
2020-05-29 14:59:35 +0300

    Lesson1_task9

---
+++ src/student_vladimir_kobenko/homeworks/Lesson1/Task11_Sum.java
@@ -0,0 +1,8 @@
+package student_vladimir_kobenko.homeworks.Lesson1;
+
+public class Task11_Sum {
+    public static void main (String [] args){
+        System.out.println("74+36=" + (74+36));
+        System.out.println("50/3 = " + 50/3);
+    }
+}
