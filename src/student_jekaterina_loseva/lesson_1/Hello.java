a03ef83a832c8ec68cd63caff976537928db5f39
Teryja <missteryja@gmail.com>
2020-05-30 22:35:23 +0300

    initial commit

---
+++ src/student_jekaterina_loseva/lesson_1/Hello.java
@@ -0,0 +1,10 @@
+package student_jekaterina_loseva.lesson_1;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Jekaterina Loseva");
+    }
+
+}
