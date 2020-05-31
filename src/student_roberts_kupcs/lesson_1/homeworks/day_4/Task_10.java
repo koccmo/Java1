ea94a53439bc395b418df9c9037dd2bd9a2fdfad
robku <rkarrow2@gmail.com>
2020-05-31 13:54:18 -0700

    added homwork

---
+++ src/student_roberts_kupcs/lesson_1/homeworks/day_4/Task_10.java
@@ -0,0 +1,8 @@
+package student_roberts_kupcs.lesson_1.homeworks.day_4;
+
+public class Task_10 {
+    public static void main (String[] args) {
+        System.out.println("Hello");
+        System.out.println("Roberts Kupcs");
+    }
+}
