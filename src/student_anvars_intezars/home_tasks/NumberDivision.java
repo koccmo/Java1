e02caee020665f9a605e46f3994481834301d265
Anvar331 <anvar.intezar@gmail.com>
2020-05-31 10:03:33 +0300

    lesson1/task11Part2

---
+++ src/student_anvars_intezars/home_tasks/NumberDivision.java
@@ -0,0 +1,6 @@
+package student_anvars_intezars.home_tasks;
+
+public class NumberDivision {
+    public static void main(String[] args) { System.out.println("The division of 50 by 3 will be\n"
+            +(50/3));}
+}
