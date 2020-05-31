b214c3abedbfa0022c0b0010f779999bd1c11f79
Anvar331 <anvar.intezar@gmail.com>
2020-05-31 09:54:04 +0300

    lesson1/task11

---
+++ src/student_anvars_intezars/home_tasks/Lesson1Task11.java
@@ -0,0 +1,8 @@
+package student_anvars_intezars.home_tasks;
+
+public class Lesson1Task11 {
+
+    public static void main(String[] args) { System.out.println("The sum of two numbers 74 and 36 will be\n"
+            +(74+36));}
+
+}
