43ab7628bfb4d510a5e6e679fb2b935d18a595e6
S4n6r4 <s4ndr41f@gmail.com>
2020-06-02 19:53:46 +0300

    Lesson_1 tasks: HelloWorld and HelloToMyself

---
+++ src/student_sandra_arniece/lesson_1/HelloToMyself.java
@@ -0,0 +1,8 @@
+package student_sandra_arniece.lesson_1;
+
+public class HelloToMyself {
+
+    public static void main(String[] args) {
+        System.out.println("Hello\nSandra Arniece");
+    }
+}
