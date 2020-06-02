b0bc0d667268469d10579c5120910044ee838247
Anvar331 <anvar.intezar@gmail.com>
2020-06-02 23:00:38 +0300

    New Package Update

--- src/student_anvars_intezars/home_tasks/lesson_1/Task11.java
+++ src/student_anvars_intezars/home_tasks/lesson_1/Task11.java
@@ -1,6 +1,6 @@
 package student_anvars_intezars.home_tasks.lesson_1;
 
-public class Lesson1Task11 {
+public class Task11 {
 
     public static void main(String[] args) {
         System.out.println("The sum of two numbers 74 and 36 will be\n"
--- src/student_anvars_intezars/home_tasks/lesson_1/Task11Pt2.java
+++ src/student_anvars_intezars/home_tasks/lesson_1/Task11Pt2.java
@@ -1,6 +1,6 @@
 package student_anvars_intezars.home_tasks.lesson_1;
 
-public class NumberDivision {
+public class Task11Pt2 {
     public static void main(String[] args) {
         System.out.println("The result of division of 50 by 3 will be\n"
             +(50/3));}
