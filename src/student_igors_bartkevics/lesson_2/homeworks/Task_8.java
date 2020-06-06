3cedf4e5253bb0504d4a88141a712669277bdf38
Igors <igors.barkevics@gmail.com>
2020-06-06 22:46:43 +0300

    Commit lesson 2

---
+++ src/student_igors_bartkevics/lesson_2/homeworks/Task_8.java
@@ -0,0 +1,14 @@
+package student_igors_bartkevics.lesson_2.homeworks;
+
+import java.util.Scanner;
+
+public class Task_8 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String userName = scanner.nextLine();
+        System.out.println("Hello " + userName);
+    }
+}
