38b1bd4a967b0fe0bcc93a6764be4ea1ad182cfc
Igors <igors.barkevics@gmail.com>
2020-06-08 22:54:50 +0300

    Commit lesson 2

---
+++ src/student_igors_bartkevics/lesson_2/homeworks/Task_24.java
@@ -0,0 +1,10 @@
+package student_igors_bartkevics.lesson_2.homeworks;
+
+public class Task_24 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+
+    }
+}
---
+++ src/student_igors_bartkevics/lesson_2/homeworks/Task_25.java
@@ -0,0 +1,8 @@
+package student_igors_bartkevics.lesson_2.homeworks;
+
+public class Task_25 {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
