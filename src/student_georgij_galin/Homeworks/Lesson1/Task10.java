b2d744ba09c722921c687cb1c7521408cc0d0280
Georgij Galin <65977707+GeorgijGalin21@users.noreply.github.com>
2020-06-10 22:42:53 +0300

    Homework Lesson1 Task10

---
+++ src/student_georgij_galin/Homeworks/Lesson1/Task10.java
@@ -0,0 +1,21 @@
+package student_georgij_galin.Homeworks.Lesson1;
+
+public class Task10 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello!");
+        System.out.println("Georgij Galin");
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
