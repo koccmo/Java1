da1496c869d820200f1a47a55e0204d1cf3c68b7
Vladimir Kobenko <vladimirs.kobenko@gmail.com>
2020-06-10 10:37:37 +0300

    Lesson 2_Task 3

---
+++ src/student_vladimir_kobenko/homeworks/lesson2/homeworks/day_2/lesson2_Task3.java
@@ -0,0 +1,9 @@
+package student_vladimir_kobenko.homeworks.lesson2.homeworks.day_2;
+
+public class lesson2_Task3 {
+    public static void main(String[] args) {
+        class book {
+
+        }
+    }
+}
