6e35680fd1f299f51783881e36b812270450d75d
Vladimir Kobenko <vladimirs.kobenko@gmail.com>
2020-06-10 10:39:22 +0300

    Lesson 2_Task 5

---
+++ src/student_vladimir_kobenko/homeworks/lesson2/homeworks/day_2/lesson2_Task5.java
@@ -0,0 +1,10 @@
+package student_vladimir_kobenko.homeworks.lesson2.homeworks.day_2;
+
+public class lesson2_Task5 {
+    static class BankAccount {
+
+        public static void main(String[] args) {
+        }
+
+    }
+}
