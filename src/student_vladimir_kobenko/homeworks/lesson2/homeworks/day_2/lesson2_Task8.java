cd57ada00fba5d0cf823a0e9a12d1ca459fd0a00
Vladimir Kobenko <vladimirs.kobenko@gmail.com>
2020-06-10 11:01:12 +0300

    Lesson 2_Task 8

---
+++ src/student_vladimir_kobenko/homeworks/lesson2/homeworks/day_2/lesson2_Task8.java
@@ -0,0 +1,17 @@
+package student_vladimir_kobenko.homeworks.lesson2.homeworks.day_2;
+
+import java.util.Scanner;
+
+public class lesson2_Task8 {
+
+    public static <string> void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите имя пользователя:");
+
+        String userName = scanner.nextLine();
+
+        System.out.println("Hello " + userName);
+    }
+}
