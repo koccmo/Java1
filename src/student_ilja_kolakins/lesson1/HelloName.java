b00614ca43369ff0701eceef408ec737f608bbd8
IlyaK <irlandec17@gmail.com>
2020-06-10 19:29:27 +0300

    Added lesson1 task classes

---
+++ src/student_ilja_kolakins/lesson1/HelloName.java
@@ -0,0 +1,9 @@
+package student_ilja_kolakins.lesson1;
+
+public class HelloName {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is:\n"
+        + "Ilya Kolakin");
+
+    }
+}
