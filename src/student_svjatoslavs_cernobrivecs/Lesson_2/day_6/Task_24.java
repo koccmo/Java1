b01051d2d39addef5ef9b5cbdf8eae1e739edd3a
scdk-web <scdk101@gmail.com>
2020-06-10 21:14:29 +0300

    Lesson_2.Task_24

---
+++ src/student_svjatoslavs_cernobrivecs/Lesson_2/day_6/Task_24.java
@@ -0,0 +1,8 @@
+package student_svjatoslavs_cernobrivecs.Lesson_2.day_6;
+
+public class Task_24 {
+
+    public static void main(String[] args){
+        System.out.println("Code outside the method will not compile!");
+    }
+}
