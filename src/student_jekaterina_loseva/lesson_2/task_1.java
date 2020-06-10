886c4f83ede1166b2685677f6976e275599e562d
losev <missteryja@gmail.com>
2020-06-10 20:55:59 +0300

    lesson_2, task_1

---
+++ src/student_jekaterina_loseva/lesson_2/task_1.java
@@ -0,0 +1,27 @@
+package student_jekaterina_loseva.lesson_2;
+
+import java.util.Scanner;
+
+public class task_1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число:");
+        int numberOne = scanner.nextInt();
+        System.out.println("Введите второе число:");
+        int numberTwo = scanner.nextInt();
+
+        int summary = numberOne + numberTwo;
+        int subtraction = numberOne - numberTwo;
+        int multiply = numberOne * numberTwo;
+        int division = numberOne / numberTwo;
+
+        System.out.println("Рузультат сложения: " + summary);
+        System.out.println("Рузультат вычитания: " + subtraction);
+        System.out.println("Рузультат умножения: " + multiply);
+        System.out.println("Рузультат деления: " + division);
+    }
+
+}
+
