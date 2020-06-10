1267b11d1e4226603b5eea5b74ad36e7401d1124
Vladimir Kobenko <vladimirs.kobenko@gmail.com>
2020-06-10 10:45:50 +0300

    Lesson 2_Task 7

---
+++ src/student_vladimir_kobenko/homeworks/lesson2/homeworks/day_2/lesson2_Task7.java
@@ -0,0 +1,12 @@
+package student_vladimir_kobenko.homeworks.lesson2.homeworks.day_2;
+
+public class lesson2_Task7 {
+    static class LoginService {
+
+        public static void main(String[] args) {
+            System.out.println("You can login!");
+        }
+
+
+    }
+}
