c0e66525cd5e4919004b86a25828aa67c55626db
Vladimir Kobenko <vladimirs.kobenko@gmail.com>
2020-06-10 10:38:39 +0300

    Lesson 2_Task 4

---
+++ src/student_vladimir_kobenko/homeworks/lesson2/homeworks/day_2/lesson2_Task4.java
@@ -0,0 +1,10 @@
+package student_vladimir_kobenko.homeworks.lesson2.homeworks.day_2;
+
+public class lesson2_Task4 {
+    public static void main(String[] args) {
+
+        class Phone {
+
+        }
+    }
+}
