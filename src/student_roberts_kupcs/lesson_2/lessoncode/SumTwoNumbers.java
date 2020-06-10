9b677d5c6b1d3e42417436de6269551deeac0b90
robku <rkarrow2@gmail.com>
2020-06-10 12:37:04 -0700

    create lesson_2 AddSubtrDivisMultiplicTwoNumbers

---
+++ src/student_roberts_kupcs/lesson_2/lessoncode/SumTwoNumbers.java
@@ -0,0 +1,9 @@
+package student_roberts_kupcs.lesson_2.lessoncode;
+
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+        int numberOne = 10;
+
+        int numberTwo = 20;
+    }
+}
