9630f8db1acc5e46a05d2228bcbeb554df0fe551
Vladimir Kobenko <vladimirs.kobenko@gmail.com>
2020-06-10 18:14:19 +0300

    lesson2_Task16

---
+++ src/student_vladimir_kobenko/lesson2/homeworks/day_5/lesson2_Task16.java
@@ -0,0 +1,10 @@
+package student_vladimir_kobenko.lesson2.homeworks.day_5;
+
+public class lesson2_Task16 {
+
+    public static void main(String[] args) {
+
+    System.out.print("Hello World! ");
+    System.out.print("God bless you");
+}
+}
