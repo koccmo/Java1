8b476cf4f114a2298845f3d848d54369d538728f
scdk-web <scdk101@gmail.com>
2020-06-10 21:19:38 +0300

    Lesson_2.Task_25

--- src/student_svjatoslavs_cernobrivecs/Lesson_2/day_6/AttentionPlease.java
+++ src/student_svjatoslavs_cernobrivecs/Lesson_2/day_6/AttentionPlease.java
@@ -1,6 +1,6 @@
 package student_svjatoslavs_cernobrivecs.Lesson_2.day_6;
 
-public class Task_24 {
+public class AttentionPlease {
 
     public static void main(String[] args){
         System.out.println("Code outside the method will not compile!");
---
+++ src/student_svjatoslavs_cernobrivecs/Lesson_2/day_6/NoSleepNoFocus.java
@@ -0,0 +1,9 @@
+package student_svjatoslavs_cernobrivecs.Lesson_2.day_6;
+
+public class NoSleepNoFocus {
+
+    public static void main(String[] args){
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
