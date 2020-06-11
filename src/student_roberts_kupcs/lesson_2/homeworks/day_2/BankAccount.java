181ef6f579fe0e42824d51b7d32a77f2b51307a6
robku <rkarrow2@gmail.com>
2020-06-11 12:56:46 -0700

    lesson_2 day_2 BankAccount.java fix compilation error

---
+++ src/student_roberts_kupcs/lesson_2/homeworks/day_2/BankAccount.java
@@ -0,0 +1,7 @@
+package student_roberts_kupcs.lesson_2.homeworks.day_2;
+
+public class BankAccount {
+    public static void main(String[] args) {
+
+    }                                           // + }
+}
