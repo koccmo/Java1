c6bda1df5c6c2b718c8c8b79bf0378e7262905fe
georgij galin <galin.georgij95@gmail.com>
2020-06-11 04:03:14 +0300

    Lesson1 Homework

---
+++ src/student_georgij_galin/Lesson1/Task10.java
@@ -0,0 +1,15 @@
+package student_georgij_galin.Lesson1;
+
+class Task_10 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello!");
+        System.out.println("Georgij Galin");
+    }
+
+
+
+
+
+}
---
+++ src/student_georgij_galin/Lesson1/Task9.java
@@ -0,0 +1,10 @@
+package student_georgij_galin.Lesson1;
+
+class Task_9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+
+}
+
