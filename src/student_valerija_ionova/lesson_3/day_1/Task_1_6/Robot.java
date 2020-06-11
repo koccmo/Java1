44ceec79fc69505919495354ee1bfed5145fde24
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-06-11 23:14:53 +0300

    Homeworks Lesson 3 Task 1-9

---
+++ src/student_valerija_ionova/lesson_3/day_1/Task_1_6/Robot.java
@@ -0,0 +1,34 @@
+package student_valerija_ionova.lesson_3.day_1.Task_1_6;
+
+/*Создайте класс робот (Robot).
+
+Научите вашего робота "говорить" привет.
+
+Создайте в классе Robot метод со следующей сигнатурой:
+
+    public void sayHello() {
+        // тут напишите команду распечатки на консоли строки "Hello!"
+
+        Task2
+        В классе робот (Robot) создайте конструктор без параметров:
+        public Robot() {  }
+        Task5
+        Создайте в классе Robot метод со следующей сигнатурой:
+        public void sayYourName() {
+        // тут напишите команду распечатки на консоли строки "My name is ROBO"
+    }
+    }*/
+
+public class Robot {
+
+    public Robot() {
+
+    }
+
+        public void sayHello() {
+            System.out.println("Hello!");
+        }
+        public void sayYourName(){
+        System.out.println("My name is ROBO");
+        }
+}
---
+++ src/student_valerija_ionova/lesson_3/day_2/Task_7.java
@@ -0,0 +1,17 @@
+package student_valerija_ionova.lesson_3.day_2;
+
+/*Учимся работать со строками.
+
+Тема: создание переменной типа String.
+
+Напишите программу в которой создайте переменную
+типа String и присвойте ей в качестве начального значения
+ваше имя. Выведете на консоль значение этой переменной.
+    */
+
+public class Task_7 {
+    public static void main(String[] args) {
+        String myName = "Valerija";
+        System.out.println(myName);
+    }
+}
