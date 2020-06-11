b414a2d02f2fe58d20edbfcd03708314db8c6982
georgij galin <galin.georgij95@gmail.com>
2020-06-11 11:11:57 +0300

    Task8 Lesson 2

--- src/student_georgij_galin/Lesson2/Task8.java
+++ src/student_georgij_galin/Lesson2/Task8.java
@@ -13,7 +13,7 @@ public class Task8 {
 
         // 1. get name from user
         Scanner myInput = new Scanner( System.in );
-        System.out.println("Enter first number: ");
+        System.out.println("Enter your name: ");
         String Name = myInput.next();
 
         // 2. print result to console
