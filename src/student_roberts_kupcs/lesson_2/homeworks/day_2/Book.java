d4c6ae77ed4a1d12f7dfce5d97e16d2b26919ac3
robku <rkarrow2@gmail.com>
2020-06-11 12:39:02 -0700

    lesson_2 Book.java fix compilation error

---
+++ src/student_roberts_kupcs/lesson_2/homeworks/day_2/Book.java
@@ -0,0 +1,11 @@
+package student_roberts_kupcs.lesson_2.homeworks.day_2;
+
+class Book {
+
+    public static void main(String[] args) {
+
+    }
+}
+// + public static void main(String[] args) {
+// + }
+// + }
