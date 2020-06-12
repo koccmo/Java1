a07be5d9175fc4cb46a3384351681caaebf7bc38
BRUTN <brutan.jaroslav@gmail.com>
2020-06-13 00:07:21 +0300

    Day x task 4,5

---
+++ src/student_jaroslav_brutan/lesson_2/day_X/Super_task_4.java
@@ -0,0 +1,7 @@
+package student_jaroslav_brutan.lesson_2.day_X;
+
+public class Super_task_4 {
+    public static void main(String []args){
+        System.out.print("Java - типизированный, статический, строгий язык");
+    }
+}
