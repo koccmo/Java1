60f462a77ca89571394b817d36c6379c425b1234
scdk-web <s.chernobrivec@gmail.com>
2020-06-13 15:16:55 +0300

    Lesson3.HomeWork

--- src/student_svjatoslavs_cernobrivecs/lesson_3/day_2/Task_7.java
+++ src/student_svjatoslavs_cernobrivecs/lesson_3/day_2/Task_7.java
@@ -3,7 +3,6 @@ package student_svjatoslavs_cernobrivecs.lesson_3.day_2;
 public class Task_7 {
 
     public static void main(String[] args){
-
         String name = "Batman!";
         System.out.println(name);
     }
