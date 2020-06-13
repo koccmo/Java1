19fca4945b81b8a1921c0e5a86dc2a425eea3d32
scdk-web <s.chernobrivec@gmail.com>
2020-06-13 14:04:03 +0300

    Lesson3.HomeWork

---
+++ src/student_svjatoslavs_cernobrivecs/lesson_3/day_1/Robot.java
@@ -0,0 +1,13 @@
+package student_svjatoslavs_cernobrivecs.lesson_3.day_1;
+
+public class Robot {
+
+    public void sayHello(){
+        System.out.println("Hello");}
+
+        public void sayYourName(){
+        System.out.println("My name is ROBO");
+        }
+    }
+
+
---
+++ src/student_svjatoslavs_cernobrivecs/lesson_3/day_1/RobotDemo.java
@@ -0,0 +1,15 @@
+package student_svjatoslavs_cernobrivecs.lesson_3.day_1;
+
+public class RobotDemo {
+
+    public static void main(String[] args){
+
+        Robot robotHelloNumberOne = new Robot();
+        robotHelloNumberOne.sayHello();
+        robotHelloNumberOne.sayYourName();
+
+        Robot robotHelloNumberTwo = new Robot();
+        robotHelloNumberTwo.sayHello();
+        robotHelloNumberTwo.sayYourName();
+    }
+}
