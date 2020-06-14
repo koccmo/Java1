e44274b9261d6d659bb8e14ce04a6b841e6f5074
Dmitryyyyo <dmitrijs.ja@gmail.com>
2020-06-15 02:16:03 +0300

    Lesson3_homework's

---
+++ src/student_dmitrijs_jasvins/lesson_3/day_6/task_27/BankAccountDemo.java
@@ -0,0 +1,12 @@
+package student_dmitrijs_jasvins.lesson_3.day_6.task_27;
+
+public class BankAccountDemo {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount("Dmitry", 150);
+        String ownerName = bankAccount.getOwner();
+        int ownerMoney = bankAccount.getMoney();
+        System.out.println("Bank information: ");
+        System.out.println("Owner name : " + ownerName);
+        System.out.println("Owner money amount : " + ownerMoney);
+    }
+}
