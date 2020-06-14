e5ed4d268b7a49b090f5fca83fe5f9d80f519122
Igors <igors.barkevics@gmail.com>
2020-06-14 22:42:12 +0300

    Lesson 3 day 6

---
+++ src/student_igors_bartkevics/lesson_3/homeworks/day_6/Task_27.java
@@ -0,0 +1,41 @@
+/*
+Найти логическую ошибку в коде.
+Исправить и запустить программу так, что бы она выводила
+на консоль владельца банковского счёта и количество денег.
+Менять можно только класс BankAccount.
+ */
+
+
+package student_igors_bartkevics.lesson_3.homeworks.day_6;
+
+class BankAccount {
+
+    private String owner;
+    private int moneyAmount;
+
+    public BankAccount(String owner, int moneyAmount) {
+        this.owner = owner;
+        this.moneyAmount = moneyAmount;
+    }
+
+    public String getOwner() {
+        return this.owner;
+    }
+
+    public int getMoney() {
+        return this.moneyAmount;
+    }
+
+}
+
+class BankAccountDemo {
+
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount("Viktor", 100000);
+        String ownerFirstName = bankAccount.getOwner();
+        int moneyAmount = bankAccount.getMoney();
+        System.out.println("Owner = " + ownerFirstName);
+        System.out.println("Money = " + moneyAmount);
+    }
+
+}
