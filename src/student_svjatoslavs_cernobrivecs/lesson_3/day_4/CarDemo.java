3d26c8a9649402550e8ad30526bcdc4c52574865
scdk-web <s.chernobrivec@gmail.com>
2020-06-14 17:50:05 +0300

    Lesson3.HomeWork

--- src/student_svjatoslavs_cernobrivecs/lesson_3/day_4/CarDemo.java
+++ src/student_svjatoslavs_cernobrivecs/lesson_3/day_4/CarDemo.java
@@ -5,6 +5,7 @@ public class CarDemo {
     public static void main(String[] args){
         Car myCar = new Car("Nissan");
         String carModel = myCar.getModel();
+        // Changed "myPhone" on "myCar"
         System.out.println("Car model = " + carModel);
     }
 }
