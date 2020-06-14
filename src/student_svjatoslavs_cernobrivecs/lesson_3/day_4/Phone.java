bc3d4e474015dfb0a7d0e8e3fdfbfe9a34bea8ad
scdk-web <s.chernobrivec@gmail.com>
2020-06-14 17:00:21 +0300

    Lesson3.HomeWork

---
+++ src/student_svjatoslavs_cernobrivecs/lesson_3/day_4/Phone.java
@@ -0,0 +1,13 @@
+package student_svjatoslavs_cernobrivecs.lesson_3.day_4;
+
+public class Phone {
+
+    private String model;
+
+    public Phone(String newModel) {
+        this.model = newModel;
+    }
+    public String getModel() {
+        return this.model;
+    }
+}
