b3823eb4ceedff8a4888cb713931c1161d970a58
scdk-web <s.chernobrivec@gmail.com>
2020-06-14 16:51:45 +0300

    Lesson3.HomeWork

---
+++ src/student_svjatoslavs_cernobrivecs/lesson_3/day_4/Book.java
@@ -0,0 +1,14 @@
+package student_svjatoslavs_cernobrivecs.lesson_3.day_4;
+
+public class Book {
+
+    private String title;
+
+    public Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+}
