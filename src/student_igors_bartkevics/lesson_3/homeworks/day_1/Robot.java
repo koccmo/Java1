47ee298dd504b15379fa78597761c6c888dc1046
Igors <igors.barkevics@gmail.com>
2020-06-14 12:14:13 +0300

    Lesson 3 day_1

--- src/student_igors_bartkevics/lesson_3/homeworks/day_1/Robot.java
+++ src/student_igors_bartkevics/lesson_3/homeworks/day_1/Robot.java
@@ -1,16 +1,26 @@
 package student_igors_bartkevics.lesson_3.homeworks.day_1;
 
+import java.util.Scanner;
+
 public class Robot {
 
-        public Robot() {
+    public Robot() {
 
-        }
+    }
+
+    String robotName;
 
     public void sayHello() {
-        System.out.println("Hello");
+        System.out.println("Hello I am robot");
+    }
+
+    public void setRobotName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Set name for robot ");
+        robotName = scanner.nextLine();
     }
 
     public void sayYourName() {
-            System.out.println("My name is ROBO");
+            System.out.println("My name is " + robotName);
     }
 }
