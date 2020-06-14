2ea567640afc8aab8dd43ff8cd16340b02cf726d
Igors <igors.barkevics@gmail.com>
2020-06-14 16:09:44 +0300

    Lesson 2 day 2 repair commit

--- src/student_igors_bartkevics/lesson_2/homeworks/day_2/Task_8.java
+++ src/student_igors_bartkevics/lesson_2/homeworks/day_2/Task_8.java
@@ -1,10 +1,11 @@
-
-
+package student_igors_bartkevics.lesson_2.homeworks.day_2;
 
 import java.util.Scanner;
 
-public class Task8 {
+public class Task_8 {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name");
         String userName = scanner.nextLine();
