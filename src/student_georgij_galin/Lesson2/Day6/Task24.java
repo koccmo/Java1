7e32c4b13b7236077d8ea70db3f176e5eac70d3b
Georgij Galin <65977707+GeorgijGalin21@users.noreply.github.com>
2020-06-15 18:18:54 +0300

    Delete Task24.java

--- src/student_georgij_galin/Lesson2/Day6/Task24.java
+++
@@ -1,7 +0,0 @@
-package student_georgij_galin.Lesson2.Day6;
-
-public class Task24 {
-    public static void main(String[] args) {
-        System.out.println("Code outside the method will not compile!");
-    }
-}
