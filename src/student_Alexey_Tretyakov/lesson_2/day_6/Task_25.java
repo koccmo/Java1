9fc38c3b04fd36ce3d0ba837dd81829cecd0dbcb
Alexey777555 <eiroartlv@inbox.lv>
2020-06-15 20:24:05 +0300

    lesson_2, Task_25

---
+++ src/student_Alexey_Tretyakov/lesson_2/day_6/Task_25.java
@@ -0,0 +1,10 @@
+package student_Alexey_Tretyakov.lesson_2.day_6;
+
+public class Task_25 {
+    //class NoSleepNoFocus {
+        public static void main(String[] args) {
+            System.out.println("I wanna coffee");
+            System.out.println("So bad");
+        }
+
+}
