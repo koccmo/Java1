fb51c03be2b4528a11add87f808e25c85a92000b
Alexey777555 <eiroartlv@inbox.lv>
2020-06-15 18:56:34 +0300

    lesson_2, Task_18

---
+++ src/student_Alexey_Tretyakov/lesson_2/day_5/SizeMatters.java
@@ -0,0 +1,14 @@
+package student_Alexey_Tretyakov.lesson_2.day_5;
+//Заставьте эту программу работать!
+public class SizeMatters {
+
+
+    // class SizeMatters {
+        public static void main(String[] args) {
+
+            System.out.println("Sorry, I have an errors.");
+            System.out.println("Please fix me!"); 
+        }
+
+
+}
