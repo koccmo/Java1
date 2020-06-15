e6ed69cb8516a18a6ec257a623f09ab42c2c8992
Georgij Galin <65977707+GeorgijGalin21@users.noreply.github.com>
2020-06-15 18:19:01 +0300

    Delete Task25.java

--- src/student_georgij_galin/Lesson2/Day6/Task25.java
+++
@@ -1,8 +0,0 @@
-package student_georgij_galin.Lesson2.Day6;
-
-public class Task25 {
-    public static void main(String[] args) {
-        System.out.println("I wanna coffee");
-        System.out.println("So bad");
-    }
-}
