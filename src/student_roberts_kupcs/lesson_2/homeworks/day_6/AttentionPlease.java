96432a981f404e6918cb746d7d1ecdac88f0f967
robku <rkarrow2@gmail.com>
2020-06-14 23:14:01 -0700

    lesson_2 day_6 create AttentionPlease.java Task_24

---
+++ src/student_roberts_kupcs/lesson_2/homeworks/day_6/AttentionPlease.java
@@ -0,0 +1,10 @@
+package student_roberts_kupcs.lesson_2.homeworks.day_6;
+
+public class AttentionPlease {
+    //System.out.println("Code outside the method will not compile!");
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+
+
+    }
+}
