8c416db55a743ad8a77e713cc0ab6560d976702b
Alexey777555 <eiroartlv@inbox.lv>
2020-06-15 23:16:30 +0300

    lesson_2, day_9

--- src/student_Alexey_Tretyakov/lesson_2/day_3/Task_9.java
+++ src/student_Alexey_Tretyakov/lesson_2/day_3/Task_9.java
@@ -9,16 +9,20 @@ public class Task_9 {
         // Enter first number;
         Scanner scannerNumber = new Scanner(System.in);
         System.out.print("Введите целое число больше 0 (по умолчанию = 8) = ");
+        int firstNumber = 8;
 
         if (scannerNumber.hasNextInt()) {
-            int firstNumber = scannerNumber.nextInt();
+            firstNumber = scannerNumber.nextInt();
                // System.out.println( firstNumber );
             for (int i = 1; i <= 10; i++) {
                  System.out.println(firstNumber + " x " + i + " = " + i * firstNumber);
                 }
 
         } else {
-            System.out.println("Вы ввели не целое число, запустите программу снова :)");
+            System.out.println("Вы ввели не неправильное значение, запустите программу снова :)");
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(firstNumber + " x " + i + " = " + i * firstNumber);
+            }
         }
 
 
