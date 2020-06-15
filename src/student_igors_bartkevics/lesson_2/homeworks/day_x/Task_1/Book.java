a75bd96ea993996ae3a3d0e8130e864c210f34f5
Igors <igors.barkevics@gmail.com>
2020-06-15 23:39:41 +0300

    Lesson 2

--- src/student_igors_bartkevics/lesson_2/homeworks/day_x/Task_1/Book.java
+++
@@ -1,5 +0,0 @@
-package student_igors_bartkevics.lesson_2.homeworks.day_x.Task_1;
-
-public class Book {
-
-}
--- src/student_igors_bartkevics/lesson_2/homeworks/day_x/Task_1/LoginService.java
+++
@@ -1,4 +0,0 @@
-package student_igors_bartkevics.lesson_2.homeworks.day_x.Task_1;
-
-public class LoginService {
-}
