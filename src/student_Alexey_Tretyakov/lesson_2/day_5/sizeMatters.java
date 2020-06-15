3a2225e404683cba1fb87d8c31654406bbd9aa2f
Alexey777555 <eiroartlv@inbox.lv>
2020-06-15 19:40:43 +0300

    lesson_2, Task_19

--- src/student_Alexey_Tretyakov/lesson_2/day_5/sizeMatters.java
+++ src/student_Alexey_Tretyakov/lesson_2/day_5/sizeMatters.java
@@ -1,13 +1,13 @@
 package student_Alexey_Tretyakov.lesson_2.day_5;
 //Заставьте эту программу работать!
-public class SizeMatters {
+public class sizeMatters {
 
 
     // class SizeMatters {
         public static void main(String[] args) {
 
             System.out.println("Sorry, I have an errors.");
-            System.out.println("Please fix me!"); 
+            System.out.println("Please fix me!");
         }
 
 
