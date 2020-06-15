b0d744a72ff566a7d13dbc73f88dd2846e30ca40
losev <missteryja@gmail.com>
2020-06-15 14:22:17 +0300

    lesson_2, day_x

---
+++ src/student_jekaterina_loseva/lesson_2/day_x/classes/Book.java
@@ -0,0 +1,10 @@
+package student_jekaterina_loseva.lesson_2.day_x.classes;
+
+public class Book {
+
+    public static void main(String[] args) {
+
+        System.out.println("Class Book");
+
+    }
+}
