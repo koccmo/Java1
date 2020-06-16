18e9486efeccc3f5ae33a21cb816742c730342e7
georgij galin <galin.georgij95@gmail.com>
2020-06-16 13:32:12 +0300

    Homeworks

---
+++ src/student_georgij_galin/lesson2/day_x/SampleProgramm.java
@@ -0,0 +1,13 @@
+package student_georgij_galin.lesson2.day_x;
+
+/*
+ Пример простой программы на Java
+*/
+
+public class SampleProgramm
+{
+    public static void main (String [] args)
+    {
+        System.out.println("Hello, Java world!");
+    }
+}
