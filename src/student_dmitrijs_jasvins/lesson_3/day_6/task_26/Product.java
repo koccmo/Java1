4e176961a60cf1f0e9b0357599d6ad18be0bb499
Dmitryyyyo <dmitrijs.ja@gmail.com>
2020-06-16 18:44:17 +0300

    Lesson3_homework's

---
+++ src/student_dmitrijs_jasvins/lesson_3/day_6/task_26/Product.java
@@ -0,0 +1,27 @@
+package student_dmitrijs_jasvins.lesson_3.day_6.task_26;
+
+public class Product {
+    private String name;
+    private double regularPrice;
+    private double actualPrice;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public double getRegularPrice(double regularPrice) {
+        this.regularPrice = regularPrice;
+        return regularPrice;
+    }
+
+    public double getActualPrice(double actualPrice) {
+        this.actualPrice = regularPrice * actualPrice / 100;
+        return actualPrice;
+    }
+
+    public void printInformation() {
+        System.out.println("Product name : " + name);
+        System.out.println("Product regular price : " + regularPrice);
+        System.out.println("Product price with discount : " + (regularPrice - actualPrice));
+    }
+}
