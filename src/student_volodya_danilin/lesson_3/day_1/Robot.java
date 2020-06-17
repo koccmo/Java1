5b5b37a1950445f420d7aedb9d96ae2d24ff6ee7
binco <vladimirs.danilins@gmail.com>
2020-06-17 22:13:06 +0300

    homeworks

---
+++ src/student_volodya_danilin/lesson_3/day_1/Robot.java
@@ -0,0 +1,35 @@
+package student_volodya_danilin.lesson_3.day_1;
+
+//Создайте класс робот (Robot).
+//
+//Научите вашего робота "говорить" привет.
+//
+//Создайте в классе Robot метод со следующей сигнатурой:
+//
+//    public void sayHello() {
+//        // тут напишите команду распечатки на консоли строки "Hello!"
+//    }
+//В классе робот (Robot) создайте конструктор без параметров:
+//
+//    public Robot() {
+//
+//    }
+
+public class Robot {
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+    public Robot(){
+
+    }
+        public static void main(String[] args){
+
+
+
+
+    }
+}
