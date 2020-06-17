4a8331c4e0079a6c2f39ee28fe99b5925f0ef166
AlexaMaksimovic <lexaferret@inbox.lv>
2020-06-17 08:09:21 +0300

    -lesson 2-homework

---
+++ src/student_aleksandra_maksimovic/lesson_3/homeworks/day_2/Task8.java
@@ -0,0 +1,10 @@
+package student_aleksandra_maksimovic.lesson_3.homeworks.day_2;
+
+public class Task8 {
+
+    public static void main(String[] args) {
+
+        String myName = "Aleksandra";
+        System.out.println("Hello " + myName + "!");
+    }
+}
