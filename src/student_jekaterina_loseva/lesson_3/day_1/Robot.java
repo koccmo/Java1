1d152f73813f85adbfa3f52a3450a8aa17e937bb
losev <missteryja@gmail.com>
2020-06-17 23:15:52 +0300

    lesson_3, day_1

---
+++ src/student_jekaterina_loseva/lesson_3/day_1/Robot.java
@@ -0,0 +1,17 @@
+package student_jekaterina_loseva.lesson_3.day_1;
+
+public class Robot {
+
+    public Robot() {
+
+    }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO!");
+    }
+
+}
