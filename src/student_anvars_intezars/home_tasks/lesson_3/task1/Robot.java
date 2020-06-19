e621c75298c44682444acfd777e48bf858833e06
Anvar331 <anvar.intezar@gmail.com>
2020-06-19 18:45:13 +0300

    Lesson3/Day1/Task1

---
+++ src/student_anvars_intezars/home_tasks/lesson_3/task1/Robot.java
@@ -0,0 +1,10 @@
+package student_anvars_intezars.home_tasks.lesson_3.task1;
+
+public class Robot {
+
+    public void sayHello() {
+
+        System.out.println("Hello!");
+    }
+    
+}
---
+++ src/student_anvars_intezars/home_tasks/lesson_3/task1/RobotDemo.java
@@ -0,0 +1,11 @@
+package student_anvars_intezars.home_tasks.lesson_3.task1;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+
+        Robot firstRobot = new Robot();
+            firstRobot.sayHello();
+
+    }
+}
