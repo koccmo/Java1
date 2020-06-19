4320488cdb01beac8e8d934c6f4c9426e0d134fb
Dmitryyyyo <dmitrijs.ja@gmail.com>
2020-06-19 22:23:13 +0300

    code review fix

--- src/student_dmitrijs_jasvins/lesson_2/day_x/SuperTask_5.java
+++ src/student_dmitrijs_jasvins/lesson_2/day_x/SuperTask_5.java
@@ -1,10 +1,8 @@
 package student_dmitrijs_jasvins.lesson_2.day_x;
 
-import java.sql.SQLOutput;  // что это за import? Не оставляйте не нужные import выражения
-
 public class SuperTask_5 {
     // Method
-    public static void main(String[] args){
+    public static void main(String[] args) {
         /*
         Enter on the screen
          */
