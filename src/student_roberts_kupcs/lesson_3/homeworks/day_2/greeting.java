5ca49eaee904e322cd576aa60eb84c6f22fb69a6
robku <rkarrow2@gmail.com>
2020-06-20 12:11:29 -0700

    Lesson 3 day 2 create Greeting.java Task_8

---
+++ src/student_roberts_kupcs/lesson_3/homeworks/day_2/greeting.java
@@ -0,0 +1,10 @@
+package student_roberts_kupcs.lesson_3.homeworks.day_2;
+
+public class greeting {
+    public static void main(String[] args) {
+
+        String userName = "Roberts";
+        String greeting1 = "Hi " + userName + " !";
+        System.out.println(greeting1);
+    }
+}
