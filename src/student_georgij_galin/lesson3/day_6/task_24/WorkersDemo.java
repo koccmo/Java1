78dcddb09ed52d94a4fce0b479b88bf5701df190
georgij galin <galin.georgij95@gmail.com>
2020-06-20 15:14:55 +0300

    Homeworks

---
+++ src/student_georgij_galin/lesson3/day_6/task_24/WorkersDemo.java
@@ -0,0 +1,20 @@
+package student_georgij_galin.lesson3.day_6.task_24;
+
+public class WorkersDemo {
+
+    public static void main(String[] args){
+        Workers workers1 = new Workers("Alex",32);
+        workers1.sayAboutYou();
+
+
+        Workers workers2 = new Workers("Andrei",34);
+        workers2.sayAboutYou();
+
+
+
+        Workers workers3 = new Workers("Mikhail",30);
+        workers3.sayAboutYou();
+
+    }
+}
+
