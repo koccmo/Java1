066918186967989da455248de3917eb1c8946f0b
Anvar331 <anvar.intezar@gmail.com>
2020-06-20 23:54:58 +0300

    Lesson3/Day1/Task6

--- src/student_anvars_intezars/home_tasks/lesson_3/day1/RobotDemo.java
+++ src/student_anvars_intezars/home_tasks/lesson_3/day1/RobotDemo.java
@@ -15,8 +15,8 @@ public class RobotDemo {
 
         System.out.println();
         Robot thirdRobot = new Robot();
-        thirdRobot.sayHello();
-        thirdRobot.sayYourName();
+            thirdRobot.sayHello();
+            thirdRobot.sayYourName();
 
     }
 }
