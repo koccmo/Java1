75cc1991b3e88413656915cac28402f6390f575f
robku <rkarrow2@gmail.com>
2020-06-20 11:35:06 -0700

    Lesson 3 day 2 create Name.java Task_7

---
+++ src/student_roberts_kupcs/lesson_3/homeworks/day_2/Name.java
@@ -0,0 +1,9 @@
+package student_roberts_kupcs.lesson_3.homeworks.day_2;
+
+public class Name {
+
+    public static void main(String[] args) {
+        String myName = "Roberts";
+        System.out.println(myName);
+    }
+}
