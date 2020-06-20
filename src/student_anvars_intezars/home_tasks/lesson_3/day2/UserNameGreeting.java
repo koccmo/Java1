bd7bd232cd8b7f01b82e4fcfc5d287601d5580a2
Anvar331 <anvar.intezar@gmail.com>
2020-06-21 00:03:48 +0300

    Lesson3/Day2/Task7,8

---
+++ src/student_anvars_intezars/home_tasks/lesson_3/day2/UserNameGreeting.java
@@ -0,0 +1,11 @@
+package student_anvars_intezars.home_tasks.lesson_3.day2;
+
+public class UserNameGreeting {
+
+    public static void main(String[] args) {
+
+        String userName = "Anvar";
+        String greeting = "Hi " + userName + "!";
+        System.out.println(greeting);
+    }
+}
