5ede635e3e77fca78efa53060667eaaeb9408f73
georgij galin <galin.georgij95@gmail.com>
2020-06-20 13:14:52 +0300

    Homeworks

--- src/student_georgij_galin/lesson3/day_6/task_28/ComputerDemo.java
+++ src/student_georgij_galin/lesson3/day_6/task_28/ComputerDemo.java
@@ -2,7 +2,7 @@ package student_georgij_galin.lesson3.day_6.task_28;
 
 public class ComputerDemo {
     public static void main(String[] args) {
-        Computer computer = new Computer("Acer", "Intel");
+        Computer computer = new Computer("Acer", "Aspire E5-576");
         String computerName = computer.getName();
         String computerModel = computer.getModel();
         System.out.println("Name : " + computerName);
