170ed4321648288b8bd843f0a8f20c29c037b3b9
losev <missteryja@gmail.com>
2020-06-21 15:51:23 +0300

    lesson_3, day_4

---
+++ src/student_jekaterina_loseva/lesson_3/day_4/task_16/Car.java
@@ -0,0 +1,15 @@
+package student_jekaterina_loseva.lesson_3.day_4.task_16;
+
+public class Car {
+
+    private String model;
+
+    public Car(String newModel) {
+        this.model = newModel;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+}
---
+++ src/student_jekaterina_loseva/lesson_3/day_4/task_16/CarDemo.java
@@ -0,0 +1,11 @@
+package student_jekaterina_loseva.lesson_3.day_4.task_16;
+
+public class CarDemo {
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.getModel();
+        System.out.println("Car model = " + carModel);
+    }
+
+}
