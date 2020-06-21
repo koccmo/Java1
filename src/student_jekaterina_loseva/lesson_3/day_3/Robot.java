6f292693a32b74af5fa46268bf83a511ca0960c0
losev <missteryja@gmail.com>
2020-06-21 15:49:34 +0300

    lesson_3, day_3

---
+++ src/student_jekaterina_loseva/lesson_3/day_3/Robot.java
@@ -0,0 +1,18 @@
+package student_jekaterina_loseva.lesson_3.day_3;
+
+public class Robot {
+
+    private String name;
+
+    public Robot(String robotName) {
+        this.name = robotName;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is " + this.name);
+    }
+}
