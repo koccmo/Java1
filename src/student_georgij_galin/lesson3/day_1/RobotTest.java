6fc85e888bf529610dadbb16417f7f08c3e58934
georgij galin <galin.georgij95@gmail.com>
2020-06-21 15:19:18 +0300

    Homeworks

--- src/student_georgij_galin/lesson3/day_1/RobotTest.java
+++ src/student_georgij_galin/lesson3/day_1/RobotTest.java
@@ -4,7 +4,7 @@ package student_georgij_galin.lesson3.day_1;
 
 public class RobotTest {
     public static void main(String[] args) {
-        System.out.println("First robot parameters :");
+        System.out.println("First robot parameter :");
        Robot firstRobot = new Robot();
         firstRobot.SayHello();
         firstRobot.sayYourName();
