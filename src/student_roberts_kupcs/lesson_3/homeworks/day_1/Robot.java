4205959aaa285c051f02cdbd6b798205774db48e
robku <rkarrow2@gmail.com>
2020-06-22 11:49:39 -0700

    Lesson 3 day 3 correct Robot.java Task_12 RobotDemo.java Task_13

--- src/student_roberts_kupcs/lesson_3/homeworks/day_1/Robot.java
+++ src/student_roberts_kupcs/lesson_3/homeworks/day_1/Robot.java
@@ -4,7 +4,10 @@ package student_roberts_kupcs.lesson_3.homeworks.day_1;
     // Создайте класс робот (Robot).
 public class Robot {
 
+    // Task_12
+    // Добавьте в класс Robot свойство name
     private String name;
+
     private String speak;
 
     // Создайте в классе Robot метод
@@ -15,14 +18,15 @@ public class Robot {
 
     // Task_2
     // В классе робот (Robot) создайте конструктор без параметров
-    public Robot () {
+    public Robot (String robotName) {
+        this.name = robotName;
 
     }
 
     //Task_5
     //Создайте в классе Robot метод
     public void sayYourName() {
-        System.out.println("My name is ROBO");
+        System.out.println("My name is " + this.name);
 
     }
 }
--- src/student_roberts_kupcs/lesson_3/homeworks/day_1/RobotDemo.java
+++ src/student_roberts_kupcs/lesson_3/homeworks/day_1/RobotDemo.java
@@ -1,28 +1,30 @@
 package student_roberts_kupcs.lesson_3.homeworks.day_1;
 
+
+
 public class RobotDemo {
     public static void main(String[] args) {
 
         // создайте экземпляр класса Robot вызвав конструктор класса Task_3
-        Robot robot1 = new Robot();
+        Robot robot = new Robot("");
 
         // вызовите
         //метод sayHello() у созданного ранее виртуального обьекта Task_4
-        robot1.sayHello();
+        robot.sayHello();
 
         // вызовите
         //метод sayYourName() Task_5
-        robot1.sayYourName();
+        robot.sayYourName();
 
         // продемонстрируйте создание
         //нескольких виртуальныx обьектов робот и работу с ними Task_6
-        Robot robot2 = new Robot();
+        Robot robot1 = new Robot("Rider");
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        Robot robot2 = new Robot("John");
         robot2.sayHello();
         robot2.sayYourName();
-
-        Robot robot3 = new Robot();
-        robot3.sayHello();
-        robot3.sayYourName();
 
 
     }
