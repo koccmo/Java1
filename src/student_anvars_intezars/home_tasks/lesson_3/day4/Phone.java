e5e7b21619ae66677a35f88581efc75d50254afa
Anvar331 <anvar.intezar@gmail.com>
2020-06-22 21:22:55 +0300

    Lesson3/Day4/Task15

---
+++ src/student_anvars_intezars/home_tasks/lesson_3/day4/Phone.java
@@ -0,0 +1,15 @@
+package student_anvars_intezars.home_tasks.lesson_3.day4;
+
+public class Phone {
+
+        private String model;
+
+    public Phone(String newModel) {
+            this.model = newModel;
+        }
+
+        public String getModel() {
+            return this.model;
+        }
+
+}
