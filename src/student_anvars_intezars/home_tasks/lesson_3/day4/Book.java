647951f2b6d662e39ef55b134c5d1b0870b5afeb
Anvar331 <anvar.intezar@gmail.com>
2020-06-22 21:19:29 +0300

    Lesson3/Day4/Task14

---
+++ src/student_anvars_intezars/home_tasks/lesson_3/day4/Book.java
@@ -0,0 +1,16 @@
+package student_anvars_intezars.home_tasks.lesson_3.day4;
+
+public class Book {
+
+    private String title;
+
+    public Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+
+}
