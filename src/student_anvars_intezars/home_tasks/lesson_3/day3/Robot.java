2e8a29ffc5a415e8e58b22fe807a942dbf35a5a4
Anvar331 <anvar.intezar@gmail.com>
2020-06-22 20:50:53 +0300

    Lesson3/Day3/Task11,12,13

---
+++ src/student_anvars_intezars/home_tasks/lesson_3/day3/Robot.java
@@ -0,0 +1,22 @@
+package student_anvars_intezars.home_tasks.lesson_3.day3;
+
+public class Robot {
+
+    private String name;
+
+    public Robot(String robotName) {
+        this.name = robotName;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is " + name); }
+
+    }
+
+
+
+
