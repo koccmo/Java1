a8c58184b17c90d5cbf6866d13fd8cfeb28c86c7
Anvar331 <anvar.intezar@gmail.com>
2020-06-22 21:27:30 +0300

    Lesson3/Day4/Task15

---
+++ src/student_anvars_intezars/home_tasks/lesson_3/day4/CarDemo.java
@@ -0,0 +1,12 @@
+package student_anvars_intezars.home_tasks.lesson_3.day4;
+
+public class CarDemo {
+
+    public static void main(String[] args) {
+
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.getModel();
+        System.out.println("Car model = " + carModel);
+
+}
+}
