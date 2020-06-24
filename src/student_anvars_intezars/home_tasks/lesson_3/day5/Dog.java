8049810defde046475edb941423fd573f68044b2
Anvar331 <anvar.intezar@gmail.com>
2020-06-24 22:03:49 +0300

    Lesson3/Day5/Task18

---
+++ src/student_anvars_intezars/home_tasks/lesson_3/day5/Dog.java
@@ -0,0 +1,15 @@
+package student_anvars_intezars.home_tasks.lesson_3.day5;
+
+public class Dog {
+
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public void voice () {
+        System.out.println("Bobik");
+        }
+
+}
---
+++ src/student_anvars_intezars/home_tasks/lesson_3/day5/DogDemo.java
@@ -0,0 +1,12 @@
+package student_anvars_intezars.home_tasks.lesson_3.day5;
+
+public class DogDemo {
+
+    public static void main(String[] args) {
+
+        Dog puppy = new Dog("Bobik");
+            puppy.voice();
+            puppy.voice();
+            puppy.voice();
+    }
+}
