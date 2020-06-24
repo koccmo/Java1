218ad0f396aa04348bc4bee6e1db6ed87aa6fb7f
scdk-web <s.chernobrivec@gmail.com>
2020-06-24 17:13:09 +0300

    Lesson3.Task_26

---
+++ src/student_svjatoslavs_cernobrivecs/lesson_3/day_6/task_26/ProductDemo.java
@@ -0,0 +1,10 @@
+package student_svjatoslavs_cernobrivecs.lesson_3.day_6.task_26;
+
+public class ProductDemo {
+
+    public static void main(String[] args){
+
+        Product car = new Product("Bmw",15000,32.7);
+        car.printInformation();
+    }
+}
