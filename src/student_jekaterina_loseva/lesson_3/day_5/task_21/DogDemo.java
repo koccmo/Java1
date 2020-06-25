3192773c9a47eda07902f7e84806f2723e442c97
losev <missteryja@gmail.com>
2020-06-26 01:02:36 +0300

    lesson_3, day_5

---
+++ src/student_jekaterina_loseva/lesson_3/day_5/task_21/DogDemo.java
@@ -0,0 +1,12 @@
+package student_jekaterina_loseva.lesson_3.day_5.task_21;
+
+public class DogDemo {
+
+    public static void main(String[] args) {
+
+        Dog myDog = new Dog("Jack", 6, "black");
+        myDog.voice();
+
+    }
+
+}
