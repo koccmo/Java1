f141fa7f7c865923c0be3e1f199c90d5498fdd9b
robku <rkarrow2@gmail.com>
2020-06-24 22:51:33 -0700

    Lesson 3 day 5 create Dog.java DogDemo.java Task_18

---
+++ src/student_roberts_kupcs/lesson_3/homeworks/day_5/Dog.java
@@ -0,0 +1,17 @@
+package student_roberts_kupcs.lesson_3.homeworks.day_5;
+
+class Dog {
+
+    private String name;
+
+    public Dog() {
+
+        this.name = "Ku";
+
+    }
+
+    public void voice() {
+
+        System.out.println("кличка собаки: " + name);
+    }
+}
---
+++ src/student_roberts_kupcs/lesson_3/homeworks/day_5/DogDemo.java
@@ -0,0 +1,11 @@
+package student_roberts_kupcs.lesson_3.homeworks.day_5;
+
+public class DogDemo {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.voice();
+        dog.voice();
+        dog.voice();
+
+    }
+}
