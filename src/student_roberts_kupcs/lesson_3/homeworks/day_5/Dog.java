95bfe17c611f999188f54c4e57d2e53834b83c91
robku <rkarrow2@gmail.com>
2020-06-25 12:36:18 -0700

    Lesson 3 day 5 correct Dog.java DogDemo.java Task_19

--- src/student_roberts_kupcs/lesson_3/homeworks/day_5/Dog.java
+++ src/student_roberts_kupcs/lesson_3/homeworks/day_5/Dog.java
@@ -4,6 +4,12 @@ class Dog {
 
     private String name;
 
+    // Task_19
+    //- Должна быть возможность указывать возраст собаки.
+    //- Возраст указывается в годах (целам числом).
+    //- Возраст собаке дают в момент создания.
+    private  int age = 5;
+
     public Dog() {
 
         this.name = "Ku";
@@ -11,7 +17,11 @@ class Dog {
     }
 
     public void voice() {
-
+        // Task_18
+        // на консоль должно выводиться
+        //кличка собаки и ее возраст.
         System.out.println("кличка собаки: " + name);
+        // Task_19
+        System.out.println(("кличка собаки: " + name + " " + age + " год"));
     }
 }
