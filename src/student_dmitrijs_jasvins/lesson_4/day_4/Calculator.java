4985063a585d8b35e5e2de10397e2886571e07d6
Dmitryyyyo <dmitrijs.ja@gmail.com>
2020-06-27 01:53:53 +0300

    lesson4_homeworks

---
+++ src/student_dmitrijs_jasvins/lesson_4/day_4/Calculator.java
@@ -0,0 +1,43 @@
+package student_dmitrijs_jasvins.lesson_4.day_4;
+
+public class Calculator {
+
+    private int firstNumber;
+    private int secondNumber;
+    private int thirdNumber;
+
+    public int addition(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int subtraction(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int multiplication(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public double divison(double firstNumber, double secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public int maxOfTwoNumbers(int firstNumber, int secondNumber) {
+        return Math.max(firstNumber, secondNumber);
+    }
+
+    public int maxOfThreeNumber(int firstNumber, int secondNumber, int thirdNumber) {
+        if (firstNumber >= secondNumber && firstNumber >= thirdNumber) {
+            return firstNumber;
+        } else if (secondNumber >= firstNumber && secondNumber >= thirdNumber) {
+            return secondNumber;
+        } else {
+            return thirdNumber;
+        }
+    }
+}
+
