17d0ffc5896f33a1a2d3b3694263fa3752f4424c
robku <rkarrow2@gmail.com>
2020-06-27 00:09:59 -0700

    Lesson 3 day 6 create Dog.java DogDemo.java Task_23 Task_24

---
+++ src/student_roberts_kupcs/lesson_3/homeworks/day_6/DogDemo.java
@@ -0,0 +1,16 @@
+package student_roberts_kupcs.lesson_3.homeworks.day_6;
+
+public class DogDemo {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Uma");
+        Dog dog1 = new Dog("Nensi");
+
+        dog.dogAge(13);
+        dog.printDog();
+
+        dog1.dogAge(14);
+        dog1.printDog();
+    }
+
+
+}
