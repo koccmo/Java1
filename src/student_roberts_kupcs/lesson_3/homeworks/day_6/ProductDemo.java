d420b6828ace29c46dbfd012ab9c8f66310215f9
robku <rkarrow2@gmail.com>
2020-06-28 12:47:40 -0700

    Lesson 3 day 6 create Product.java ProductDemo.java Task_26

--- src/student_roberts_kupcs/lesson_3/homeworks/day_6/ProductDemo.java
+++ src/student_roberts_kupcs/lesson_3/homeworks/day_6/ProductDemo.java
@@ -1,4 +1,10 @@
 package student_roberts_kupcs.lesson_3.homeworks.day_6;
 
 public class ProductDemo {
+    public static void main(String[] args) {
+        Product product = new Product("Product1");
+        product.printInformation();
+
+
+    }
 }
