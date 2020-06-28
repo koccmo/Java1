8f44f1ff0cea6d948e5e5bcabf97c25ecd648b12
Alexey777555 <eiroartlv@inbox.lv>
2020-06-28 11:19:07 +0300

    lesson_3, day_4

---
+++ src/student_alexey_tretyakov/lesson_3/day_4/BankAccount.java
@@ -0,0 +1,43 @@
+package student_alexey_tretyakov.lesson_3.day_4;
+
+public class BankAccount {
+    private String ownerFirstName;
+    private String ownerLastName;
+    private int moneyAmount;
+
+    public BankAccount(String ownerFirstName,
+                       String ownerLastName,
+                       int moneyAmount) {
+        this.ownerFirstName = ownerFirstName;
+        this.ownerLastName = ownerLastName;
+        this.moneyAmount = moneyAmount;
+    }
+
+    public String getOwnerFirstName() {
+        return this.ownerFirstName;
+    }
+
+    public String getOwnerLastName() {
+        return this.ownerLastName;
+    }
+
+     public String getMoneyAmount () {
+        return String.valueOf( moneyAmount );
+    }
+
+
+}
+
+class BankAccountDemo {
+
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount("Viktor", "Savonin", 100000);
+        String ownerFirstName = bankAccount.getOwnerFirstName();
+        String ownerLastName = bankAccount.getOwnerLastName();
+        String moneyAmount = bankAccount.getMoneyAmount();
+        System.out.println("Owner first name = " + ownerFirstName);
+        System.out.println("Owner last name = " + ownerLastName);
+        System.out.println("Money amount = " + moneyAmount);
+    }
+
+}
---
+++ src/student_alexey_tretyakov/lesson_3/day_4/Car.java
@@ -0,0 +1,24 @@
+package student_alexey_tretyakov.lesson_3.day_4;
+
+public class Car {
+    private String model;
+
+    public Car (String newModel) {
+        this.model = newModel;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+}
+
+class CarDemo {
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.getModel();
+        System.out.println("Car model = " + carModel);
+    }
+
+}
