078edb9660e93d9abff4e3c7002e3dde3fdabdda
robku <rkarrow2@gmail.com>
2020-06-29 04:12:24 -0700

    Lesson 3 day 6 correct BankAccount.java BankAccountDemo.java Task_27

---
+++ src/student_roberts_kupcs/lesson_3/homeworks/day_6/BankAccount.java
@@ -0,0 +1,19 @@
+package student_roberts_kupcs.lesson_3.homeworks.day_6;
+
+public class BankAccount {
+    private String owner;
+    private int money;
+
+    public BankAccount(String ownerFirstName, int moneyAmount) {
+        this.owner = ownerFirstName;
+        this.money = moneyAmount;
+    }
+
+    public String getOwner() {
+        return this.owner;
+    }
+
+    public int getMoney() {
+        return this.money;
+    }
+}
