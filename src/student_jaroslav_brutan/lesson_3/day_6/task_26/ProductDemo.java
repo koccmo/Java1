7f798865548e616196bf6567c9976d2270fa4354
BRUTN <brutan.jaroslav@gmail.com>
2020-06-29 23:56:36 +0300

    Lesson 3, day 6

---
+++ src/student_jaroslav_brutan/lesson_3/day_6/task_26/ProductDemo.java
@@ -0,0 +1,11 @@
+package student_jaroslav_brutan.lesson_3.day_6.task_26;
+
+public class ProductDemo {
+    public static void main(String []args){
+        Product product = new Product("Guitar");
+        product.regularPrice = 250;
+        product.discount = 17;
+        product.printInformation();
+
+    }
+}
