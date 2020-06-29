5799102188a3bb8b70ba4d7a762161ad5cd0b43d
Anvar331 <anvar.intezar@gmail.com>
2020-06-29 22:28:26 +0300

    Lesson3/Day6/Task23

---
+++ src/student_anvars_intezars/home_tasks/lesson_3/day6/task23/WhatIsJavaHome.java
@@ -0,0 +1,23 @@
+package student_anvars_intezars.home_tasks.lesson_3.day6.task23;
+
+public class WhatIsJavaHome {
+
+    /*
+    JAVA_HOME is a environment variable (in Unix terminologies),
+     or a PATH variable (in Windows terminology).
+     A lot of well behaving Java applications (which need the JDK/JRE) to run,
+     looks up the JAVA_HOME variable for the location where the Java compiler/interpreter
+     may be found.
+     JAVA HOME is used for setting up the
+     environment variable for JAVA.
+     The JAVA_HOME environment variable needs to point to
+     the installation directory on your system that contains the bin/ directory
+     where the java executable physically resides, and the lib/ directory which
+     contains the core java libraries and properties files. For linux users, it
+     is almost certainly not the same directory that you get if you do "which java"
+     from the command line, because that would be too easy. So if JAVA_HOME isn't already set,
+     your next task is to find the directory to which you need to set it.
+
+
+     */
+}
