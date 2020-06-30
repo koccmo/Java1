19d3866381b614ae02be8450c1c0cb2c5511d39c
Alexey777555 <eiroartlv@inbox.lv>
2020-06-30 12:16:48 +0300

    lesson_3, day_6

---
+++ src/student_alexey_tretyakov/lesson_3/day_6/Circle.java
@@ -0,0 +1,27 @@
+package student_alexey_tretyakov.lesson_3.day_6;
+
+import static java.lang.Math.PI;
+
+class Circle {
+    private double radius;
+
+    public Circle (double radius){
+        this.radius = radius;
+    }
+    public double getRadius(){
+        return radius;
+    }
+    public double calculateArea(){
+        return  2 * PI * radius;
+    }
+
+}
+
+class CircleDemo {
+    public static void main(String[] arg) {
+
+        Circle newCircle = new Circle(5.2);
+        System.out.println ("\nWe calculate area of circle with radius " + newCircle.getRadius() +
+               " = " + newCircle.calculateArea() );
+    }
+}
---
+++ src/student_alexey_tretyakov/lesson_3/day_6/Product.java
@@ -0,0 +1,46 @@
+package student_alexey_tretyakov.lesson_3.day_6;
+
+public class Product {
+    private String productName;
+    private double regularPrice;
+    private double discount;
+    private double actualPrice;
+
+    public Product (  String productName, double regularPrice, double discount ) {
+        this.productName = productName;
+        this.regularPrice = regularPrice;
+        this.discount = discount;
+        this.actualPrice = setActualPrice();
+    }
+
+    public double setRegularPrice( double Price) {
+        regularPrice = Price;
+        actualPrice = setActualPrice();
+        return regularPrice ;
+    }
+    public double setDiscount( double Discount) {
+        discount = Discount;
+        actualPrice = setActualPrice();
+        return discount;
+    }
+    public double setActualPrice ()  {
+        actualPrice = regularPrice * ( 1 - discount );
+        return actualPrice;
+    }
+    public void printInformation() {
+        System.out.println("\n Product name: " + productName + "\n Regular price: " + regularPrice
+                           + "\n Discount: " + discount * 100 + "%"
+                            + "\n Actual price: " + actualPrice );
+    }
+}
+class ProductDemo {
+    public static void main(String[] arg) {
+        Product newProduct = new Product ("Bred",1.20, 0.30);
+        newProduct.printInformation();
+        newProduct.setRegularPrice(1.8);
+        newProduct.printInformation();
+        newProduct.setDiscount(0.50);
+        newProduct.printInformation();
+
+    }
+}
