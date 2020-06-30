0de28517b12855d22e7c96361b47d7ff7bbdccb0
georgij galin <galin.georgij95@gmail.com>
2020-06-30 10:11:28 +0300

    Homeworks

--- src/student_georgij_galin/lesson3/day_6/task_26/ProductDemo.java
+++ src/student_georgij_galin/lesson3/day_6/task_26/ProductDemo.java
@@ -1,11 +1,15 @@
 package student_georgij_galin.lesson3.day_6.task_26;
 
 
+import student_valerija_ionova.lesson_3.hometasks.day_6.task_26.Product;
+
 public class ProductDemo {
     public static void main(String[] args) {
-        Product myProduct = new Product("Television");
-        myProduct.getRegularPrice(600);
-        myProduct.getActualPrice(120);
-        myProduct.printInformation();
+        Product product = new Product("Television");
+        product.price=800;
+        product.discount=30;
+        product.printInformation();
+
+
     }
 }
