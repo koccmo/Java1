ebc5cf8e2f30fb5b1abeee53d2c60c34ac1476d0
Igors <igors.barkevics@gmail.com>
2020-06-30 20:59:15 +0300

    Lesson 2 Day 1 Task_3 version 2

--- src/student_igors_bartkevics/lesson_4/homeworks/day_1/Task_3.java
+++ src/student_igors_bartkevics/lesson_4/homeworks/day_1/Task_3.java
@@ -20,13 +20,46 @@ public class Task_3 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        /*
+        Version 1 (using String[]):
+
         String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday",
+
                                 "Friday", "Saturday", "Sunday"};
 
-        System.out.println("Enter number of day of week: ");
+        System.out.println("Enter number of day of week (1 - 7): ");
         int numberOfWeek = scanner.nextInt();
         System.out.println((daysOfWeek[numberOfWeek - 1]));
+        */
 
+        // Version 2 using IF
+        System.out.println("Enter number of day of week (1 - 7): ");
+        int numberOfWeek = scanner.nextInt();
+
+        if (numberOfWeek == 1) {
+            System.out.println("Monday");
+        }
+        else if (numberOfWeek == 2) {
+            System.out.println("Tuesday");
+        }
+        else if (numberOfWeek == 3) {
+            System.out.println("Wednesday");
+        }
+        else if (numberOfWeek == 4) {
+            System.out.println("Thursday");
+        }
+        else if (numberOfWeek == 5) {
+            System.out.println("Friday");
+        }
+        else if (numberOfWeek == 6) {
+            System.out.println("Saturday");
+        }
+        else if (numberOfWeek == 7) {
+            System.out.println("Sunday");
+        }
+        else {
+            System.out.println("Your entered wrong number");
+        }
     }
 
 }
