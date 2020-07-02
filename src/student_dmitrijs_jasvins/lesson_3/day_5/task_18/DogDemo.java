6e76188d7bce81fab1081a8712f83eb0d3f4ecee
Dmitryyyyo <dmitrijs.ja@gmail.com>
2020-07-02 22:35:41 +0300

    lesson3_homeworks_refactoring

--- src/student_dmitrijs_jasvins/lesson_3/day_5/task_18/DogDemo.java
+++ src/student_dmitrijs_jasvins/lesson_3/day_5/task_18/DogDemo.java
@@ -1,7 +1,7 @@
 package student_dmitrijs_jasvins.lesson_3.day_5.task_18;
 
 public class DogDemo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Dog myDog = new Dog("Dmitry");
         myDog.voice();
     }
