2538ba251871c9106baba4e49d657b84fb98cb13
Anvar331 <anvar.intezar@gmail.com>
2020-07-02 23:31:23 +0300

    Lesson4/Level1/Task1,2

--- src/student_anvars_intezars/home_tasks/lesson_4/level_1/Task2.java
+++ src/student_anvars_intezars/home_tasks/lesson_4/level_1/Task2.java
@@ -1,8 +1,8 @@
-package student_anvars_intezars.home_tasks.lesson_4;
+package student_anvars_intezars.home_tasks.lesson_4.level_1;
 
 import java.util.Scanner;
 
-public class Task1 {
+public class Task2 {
 
     public static void main(String[] args) {
 
@@ -11,12 +11,17 @@ public class Task1 {
         System.out.println("Please enter Integer:");
         int firstNumber = sc.nextInt();
 
-        if (firstNumber >=0) {
+        if (firstNumber >0) {
             System.out.println("Your number is positive");
         }
         else if (firstNumber <0) {
             System.out.println("Your number is negative");
         }
+        else {
+            System.out.println("Your number equals 0");
+        }
 
     }
 }
+
+
---
+++ src/student_anvars_intezars/home_tasks/lesson_4/level_1/Task3.java
@@ -0,0 +1,15 @@
+/*package student_anvars_intezars.home_tasks.lesson_4.level_1;
+
+import java.util.Scanner;
+
+public class Task3 {
+
+   /* public static void main(String[] args) {
+
+     Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter number from 1 till 7:");
+        int numberOfTheyOfTheWeek = sc.nextInt();
+
+        if ((numberOfTheyOfTheWeek > 0) {
+            System.out.println("Monday");*/
