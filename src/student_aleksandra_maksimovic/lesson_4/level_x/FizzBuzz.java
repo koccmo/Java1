d291d7b6d88785ad71897f5f52cfe5655b56a753
AlexaMaksimovic <lexaferret@inbox.lv>
2020-07-03 13:18:25 +0300

    -lesson 4 -homework : level - x task - 1

---
+++ src/student_aleksandra_maksimovic/lesson_4/level_x/FizzBuzz.java
@@ -0,0 +1,35 @@
+package student_aleksandra_maksimovic.lesson_4.level_x;
+/*
+Создайте класс FizzBuzz.
+В классе FizzBuzz создайте метод со следующей сигнатурой:
+
+class FizzBuzz {
+
+    public String detect(int number) {
+        // Реализуйте следующие требования:
+        // - если переданное число делится на три возвращайте "Fizz"
+        // - если переданное число делится на пять возвращайте "Buzz"
+        // - если переданное число делится на три и на пять возвращайте "FizzBuzz"
+        // - иначе возвращайте само число в виде строки (подсказка: return "" +  number)
+    }
+
+}
+
+Создайте класс FizzBuzzTest и напишите автоматические тесты,
+которые покрывают все возможные сценарии.
+ */
+public class FizzBuzz {
+    public String detect (int number) {
+        boolean fizz = number % 3 == 0;
+        boolean buzz = number % 5 == 0;
+        if (fizz && buzz) {
+            return "FizzBuzz";
+        } else if (fizz) {
+            return "Fizz";
+        } else if (buzz) {
+            return "Buzz";
+        } else {
+            return "" +  number;
+        }
+    }
+}
