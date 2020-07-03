c8af87af61edfb79d3257f961de776fb51b277ff
Anvar331 <anvar.intezar@gmail.com>
2020-07-03 18:55:57 +0300

    Lesson4/Level1/Task3

--- src/student_anvars_intezars/home_tasks/lesson_4/level_1/Task3.java
+++ src/student_anvars_intezars/home_tasks/lesson_4/level_1/Task3.java
@@ -1,15 +1,58 @@
-/*package student_anvars_intezars.home_tasks.lesson_4.level_1;
+package student_anvars_intezars.home_tasks.lesson_4.level_1;
 
 import java.util.Scanner;
 
 public class Task3 {
 
-   /* public static void main(String[] args) {
+    public static void main(String[] args) {
 
-     Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Please enter number from 1 till 7:");
-        int numberOfTheyOfTheWeek = sc.nextInt();
+        int dayOfTheWeek = sc.nextInt();
 
-        if ((numberOfTheyOfTheWeek > 0) {
-            System.out.println("Monday");*/
+        switch (dayOfTheWeek) {
+
+            case 1: {
+                System.out.println("Monday");
+            }
+            break;
+
+            case 2: {
+                System.out.println("Tuesday");
+            }
+            break;
+
+            case 3: {
+                System.out.println("Wednesday");
+            }
+            break;
+
+            case 4:
+                {System.out.println("Thursday");
+            }
+            break;
+
+            case 5: {
+                System.out.println("Friday");
+            }
+            break;
+
+            case 6: {
+                System.out.println ("Saturday");
+            }
+            break;
+
+            case 7: {
+                System.out.println ("Sunday");
+            }
+            break;
+
+            default: {
+                System.out.println("The value, which you have entered");
+                System.out.print("does not correspond to neccessary one");
+            }
+
+        }
+    }
+}
