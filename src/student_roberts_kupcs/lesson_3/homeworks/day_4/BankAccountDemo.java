e52dbcf04e700d743b9356f35906dec2631aabc9
vsavonins <viktors.savonins@proofit.lv>
2020-07-04 10:41:34 +0300

    Lesson 3 code review

--- src/student_roberts_kupcs/lesson_3/homeworks/day_4/BankAccountDemo.java
+++ src/student_roberts_kupcs/lesson_3/homeworks/day_4/BankAccountDemo.java
@@ -1,6 +1,6 @@
 package student_roberts_kupcs.lesson_3.homeworks.day_4;
 
-import static java.lang.System.out;
+import static java.lang.System.out;  // что это за импорт?
 
 public class BankAccountDemo {
     public static void main(String[] args) {
