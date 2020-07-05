ea431bf0e25eca0cb7e15768d20b5753cee93f82
robku <rkarrow2@gmail.com>
2020-07-05 12:57:06 -0700

    Lesson 4 level 3 create Calculator_1.java Task_11

---
+++ src/student_roberts_kupcs/lesson_4/homework/level_4/Calculator_1.java
@@ -0,0 +1,21 @@
+package student_roberts_kupcs.lesson_4.homework.level_4;
+
+class Calculator_1 {
+
+        public int sum ( int firstNumber, int secondNumber){
+        return firstNumber + secondNumber;
+    }
+
+        public int subtraction ( int firstNumber, int secondNumber){
+        return firstNumber - secondNumber;
+    }
+
+        public int multiplication ( int firstNumber, int secondNumber){
+        return firstNumber * secondNumber;
+    }
+
+        public int division ( int firstNumber, int secondNumber){
+        return firstNumber / secondNumber;
+    }
+
+    }
