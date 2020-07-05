b1fb2831a834070cb183157bee89bfab0b3166be
scdk-web <s.chernobrivec@gmail.com>
2020-07-05 13:01:18 +0300

    Lesson4.Task_11

---
+++ src/student_svjatoslavs_cernobrivecs/lesson_4/level_4/Calculator.java
@@ -0,0 +1,17 @@
+package student_svjatoslavs_cernobrivecs.lesson_4.level_4;
+
+public class Calculator {
+
+    public int sum(int firstNumber, int secondNumber){
+        return firstNumber + secondNumber;
+    }
+    public int substract(int firstNumber, int secondNumber){
+        return firstNumber - secondNumber;
+    }
+    public int multiply(int firstNumber, int secondNumber){
+        return firstNumber * secondNumber;
+    }
+    public int devide(int firstNumber, int secondNumber){
+        return firstNumber / secondNumber;
+    }
+}
