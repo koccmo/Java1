0e8b2584dfc7c564f46dc7d1bc063ca9eeb814c9
Anvar331 <anvar.intezar@gmail.com>
2020-07-05 16:11:16 +0300

    Lesson4/Level4/Task11

---
+++ src/student_anvars_intezars/home_tasks/lesson_4/level_4/Caclulator.java
@@ -0,0 +1,20 @@
+package student_anvars_intezars.home_tasks.lesson_4.level_4;
+
+public class Caclulator {
+
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int subtraction(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public int division(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+}
