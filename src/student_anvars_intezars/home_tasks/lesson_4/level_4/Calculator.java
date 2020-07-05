e6f4788ac148c19564bca7bf9dfd028c9b86586b
Anvar331 <anvar.intezar@gmail.com>
2020-07-05 21:56:57 +0300

    Lesson4/Level4/Task15

--- src/student_anvars_intezars/home_tasks/lesson_4/level_4/Calculator.java
+++ src/student_anvars_intezars/home_tasks/lesson_4/level_4/Calculator.java
@@ -22,15 +22,25 @@ public class Calculator {
         return (number % 2) == 0;
     }
 
-    public int maxOfTwoNumbers(int firstNumber, int secondNumber){
-            if (firstNumber >= secondNumber) {
-                return firstNumber;
-            }
-            else {
-                return secondNumber;
-            }
-
+    public int maxOfTwoNumbers(int firstNumber, int secondNumber) {
+        if (firstNumber >= secondNumber) {
+            return firstNumber;
+        } else {
+            return secondNumber;
         }
 
     }
 
+    public int maxOfThreeNumbers(int firstNumber, int secondNumber, int thirdNumber) {
+        if ((firstNumber >= secondNumber) && (firstNumber >= thirdNumber)) {
+            return firstNumber;
+
+        } else if ((secondNumber >= firstNumber) && (secondNumber >= thirdNumber)) {
+            return secondNumber;
+
+        } else {
+            return thirdNumber;
+        }
+
+    }
+}
