4f3c306dcc3e5b92f0269f17925e655f5b66f19e
Vladimir Kobenko <vladimirs.kobenko@gmail.com>
2020-07-06 20:41:49 +0300

    lesson3_hometasks_day1-3

---
+++ src/student_vladimir_kobenko/lesson_3/homeworks/day_3/Robot.java
@@ -0,0 +1,14 @@
+package student_vladimir_kobenko.lesson_3.homeworks.day_3;
+
+public class Robot {
+        private String name;
+        public Robot (String name){
+            this.name=name;
+        }
+        public void sayYourName(){
+            System.out.println("My name is " + this.name);
+        }
+        public void sayHello() {
+            System.out.println("Hello!");
+        }
+    }
