48fded183ee9e3c4d7c46397dc898b9a116f5030
Alexey777555 <eiroartlv@inbox.lv>
2020-07-07 21:12:00 +0300

    lesson_4, level_x Super_Task_1

---
+++ src/student_alexey_tretyakov/lesson_4/level_x/Super_Task_1/FizzBuzz.java
@@ -0,0 +1,15 @@
+package student_alexey_tretyakov.lesson_4.level_x.Super_Task_1;
+
+public class FizzBuzz {
+
+    public void FizzBuzz(){}
+    public String detect ( int number) {
+        if (((number % 3) + (number % 5)) == 0) return "FizzBuzz";
+        else {
+            if ((number % 3) == 0) return "Fizz";
+            if ((number % 5) == 0) return "Buzz";
+            else return ("" + number);
+        }
+
+    }
+}
---
+++ src/student_alexey_tretyakov/lesson_4/level_x/Super_Task_1/FizzBuzzTest.java
@@ -0,0 +1,19 @@
+package student_alexey_tretyakov.lesson_4.level_x.Super_Task_1;
+
+import student_alexey_tretyakov.spec.Tester;
+
+public class FizzBuzzTest {
+
+    public static void main(String[] args) {
+
+        Tester fizzTester = new Tester();
+        FizzBuzz testFizz = new FizzBuzz();
+        fizzTester.testResult( ( testFizz.detect (18) ).equals("Fizz"), "Число кратно 3.");
+        fizzTester.testResult( ( testFizz.detect (25) ).equals("Buzz"), "Число кратно 5.");
+        fizzTester.testResult( ( testFizz.detect (45) ).equals("FizzBuzz"), "Число кратно 3 и 5.");
+        fizzTester.testResult( ( testFizz.detect (19) ).equals("" + 19 ), "Число HE кратно 3 или 5.");
+
+
+    }
+}
+
