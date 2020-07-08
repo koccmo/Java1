71c61ceb21ab1da9bbe19ac7ee4759d2ade30278
Igors <igors.barkevics@gmail.com>
2020-07-08 20:44:53 +0300

    Lesson 5 Level 1

---
+++ src/student_igors_bartkevics/lesson_5/homework/level_1/task_1/ArrayDefinition1.java
@@ -0,0 +1,22 @@
+/*
+Найдите ошибку в программе и исправить её:
+
+class ArrayDefinition1 {
+
+    public static void main(String[] args) {
+        int numbers = new int[];
+    }
+
+}
+ */
+package student_igors_bartkevics.lesson_5.homework.level_1.task_1;
+
+class ArrayDefinition1 {
+
+    public static void main(String[] args) {
+        // int[] numbers; or:
+        int[] numbers = new int[10];
+
+    }
+
+}
