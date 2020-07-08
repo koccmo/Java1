3a63fa17241772b3e8d4a43eb1e40e5060d9c975
scdk-web <s.chernobrivec@gmail.com>
2020-07-08 20:43:38 +0300

    Lesson5.Task_4

---
+++ src/student_svjatoslavs_cernobrivecs/lesson_5/level_1/ArrayDefinition4.java
@@ -0,0 +1,8 @@
+package student_svjatoslavs_cernobrivecs.lesson_5.level_1;
+
+public class ArrayDefinition4 {
+
+    public static void main(String[] args){
+        int[] numbers = {1, 2, 3,};
+    }
+}
