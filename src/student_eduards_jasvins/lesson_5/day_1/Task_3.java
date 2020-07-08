76fbb7277c20f2c12db180f9442968855b880bce
EdwardBisq1 <eduards.jasvins@gmail.com>
2020-07-08 22:32:06 +0300

    Homework

---
+++ src/student_eduards_jasvins/lesson_5/day_1/Task_3.java
@@ -0,0 +1,17 @@
+package student_eduards_jasvins.lesson_5.day_1;
+
+/*
+class ArrayDefinition3 {
+
+    public static void main(String[] args) {
+        boolean numbers = new int[10];
+    }
+
+}
+*/
+
+public class Task_3 {
+    public static void main(String[] args) {
+        boolean[] numbers = new boolean[10];
+    }
+}
---
+++ src/student_eduards_jasvins/lesson_5/day_3/ForLoopDefinition5.java
@@ -0,0 +1,22 @@
+package student_eduards_jasvins.lesson_5.day_3;
+/*
+Найдите логическую ошибку в программе и исправить её:
+
+class ForLoopDefinition5 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++ {
+            System.out.println(i);
+        }
+    }
+
+}
+ */
+
+public class ForLoopDefinition5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
