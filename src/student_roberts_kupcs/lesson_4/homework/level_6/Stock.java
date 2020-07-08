c20fbb74814226ce409d910e0c6e9c3af1a91b7e
robku <rkarrow2@gmail.com>
2020-07-07 23:05:24 -0700

    Lesson 4 level 5 create LightColorDetector.java LightColorDetectorTest.java Task_16

---
+++ src/student_roberts_kupcs/lesson_4/homework/level_6/Stock.java
@@ -0,0 +1,18 @@
+package student_roberts_kupcs.lesson_4.homework.level_6;
+
+class Stock {
+    private String nameOfCompany;
+    private String currentValue;
+    private String minimumCost;
+    private String maximumCost;
+
+    //Методы:
+    //- Обновить текущую стоимость акции updatePrice()
+    //- Распечатать информацию об акции printInformation()
+    //Имя компании и начальную стоимость необходимо
+    //задавать в момент создания акции.
+    //Текущая, минимальная и максимальная стоимость
+    //должны меняться только через метод updatePrice().
+
+
+}
