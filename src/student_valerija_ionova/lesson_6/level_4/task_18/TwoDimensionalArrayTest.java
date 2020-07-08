586a52a1432d9f56808652b45597452e77ae9dff
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-07-08 23:22:14 +0300

    Homework Lesson 6 Task -27

---
+++ src/student_valerija_ionova/lesson_6/level_4/task_18/TwoDimensionalArrayTest.java
@@ -0,0 +1,25 @@
+package student_valerija_ionova.lesson_6.level_4.task_18;
+
+/*Создать класс TwoDimensionalArrayTest и написать
+автоматические тесты для метода подсчёта суммы всех
+чисел в массиве.*/
+
+
+public class TwoDimensionalArrayTest {
+    public static void main(String[] args){
+        TwoDimensionalArrayTest arrayTest = new TwoDimensionalArrayTest();
+        arrayTest.testSumOfTwoDimensionalArray();
+    }
+
+    public void testSumOfTwoDimensionalArray(){
+        TwoDimensionalArray new2Array = new TwoDimensionalArray();
+        int [] [] arrayForTest = { { 1, 2 , 0, 4, -6, 9}, { 3, 4, 6, 3, 33, 7 }, { 3, 4, 6, 3, 33, 7 },
+                { 3, 4, 6, 3, 33, 7 }, { 3, 4, 6, 3, 33, 7 } };
+        if (new2Array.sumOfTwoDimensionalArray(arrayForTest) == 234){
+            System.out.println("test testSumOfTwoDimensionalArray OK");
+        }else{
+            System.out.println("test testSumOfTwoDimensionalArray FAIL");
+        }
+
+    }
+}
