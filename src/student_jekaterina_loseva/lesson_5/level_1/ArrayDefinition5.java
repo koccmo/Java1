f7ec89ada0bf85576d1b566c2455ddd9ca6c496b
losev <missteryja@gmail.com>
2020-07-09 22:36:35 +0300

    lesson_5, level_1

---
+++ src/student_jekaterina_loseva/lesson_5/level_1/ArrayDefinition5.java
@@ -0,0 +1,9 @@
+package student_jekaterina_loseva.lesson_5.level_1;
+
+public class ArrayDefinition5 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+    }
+
+}
