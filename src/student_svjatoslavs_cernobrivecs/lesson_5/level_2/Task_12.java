7186c01c53eec7b3e72b4c14c2ec925c3b2a9aff
scdk-web <s.chernobrivec@gmail.com>
2020-07-09 18:56:02 +0300

    Lesson5.Task_12

--- src/student_svjatoslavs_cernobrivecs/lesson_5/level_2/Task_12.java
+++ src/student_svjatoslavs_cernobrivecs/lesson_5/level_2/Task_12.java
@@ -3,7 +3,9 @@ package student_svjatoslavs_cernobrivecs.lesson_5.level_2;
 import java.util.Random;
 
 public class Task_12 {
+
     public static void main(String[] args){
+
         int[] numbers = new int[3];
 
         Random random = new Random();
