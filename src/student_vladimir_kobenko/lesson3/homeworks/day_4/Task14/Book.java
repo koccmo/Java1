221544f759e5ea4465fd62b0190e76c63dafa80e
Vladimir Kobenko <vladimirs.kobenko@gmail.com>
2020-07-10 18:49:09 +0300

    Lesson3_Rename folder

---
+++ src/student_vladimir_kobenko/lesson3/homeworks/day_4/Task14/Book.java
@@ -0,0 +1,13 @@
+package student_vladimir_kobenko.lesson3.homeworks.day_4.Task14;
+
+public class Book {
+        private String title;
+
+        Book(String bookTitle) {
+        this.title = bookTitle;        }
+
+        public String getTitle() {
+        return this.title;
+        }
+
+    }
