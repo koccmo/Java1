f03aa63c81f78da1bbb98c4c0a1242de2488724a
scdk-web <s.chernobrivec@gmail.com>
2020-07-11 01:01:01 +0300

    Lesson5.Task_16

---
+++ src/student_svjatoslavs_cernobrivecs/lesson_5/level_3/ForLoopDefinition1.java
@@ -0,0 +1,10 @@
+package student_svjatoslavs_cernobrivecs.lesson_5.level_3;
+
+public class ForLoopDefinition1 {
+
+    public static void main(String[] args){
+        for (int i = 1; i < 10; i++){
+            System.out.println(i);
+        }
+    }
+}
