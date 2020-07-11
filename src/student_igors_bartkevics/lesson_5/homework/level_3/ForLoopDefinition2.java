4db3c507357a5be1cb1dd5a94e0878f1395911ca
Igors <igors.barkevics@gmail.com>
2020-07-11 11:10:41 +0300

    Lesson 5 Level 3

---
+++ src/student_igors_bartkevics/lesson_5/homework/level_3/ForLoopDefinition2.java
@@ -0,0 +1,22 @@
+/*
+Найдите ошибку в программе и исправить её:
+
+class ForLoopDefinition2 {
+
+    public static void main(String[] args) {
+        for (int i = 0 i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
+*/
+package student_igors_bartkevics.lesson_5.homework.level_3;
+public class ForLoopDefinition2 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/student_igors_bartkevics/lesson_5/homework/level_3/ForLoopDefinition9.java
@@ -0,0 +1,24 @@
+/*
+Исправьте код таким образом, что бы программа
+выводила на консоль числа от 0 до 100:
+
+class ForLoopDefinition8 {
+
+    public static void main(String[] args) {
+        for ( ; ; ) {
+            System.out.println(i);
+        }
+    }
+
+}
+*/
+package student_igors_bartkevics.lesson_5.homework.level_3;
+class ForLoopDefinition9 {
+
+    public static void main(String[] args) {
+        for (int i = 0 ; i < 100 ; i++ ) {
+            System.out.println(i);
+        }
+    }
+
+}
