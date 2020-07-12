60873dd1453cffb70608948ff614c40562fae1e2
Vladimir Kobenko <vladimirs.kobenko@gmail.com>
2020-07-12 23:17:40 +0300

    Lesson5_level_1_homeworks

---
+++ src/student_vladimir_kobenko/lesson5/homeworls/level_1/ArrayDefinition1.java
@@ -0,0 +1,9 @@
+package student_vladimir_kobenko.lesson5.homeworls.level_1;
+
+public class ArrayDefinition1 {
+    public static void main(String[] args) {
+            int [] numbers = new int[5];
+        }
+
+    }
+
---
+++ src/student_vladimir_kobenko/lesson5/homeworls/level_1/ArrayDefinition4.java
@@ -0,0 +1,7 @@
+package student_vladimir_kobenko.lesson5.homeworls.level_1;
+
+public class ArrayDefinition4 {
+    public static void main(String[] args) {
+        int [] numbers = {1, 2, 3};
+    }
+}
