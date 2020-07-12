d87cb2e8432259ae66e93241b4f711c42542ef60
Igors <igors.barkevics@gmail.com>
2020-07-13 00:59:38 +0300

    Lesson 6 Level 4 Task_15

---
+++ src/student_igors_bartkevics/lesson_6/homework/level_4/Task_15.java
@@ -0,0 +1,27 @@
+/*
+Напишите бесконечный цикл при помощи while и for.
+ */
+package student_igors_bartkevics.lesson_6.homework.level_4;
+
+public class Task_15 {
+
+    public static void main(String[] args) {
+
+        //Endless for loop example
+        for (int i = 0; i > -1; i++) {
+            System.out.println(i);
+        }
+
+
+        //Endless while loop example
+        int i = 0;
+        while (i > -1) {
+            System.out.println("Endless loop");
+            i++;
+        }
+    }
+
+
+
+
+}
