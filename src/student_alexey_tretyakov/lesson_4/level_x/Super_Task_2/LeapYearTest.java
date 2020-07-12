c782bc2ee57c18095184115ef1bfe9160e277223
Alexey777555 <eiroartlv@inbox.lv>
2020-07-12 09:22:35 +0300

    lesson_4, level_x, Super_task_1_2

--- src/student_alexey_tretyakov/lesson_4/level_x/Super_Task_2/LeapYearTest.java
+++ src/student_alexey_tretyakov/lesson_4/level_x/Super_Task_2/LeapYearTest.java
@@ -1,13 +1,20 @@
 package student_alexey_tretyakov.lesson_4.level_x.Super_Task_2;
-/*import student_alexey_tretyakov.spec.Tester;
+
 
 public class LeapYearTest {
     public static void main(String[] args) {
         LeapYear testLeapYear = new LeapYear();
-        Tester testerLeapYear = new Tester();
-        testerLeapYear.testResult( testLeapYear.isLeapYear( 2020 ),"Высокосный год." );
-        testerLeapYear.testResult( !testLeapYear.isLeapYear( 2019 ),"Не высокосный год." );
-        testerLeapYear.testResult( testLeapYear.isLeapYear( 2000 ),"Высокосный год ( столетний ) ." );
+
+        LeapYearTest.testResult( testLeapYear.isLeapYear( 2020 ),"Высокосный год." );
+        LeapYearTest.testResult( !testLeapYear.isLeapYear( 2019 ),"Не высокосный год." );
+        LeapYearTest.testResult( testLeapYear.isLeapYear( 2000 ),"Высокосный год ( столетний ) ." );
+    }
+    private static void testResult(boolean status, String testingString) {
+        if (status) {
+            System.out.println( testingString + " Test OK.");
+        } else {
+            System.out.println( testingString + " Test FAIL!!!");
+        }
     }
 
-}*/
+}
