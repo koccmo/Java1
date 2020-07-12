b3f751e32dea8b62223201f379bf119872d89cfc
Igors <igors.barkevics@gmail.com>
2020-07-12 17:17:56 +0300

    Lesson 6 Level 3 Task_12

--- src/student_igors_bartkevics/lesson_6/homework/level_3/task_12/ArrayUtilTest.java
+++ src/student_igors_bartkevics/lesson_6/homework/level_3/task_12/ArrayUtilTest.java
@@ -1,8 +1,8 @@
 /*
-Добавьте в класс ArrayUtil метод для замены первого вхождения указанного числа в массиве целых чисел на другое число.
+Добавьте в класс ArrayUtil метод для замены вхождений указанного числа в массиве целых чисел на другое число.
 Напишите автоматические тесты в классе ArrayUtilTest доказывающие, что ваша реализацяия метода работает правильно.
 
-Подсказка: void replace(int[] arr, int numberToReplace, int newNumber).
+Подсказка: void replaceAll(int[] arr, int numberToReplace, int newNumber).
  */
 package student_igors_bartkevics.lesson_6.homework.level_3.task_12;
 
