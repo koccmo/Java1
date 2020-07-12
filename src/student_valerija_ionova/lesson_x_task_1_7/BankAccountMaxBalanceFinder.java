0cf4d1ea89c1ac692bc8e717d8836931c707ad6f
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-07-12 09:39:04 +0300

    Homework Lesson x

---
+++ src/student_valerija_ionova/lesson_x_task_1_7/BankAccountMaxBalanceFinder.java
@@ -0,0 +1,44 @@
+package student_valerija_ionova.lesson_x_task_1_7;
+
+/*Решаем задачу: какая наибольшая сумма денег была у пользователя
+на счету за всё время (наибольший текущий баланс)?
+
+Для выполнения этого расчёта создайте новый классе BankAccountMaxBalanceFinder.
+
+class BankAccountMaxBalanceFinder {
+
+    public int find(BankAccount bankAccount) {
+        // реализуйте данный метод, за основу возьмите способ расчёта
+        // текущего баланса описанный в предыдущих заданиях
+        // и адаптируйте его под нахождение максимального занчения баланса.
+    }
+
+}*/
+
+public class BankAccountMaxBalanceFinder {
+
+    public int find(BankAccount bankAccount) {
+        Transaction [] transactions = bankAccount.getTransactions();
+        int maxBalance;
+        if (transactions[0].isDeposit()) {
+            maxBalance = transactions[0].getAmount();
+        }else{
+            maxBalance = - transactions[0].getAmount();
+        }
+        int currentBalance = maxBalance;
+        for (int i = 1; i<transactions.length; i++){
+            if (transactions[i].isDeposit()) {
+                currentBalance += transactions[i].getAmount();
+            }else{
+                currentBalance -= transactions[i].getAmount();
+            }
+            if (currentBalance > maxBalance){
+                maxBalance = currentBalance;
+            }
+        }
+
+
+        return maxBalance;
+    }
+
+}
