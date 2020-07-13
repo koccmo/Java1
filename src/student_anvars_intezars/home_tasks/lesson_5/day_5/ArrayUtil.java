2528907b7141939934561ef18c9b4be604f663cf
Anvar331 <anvar.intezar@gmail.com>
2020-07-13 14:31:30 +0300

    Lesson5/Day5/Task33,34

--- src/student_anvars_intezars/home_tasks/lesson_5/day_5/ArrayUtil.java
+++ src/student_anvars_intezars/home_tasks/lesson_5/day_5/ArrayUtil.java
@@ -1,8 +1,18 @@
 package student_anvars_intezars.home_tasks.lesson_5.day_5;
 
+import java.util.Random;
+
 public class ArrayUtil {
 
     public int[] createArray(int arrayLength) {
         return new int[arrayLength];
+    }
+
+    public void fillArrayWithRandomNumbers(int[] array) {
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            int randomNumber = random.nextInt(10);
+            array[i] = randomNumber;
+        }
     }
 }
--- src/student_anvars_intezars/home_tasks/lesson_5/day_5/ArrayUtilTest.java
+++ src/student_anvars_intezars/home_tasks/lesson_5/day_5/ArrayUtilTest.java
@@ -1,22 +1,33 @@
 package student_anvars_intezars.home_tasks.lesson_5.day_5;
 
+import java.util.Arrays;
+
 public class ArrayUtilTest {
 
     public static void main(String[] args) {
 
         ArrayUtilTest test = new ArrayUtilTest();
         test.shouldCreateArray();
+        test.shouldFillArrayWithRandomNumbers();
     }
 
     public void shouldCreateArray() {
         ArrayUtil arrayUtil = new ArrayUtil();
         int[] firstArray = arrayUtil.createArray(5);
-        if(firstArray.length ==5) {
+        if(firstArray.length == 5) {
             System.out.println("Create Array Test = OK");
         }
         else {
             System.out.println("Create Array Test = FAIL");
         }
+    }
+    public void shouldFillArrayWithRandomNumbers() {
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int[] firstArray = arrayUtil.createArray(5);
+        arrayUtil.fillArrayWithRandomNumbers(firstArray);
+        System.out.println(Arrays.toString(firstArray));
+        // Буду еще думать над тестом. Сам метод работает. Пока не разобрался как лучше
+        //затестить.
     }
 }
 
