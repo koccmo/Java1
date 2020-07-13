f016b2cdd05e840984bbf9615680bd03227be40a
Igors <igors.barkevics@gmail.com>
2020-07-14 00:15:11 +0300

    Lesson 6 Level 5 Task_20

--- src/student_igors_bartkevics/lesson_6/homework/level_5/task_20/TicTacToeTest.java
+++ src/student_igors_bartkevics/lesson_6/homework/level_5/task_20/TicTacToeTest.java
@@ -30,17 +30,42 @@ public class TicTacToeTest {
     TicTacToe ticTacToe = new TicTacToe();
 
     public void winPositionForVerticals() {
-        int[][] field = {    {1,  1, 0},
-                             {0,  1, 0},
-                             {1, -1, 0}};
-        checkResult(ticTacToe.isWinPositionForVerticals(field, 0), "Win position for verticals");
+        int[][] field1 = {    {0,  1, -1},
+                              {0,  1,  1},
+                              {0, -1, -1}};
+
+        int[][] field2 = {    {1,   0, -1},
+                              {1,   0,  1},
+                              {-1,  0,  1}};
+
+        int[][] field3 = {    {1,  1, 0},
+                              {0,  1, 0},
+                              {1, -1, 0}};
+
+        boolean condition = ticTacToe.isWinPositionForVerticals(field1, 0) &&
+                            ticTacToe.isWinPositionForVerticals(field2, 0) &&
+                            ticTacToe.isWinPositionForVerticals(field3, 0);
+        checkResult(condition, "Win position for verticals");
     }
 
     public void winPositionForHorizontals() {
-        int[][] field = {   {1,  0, 1},
-                            {0,  0, 0},
-                            {1, -1, 1}};
-        checkResult(ticTacToe.isWinPositionForHorizontals(field, 0), "Win position for horizontals");
+        int[][] field1 = {   { 1,  1,  1},
+                             {-1,  0,  0},
+                             { 0, -1, -1}};
+
+        int[][] field2 = {   { 0,  0, 1},
+                             { 1,  1, 1},
+                             {-1, -1, 0}};
+
+        int[][] field3 = {   {-1,  0, -1},
+                             { 0, -1,  0},
+                             { 1,  1,  1}};
+
+        boolean condition = ticTacToe.isWinPositionForHorizontals(field1, 1) &&
+                            ticTacToe.isWinPositionForHorizontals(field2, 1) &&
+                            ticTacToe.isWinPositionForHorizontals(field3, 1);
+
+        checkResult((condition), "Win position for horizontals");
     }
 
     public void checkResult(boolean condition, String testName) {
