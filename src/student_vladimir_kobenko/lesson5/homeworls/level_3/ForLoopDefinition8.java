67602ab0276417e8618198a10ed07b62e1b8d458
Vladimir Kobenko <vladimirs.kobenko@gmail.com>
2020-07-13 19:50:32 +0300

    Lesson5_level_3_homeworks

---
+++ src/student_vladimir_kobenko/lesson5/homeworls/level_3/ForLoopDefinition8.java
@@ -0,0 +1,9 @@
+package student_vladimir_kobenko.lesson5.homeworls.level_3;
+
+public class ForLoopDefinition8 {
+    public static void main(String[] args) {
+        for ( int i=0; i<101 ; i++ ) {
+            System.out.println(i);
+        }
+    }
+}
