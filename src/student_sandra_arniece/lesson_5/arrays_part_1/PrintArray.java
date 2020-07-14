5f46d1f283aa9d8d1503eb9b04b2273a6f988bf8
S4n6r4 <s4ndr41f@gmail.com>
2020-07-14 18:56:59 +0300

    Lesson_5(part_1)

---
+++ src/student_sandra_arniece/lesson_5/arrays_part_1/PrintArray.java
@@ -0,0 +1,11 @@
+package student_sandra_arniece.lesson_5.arrays_part_1;
+//Task 10
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        System.out.println(java.util.Arrays.toString(numbers));
+    }
+}
