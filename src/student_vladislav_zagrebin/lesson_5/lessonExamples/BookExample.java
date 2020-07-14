51a426031e7ecbb56e7e2fcc2c7a5d1ecec6241e
VladZ <vladislavzagrebin@gmail.com>
2020-07-14 14:38:20 +0300

    HomeWork

---
+++ src/student_vladislav_zagrebin/lesson_5/lessonExamples/BookExample.java
@@ -0,0 +1,16 @@
+package student_vladislav_zagrebin.lesson_5.lessonExamples;
+
+public class BookExample {
+
+    private String author;
+    private String title;
+
+    public BookExample(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+
+
+
+}
