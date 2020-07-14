40a27f48f7c575bda98fb7be2a6547acb77ad5bf
vsavonins <viktors.savonins@proofit.lv>
2020-07-14 20:09:14 +0300

    Lesson_6 code

--- src/teacher/lesson_6/lessoncode/DoubleForExample.java
+++ src/teacher/lesson_6/lessoncode/DoubleForExample.java
@@ -5,8 +5,13 @@ class DoubleForExample {
 	public static void main(String[] args) {
 		//int[][] arr = new int[5][5];
 
-		int[][] arr = {{0, 1, 7},
-				       {4, 5, 9},
+		int[] withData = {1, 2, 3, 4};
+		String[] withDataStr = {"A", "B", "C"};
+
+		int[][] twoDimentionalArray = new int[2][5];
+
+		int[][] arr = {{0, 1, 7},        //{1,2,3}, {1,2,2}, {1,2,3}
+				       {4, 5, 9, 7, 8, 9},
 					   {7, 8, 0}};
 		for(int i = 0; i < arr.length; i++) {
 			for(int j = 0; j < arr[i].length; j++) {
@@ -14,6 +19,8 @@ class DoubleForExample {
 				System.out.println("[" + i + "," + j + "] = " + number);
 			}
 		}
+
+		arr[0][0] = 555;
 	}
 
 }
