6a04e9a8e20249f4b4ea751a6a1e1943121293d3
binco <vladimirs.danilins@gmail.com>
2020-07-16 19:39:12 +0300

    homeworks

---
+++ src/student_volodya_danilin/lesson_5/level_1/ArrayDefinition1.java
@@ -0,0 +1,8 @@
+package student_volodya_danilin.lesson_5.level_1;
+
+class ArrayDefinition1 {
+
+    public static void main(String[] Args) {
+        int[] numbers = {};
+    }
+}
---
+++ src/student_volodya_danilin/lesson_5/level_1/ArrayLength1.java
@@ -0,0 +1,9 @@
+package student_volodya_danilin.lesson_5.level_1;
+
+class ArrayLength1 {
+
+    public static void main(String[] Args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers[1]);
+    }
+}
