b20738a8bfc82fa53da180023dfc187b73c79fc7
Igors <igors.barkevics@gmail.com>
2020-07-20 01:17:17 +0300

    Lesson X bank_account Task_8

---
+++ src/student_igors_bartkevics/lesson_x/bank_account/part_1_basic/BankAccountCurrentBalanceCalculatorTest.java
@@ -0,0 +1,27 @@
+package student_igors_bartkevics.lesson_x.bank_account.part_1_basic;
+
+class BankAccountCurrentBalanceCalculatorTest {
+
+    public static void main(String[] args) {
+        BankAccountCurrentBalanceCalculatorTest test = new BankAccountCurrentBalanceCalculatorTest();
+        test.currentAmountCalculation();
+    }
+
+    public void currentAmountCalculation() {
+        Transaction[] transactions = new Transaction[3];
+        transactions[0] = (new Transaction(3000, TransactionType.DEPOSIT));
+        transactions[1] = (new Transaction(2500, TransactionType.DEPOSIT));
+        transactions[2] = (new Transaction(3500, TransactionType.WITHDRAWAL));
+        BankAccountCurrentBalanceCalculator calculator = new BankAccountCurrentBalanceCalculator();
+        BankAccount bankAccount = new BankAccount("Name Surname", transactions);
+        checkResult(calculator.calculate(bankAccount) == 2000, "Current amount calculation");
+    }
+
+    public void checkResult(boolean condition, String testName) {
+        if (condition) {
+            System.out.println(testName + " test = OK");
+        } else {
+            System.out.println(testName + " test = FAIL");
+        }
+    }
+}
