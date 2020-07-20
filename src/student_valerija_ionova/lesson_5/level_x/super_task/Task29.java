39c2729c992ca0f74c95f9cb7af40e35146d0fcc
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-07-20 10:01:11 +0300

    Homework Lesson 5 Level x Super Task1

---
+++ src/student_valerija_ionova/lesson_5/level_x/super_task/Task29.java
@@ -0,0 +1,22 @@
+package student_valerija_ionova.lesson_5.level_x.super_task;
+
+/*Напишите программу, в которой:
+- создайте массив произвольной длины
+- заполните массив случайными числами
+- распечатайте на консоль все элементы массива
+- найдите все чётные числа в массиве и выведети их на консоль.*/
+
+public class Task29 {
+
+    public static void main (String[] args){
+        ArrayUtil arrayUtil = new ArrayUtil();
+
+        int [] newArray = arrayUtil.newArrayWithRandomLength();
+        int [] filledArray = arrayUtil.fillArrayWithRandomNumbers(newArray);
+
+        arrayUtil.printArray(filledArray);
+
+        arrayUtil.printEvenNumbersOfArray(filledArray);
+    }
+
+}
