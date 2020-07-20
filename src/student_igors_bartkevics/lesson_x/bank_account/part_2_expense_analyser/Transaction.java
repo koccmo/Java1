dfc9711cfd6d5bcd246e4aaf9a04ebb9bc664318
Igors <igors.barkevics@gmail.com>
2020-07-20 19:50:29 +0300

    Lesson X bank_account Task_18

---
+++ src/student_igors_bartkevics/lesson_x/bank_account/part_2_expense_analyser/Transaction.java
@@ -0,0 +1,118 @@
+/*
+Task_3:
+
+Создайте класс Transaction, который будет представлять из себя
+транзакцию которая совершается с банковским счётом.
+У транзакции должны быть следующие свойства:
+- полное имя владельца транзакции;
+- сумма (можно для простоты представить в центах - тоесть целым числом);
+- тип транзакции (свойство, которое будет хранить значение из enum TransactionType).
+
+class Transaction {
+
+    private int amount;
+    private TransactionType transactionType;
+
+    // создайте конструктор класса с двумя параметрами: аmount, transactionType.
+
+    // создайте геттер метод для свойства amount
+}
+
+Task_4
+
+Продолжаем создание класса Transaction, который будет представлять из себя
+транзакцию которая совершается с банковским счётом.
+
+class Transaction {
+
+    private int amount;
+    private TransactionType transactionType;
+
+    public Transaction(int amount, TransactionType transactionType) {
+        this.amount = amount;
+        this.transactionType = transactionType;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    // Маленький хак как сделать ваши доменные объекты более удобными в использовании.
+    // Со стандартными методами get() и set() не всегда удобно работать.
+    // Иногда мы можем добавить полезное поведение в наши доменные объекты
+    // просто реализовав внутри доменных объектов простые методы.
+
+    // создайте метод isDeposit(), данный метод должен возвращать boolean:
+    // - true если транзакция является депозитом
+    // - false если транзакция не является депозитом
+
+    // создайте метод isWithdrawal(), данный метод должен возвращать boolean:
+    // - true если транзакция является снятием денег со счёта
+    // - false если транзакция не является снятием денег со счёта
+}
+
+Task_18:
+
+Вводим новую функциональность: анализ расходов.
+
+Для проведения анализа расходов необходимо, что бы у каждой
+транзакции было указано к какой категории расходов она относится.
+
+Банк провёл огромную работу и готов предоставить информацию к какой
+категории расходов относится каждая транзакция.
+
+Решаем задачу в лоб. Добавьте новое свойство expenseCategory к классу Transaction.
+
+class Transaction {
+
+    private int amount;
+    private TransactionType transactionType;
+
+    public Transaction(int amount, TransactionType transactionType) {
+        this.amount = amount;
+        this.transactionType = transactionType;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public boolean isDeposit() {
+        return TransactionType.DEPOSIT == this.transactionType;
+    }
+
+    public boolean isWithdrawal() {
+        return TransactionType.WITHDRAWAL == this.transactionType;
+    }
+}
+ */
+package student_igors_bartkevics.lesson_x.bank_account.part_2_expense_analyser;
+
+
+
+class Transaction {
+
+    private int amount;
+    private TransactionType transactionType;
+    private ExpenseCategory expenseCategory;
+
+    // создайте конструктор класса с двумя параметрами: amount, transactionType.
+    Transaction(int amount, TransactionType transactionType) {
+        this.amount = amount;
+        this.transactionType = transactionType;
+    }
+
+    // создайте геттер метод для свойства amount
+    public int getAmount() {
+        return amount;
+    }
+
+    public boolean isDeposit() {
+        return (this.transactionType == TransactionType.DEPOSIT);
+    }
+    public boolean isWithdrawal() {
+        return (this.transactionType == TransactionType.WITHDRAWAL);
+    }
+
+
+}
