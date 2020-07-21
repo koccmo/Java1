07cf93e32b61c90bef794272b2cbee30f032df3b
Anvar331 <anvar.intezar@gmail.com>
2020-07-21 17:34:57 +0300

    Lesson6/Day4/Task18

---
+++ src/student_anvars_intezars/home_tasks/lesson_6/day_4/task_18/TestNew.java
@@ -0,0 +1,13 @@
+package student_anvars_intezars.home_tasks.lesson_6.day_4.task_18;
+
+public class TestNew {
+
+    public static void main(String[] args) {
+
+        TwoDimensionalArray twoDimensionalArray = new TwoDimensionalArray();
+        int[][] array = new int[2][2];
+        twoDimensionalArray.fillTwoDimensionalArrayWithRandomNumbers(array);
+
+
+    }
+}
