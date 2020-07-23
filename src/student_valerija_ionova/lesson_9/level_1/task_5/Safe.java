48d72cbbfa32f962edc6a2e8fdc3d6c611188f66
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-07-23 18:25:05 +0300

    Homework Lesson 9 Level 1-2 Task 1-10

---
+++ src/student_valerija_ionova/lesson_9/level_1/task_5/Safe.java
@@ -0,0 +1,36 @@
+package student_valerija_ionova.lesson_9.level_1.task_5;
+
+/*Создайте класс сейф версии 2 с двумя свойствами: пароль и количество денег в сейфе.
+Начальные значения этим свойствам задавайте через публичный конструктор.
+Сделайте так, что бы достать деньги из сейфа или положить деньги в сейф
+можно было только предоставя правильный пароль.
+Подсказка: реализуйте эту функциональность через публичные методы: getMoney(...), putMoney(...).
+Создайте класс демо и продемонстрируйте, принцып инкапсуляции в действии!*/
+
+public class Safe {
+
+    private int pinCode;
+    private int moneyAmount;
+
+    public Safe (int pinCode, int moneyAmount){
+        this.pinCode = pinCode;
+        this.moneyAmount = moneyAmount;
+    }
+
+    public void getMoney (int pinCode, int amount){
+        if (this.pinCode == pinCode){
+            this.moneyAmount -= amount;
+        }
+    }
+
+    public void putMoney (int pinCode, int amount){
+        if (this.pinCode == pinCode){
+            moneyAmount += amount;
+        }
+    }
+
+    public int getMoneyAmount(){
+        return moneyAmount;
+    }
+
+}
---
+++ src/student_valerija_ionova/lesson_9/level_2/task_9/PrivateConstructorDemo.java
@@ -0,0 +1,13 @@
+package student_valerija_ionova.lesson_9.level_2.task_9;
+
+public class PrivateConstructorDemo {
+
+    public static void main (String [] args){
+
+        //PrivateConstructor constructor = new PrivateConstructor();
+
+
+
+    }
+
+}
