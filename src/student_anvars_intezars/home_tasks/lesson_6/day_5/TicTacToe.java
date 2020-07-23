f7a448cb9d07e4d0942a0460ad87a7efcf865521
Anvar331 <anvar.intezar@gmail.com>
2020-07-23 23:28:32 +0300

    Lesson6/Day5/Task20

--- src/student_anvars_intezars/home_tasks/lesson_6/day_5/TicTacToe.java
+++ src/student_anvars_intezars/home_tasks/lesson_6/day_5/TicTacToe.java
@@ -3,13 +3,21 @@ package student_anvars_intezars.home_tasks.lesson_6.day_5;
 public class TicTacToe {
 
    public boolean isWinPositionForHorizontals(int[][] field, int playerToCheck) {
-        for (int i = 0; i < field.length; i++) {
+        for (int i = 0; i < 3; i++) {
             if ((field[i][0] == playerToCheck) && (field[i][1] == playerToCheck) && (field[i][2] == playerToCheck)) {
-            } else {
-                return false;
+                return true;
             }
         }
-       return true;
+        return false;
+   }
+
+   public boolean isWinPositionForVerticals(int[][] field, int playerToCheck) {
+       for (int i = 0; i < 3; i++) {
+           if ((field[0][i] == playerToCheck) && (field[1][i] == playerToCheck) && (field[2][i] == playerToCheck)) {
+               return true;
+           }
+       }
+       return false;
    }
 }
 
--- src/student_anvars_intezars/home_tasks/lesson_6/day_5/TicTacToeTest.java
+++ src/student_anvars_intezars/home_tasks/lesson_6/day_5/TicTacToeTest.java
@@ -10,13 +10,16 @@ public class TicTacToeTest {
         ticTacToeTest.firstWinPositionHorizontalTest();
         ticTacToeTest.secondWinPositionHorizontalTest();
         ticTacToeTest.thirdWinPositionHorizontalTest();
+        ticTacToeTest.firstWinPositionVerticalTest();
+        ticTacToeTest.secondWinPositionVerticalTest();
+        ticTacToeTest.thirdWinPositionalVerticalTest();
     }
 
     public void firstWinPositionHorizontalTest() {
         TicTacToe ticTacToe = new TicTacToe();
         int[][] field = {{1,1,1}, {0,0,0}, {0,0,0}};
         boolean result = ticTacToe.isWinPositionForHorizontals(field,1);
-        if (result = true) {
+        if (result) {
             System.out.println("Test first line for horizontal win = OK");
         }
         else {
@@ -29,7 +32,7 @@ public class TicTacToeTest {
         TicTacToe ticTacToe = new TicTacToe();
         int[][] field = {{0,0,0}, {1,1,1}, {0,0,0}};
         boolean result = ticTacToe.isWinPositionForHorizontals(field,1);
-        if (result = true) {
+        if (result) {
             System.out.println("Test second line for horizontal win = OK");
         }
         else {
@@ -41,11 +44,47 @@ public class TicTacToeTest {
         TicTacToe ticTacToe = new TicTacToe();
         int[][] field = {{0,0,0}, {0,0,0}, {1,1,1}};
         boolean result = ticTacToe.isWinPositionForHorizontals(field,1);
-        if (result = true) {
+        if (result) {
             System.out.println("Test third line for horizontal win = OK");
         }
         else {
             System.out.println("Test third line for horizontal win = FAIL");
+        }
+    }
+
+    public void firstWinPositionVerticalTest() {
+        TicTacToe ticTacToe = new TicTacToe();
+        int[][] field = {{1,0,0}, {1,0,0}, {1,0,0}};
+        boolean result = ticTacToe.isWinPositionForVerticals(field,1);
+        if (result) {
+            System.out.println("Test first line for vertical win = OK");
+        }
+        else {
+            System.out.println("Test first line for vertical win = FAIL");
+        }
+    }
+
+    public void secondWinPositionVerticalTest() {
+        TicTacToe ticTacToe = new TicTacToe();
+        int[][] field = {{0,1,0}, {0,1,0}, {0,1,0}};
+        boolean result = ticTacToe.isWinPositionForVerticals(field,1);
+        if (result) {
+            System.out.println("Test second line for vertical win = OK");
+        }
+        else {
+            System.out.println("Test second line for vertical win = FAIL");
+        }
+    }
+
+    public void thirdWinPositionalVerticalTest() {
+        TicTacToe ticTacToe = new TicTacToe();
+        int [][] field = {{0,0,1}, {0,0,1}, {0,1,1}};
+        boolean result = ticTacToe.isWinPositionForVerticals(field,1);
+        if (result) {
+            System.out.println("Test for third vertical win = OK");
+        }
+        else {
+            System.out.println("Test for third vertical win = FAIL");
         }
     }
 }
