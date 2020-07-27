4353706dc002f8a164658bd31e66726e0f115d2f
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-07-27 20:26:38 +0300

    Homework Lesson 9 ST2

---
+++ src/student_valerija_ionova/lesson_9/level_x/super_task_2/CreditCardDemo.java
@@ -0,0 +1,15 @@
+package student_valerija_ionova.lesson_9.level_x.super_task_2;
+
+public class CreditCardDemo{
+
+    public static void main (String [] args){
+
+        CreditCard card = new CreditCard.CreditCardBuilder("123456789", 1234)
+                .balance(100)
+                .creditLimit(100)
+                .creditBalance(100).build();
+
+        System.out.println(card.toString());
+    }
+
+}
