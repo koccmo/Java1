7d3553db0ea9c5623597676da02c767847580320
vsavonins <viktors.savonins@proofit.lv>
2020-07-28 16:26:23 +0300

    Lesson 6 code review

--- src/student_alexey_tretyakov/lesson_6/level_3/Task_15.java
+++ src/student_alexey_tretyakov/lesson_6/level_3/Task_15.java
@@ -3,6 +3,7 @@ package student_alexey_tretyakov.lesson_6.level_3;
 public class Task_15 {
 
     public static void main(String[] args) {
+    	// более простой бесконечный цикл for (;;)
         for (int i = 0; i < 3; i++) {
             System.out.println("Бесконечный цикл for ");
             i--;
