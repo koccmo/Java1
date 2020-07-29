98afe90a3872289362a946b2c44ba5f3fbf60aae
Anvar331 <anvar.intezar@gmail.com>
2020-07-29 23:24:08 +0300

    Lesson7/Day1/Task2

--- src/student_anvars_intezars/home_tasks/lesson_7/day_1/WordService.java
+++ src/student_anvars_intezars/home_tasks/lesson_7/day_1/WordService.java
@@ -2,35 +2,42 @@ package student_anvars_intezars.home_tasks.lesson_7.day_1;
 
 import java.util.Arrays;
 
-public class WordService {
+class WordService {
 
     public String findMostFrequentWord(String text) {
+        String[] readyText = getTextLineToArray(text);
+        countWordsInText(readyText);
 
-        // Полученую строку текста разобьем на слова.
-        //Слова запишем в массив, чтобы можно было каждое слово посчитать в дальнейшем.
-        String [] lineOfTextToWords = text.split(" ");
+        return text;
+    }
+
+
+    public String[] getTextLineToArray(String text) {
+        String[] lineOfTextToWords = text.split(" ");
         Arrays.sort(lineOfTextToWords);
+        return lineOfTextToWords;
+    }
 
-            int mostFrequentWord = 0;
-            int count = 0;
-            String word = lineOfTextToWords[0];
-            String current = lineOfTextToWords[0];
-            for (int i = 1; i < lineOfTextToWords.length; i++) {
-                if (lineOfTextToWords[i].equals(current)) {
-                    count++;
-                }
-                else {
-                    count = 1;
-                    current = lineOfTextToWords[i];
-                }
-                if ( mostFrequentWord < count) {
-                    mostFrequentWord = count;
-                    word = lineOfTextToWords[i];
-                }
+    public String countWordsInText(String[] lineOfTextToWords) {
+        int mostFrequentWord = 0;
+        int count = 0;
+        String word = lineOfTextToWords[0];
+        String current = lineOfTextToWords[0];
+        for (int i = 1; i < lineOfTextToWords.length; i++) {
+            if (lineOfTextToWords[i].equals(current)) {
+                count++;
+            } else {
+                count = 1;
+                current = lineOfTextToWords[i];
             }
-                System.out.println("Most frequent word is " + word + ", found " + mostFrequentWord +" times in the text");
+            if (mostFrequentWord < count) {
+                mostFrequentWord = count;
+                word = lineOfTextToWords[i];
+            }
+        }
+        System.out.println("Most frequent word is " + word + ", found " + mostFrequentWord +" times in the text");
         return word;
     }
-    }
 
+}
 
