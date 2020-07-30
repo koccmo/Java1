852933294eab371814de12d0cce70e862f6bf88c
EdwardBisq1 <eduards.jasvins@gmail.com>
2020-07-30 19:37:50 +0300

    Homework

---
+++ src/student_eduards_jasvins/lesson_7/WordServiceTest.java
@@ -0,0 +1,22 @@
+package student_eduards_jasvins.lesson_7;
+
+public class WordServiceTest {
+    public static void main(String[] args) {
+        WordServiceTest wordServiceTest = new WordServiceTest();
+        wordServiceTest.mostFrequentWord();
+    }
+
+    private void mostFrequentWord() {
+        WordService wordService = new WordService();
+        String text = "how much word how i can use in this how text";
+        String result = wordService.findMostFrequentWord(text);
+        if (result.equals("how")) {
+            System.out.println("Test = OK");
+        } else {
+            System.out.println("Test = FAIL");
+        }
+    }
+}
+
+
+
