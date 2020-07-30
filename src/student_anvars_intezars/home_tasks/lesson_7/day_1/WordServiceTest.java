e677dcb5f33f16db129dadfb485fc2b679ae50f5
Anvar331 <anvar.intezar@gmail.com>
2020-07-30 21:24:06 +0300

    Lesson7/Day1/Task2(updated)

--- src/student_anvars_intezars/home_tasks/lesson_7/day_1/WordServiceTest.java
+++ src/student_anvars_intezars/home_tasks/lesson_7/day_1/WordServiceTest.java
@@ -1,14 +1,13 @@
 package student_anvars_intezars.home_tasks.lesson_7.day_1;
 
-import java.util.Arrays;
-
-public class WorldServiceTest {
+public class WordServiceTest {
 
     public static void main(String[] args) {
 
-        WorldServiceTest worldServiceTest = new WorldServiceTest();
-        worldServiceTest.mostFrequentWordInTextTest();
-        worldServiceTest.splitLineOfTextToWordsTest();
+        WordServiceTest wordServiceTest = new WordServiceTest();
+        wordServiceTest.mostFrequentWordInTextTest();
+        wordServiceTest.splitLineOfTextToWordsTest();
+        wordServiceTest.countWordsInTextTest();
     }
 
     public void mostFrequentWordInTextTest() {
@@ -16,7 +15,10 @@ public class WorldServiceTest {
         String text = ("My name is Anvar. My name has muslim roots.");
         String result = wordService.findMostFrequentWord(text);
         if (result.equals("My")) {
-            System.out.println("Tes for finding most frequent word = OK");
+            System.out.println("Test for finding most frequent word = OK");
+        }
+        else {
+            System.out.println("Test for finding most frequent word = FAIL");
         }
     }
 
@@ -27,9 +29,23 @@ public class WorldServiceTest {
         if ((result[0].equals("I")) && (result[1].equals("believe")) && (result[2].equals("in") && result[3].equals("programming"))) {
             //System.out.println(Arrays.toString(result));
             System.out.println("Test for splitting line of text to words = OK");
+            System.out.println();
         }
         else {
             System.out.println("Test for splitting line of text to words = FAIL");
+        }
+    }
+
+    public void countWordsInTextTest() {
+        WordService wordService = new WordService();
+        String text = ("How many text words in this text");
+        String[] result = wordService.getTextLineToArray(text);
+        String mostFrequentWord = wordService.countWordsInText(result);
+        if(mostFrequentWord.equals("text")) {
+            System.out.println("Test for counting words = OK");
+        }
+        else {
+            System.out.println("Test for counting words = FAIL");
         }
     }
 }
