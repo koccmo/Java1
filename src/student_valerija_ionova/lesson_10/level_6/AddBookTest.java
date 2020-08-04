3798fe555503d1b67e9a3bc4e8bacea46aa643a5
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-08-04 22:09:56 +0300

    Homework Lesson 10 Level 6 AddBook

---
+++ src/student_valerija_ionova/lesson_10/level_6/AddBookTest.java
@@ -0,0 +1,50 @@
+package student_valerija_ionova.lesson_10.level_6;
+
+public class AddBookTest {
+
+    public static void main (String [] args){
+
+        AddBookTest addBookTest = new AddBookTest();
+
+        addBookTest.testAddFirstBook();
+        addBookTest.testAddSecondNewBook();
+        addBookTest.testAddSecondOldBook();
+    }
+
+    void printResult(String name, boolean testPassed){
+        if (testPassed){
+            System.out.println(name+" OK");
+        }else{
+            System.out.println(name+" FAIL");
+        }
+    }
+
+    void testAddFirstBook(){
+        BookReaderImpl bookReader = new BookReaderImpl();
+        Book book1 = new Book ("Tree", "Darvin");
+        BookLibrary bookLibrary = new BookLibrary();
+        bookReader.addBook(book1, bookLibrary);
+
+        printResult("testAddFirstBook", bookLibrary.getBookLibrary().get(0).getTitle().equals("Tree"));
+    }
+
+    void testAddSecondNewBook(){
+        BookReaderImpl bookReader = new BookReaderImpl();
+        Book book1 = new Book ("Tree", "Darvin");
+        Book book2 = new Book ("Tree and Monkey", "John Tree");
+        BookLibrary bookLibrary = new BookLibrary(book1);
+
+        printResult("testAddSecondNewBook", bookReader.addBook(book2, bookLibrary));
+    }
+
+    void testAddSecondOldBook(){
+        BookReaderImpl bookReader = new BookReaderImpl();
+        Book book1 = new Book ("Tree", "Darvin");
+        Book book2 = new Book ("Tree and Monkey", "John Tree");
+        Book book3 = new Book ("Tree and Monkey", "John Tree");
+        BookLibrary bookLibrary = new BookLibrary(book1, book2);
+
+        printResult("testAddSecondOldBook", !bookReader.addBook(book3, bookLibrary));
+    }
+
+}
