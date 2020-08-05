8a11cc041a9fb61b5be4bafdb5817f9f5bc71cf5
pavels-s <pavels.sarkels@gmail.com>
2020-08-05 20:22:09 +0300

    Lesson 7, level 6, task 10 homework

--- src/student_pavel_sharkel/lesson_7/level_6/task_10/PowerCalculatorTest.java
+++ src/student_pavel_sharkel/lesson_7/level_6/task_10/PowerCalculatorTest.java
@@ -6,16 +6,27 @@ class PowerCalculatorTest {
 
         PowerCalculatorTest powerCalculatorTest = new PowerCalculatorTest();
         powerCalculatorTest.powerTest();
+        powerCalculatorTest.powerTest2();
 
     }
         void powerTest() {
             PowerCalculator powerCalculator = new PowerCalculator();
 
             if (powerCalculator.intoPower(4, 3) == 64) {
-                System.out.println("PowerCalculator test is OK");
+                System.out.println("PowerCalculator 4^3 test is OK");
             } else {
-                System.out.println("PowerCalculator test is FAILED");
+                System.out.println("PowerCalculator 4^3 test is FAILED");
             }
         }
+
+    void powerTest2() {
+        PowerCalculator powerCalculator = new PowerCalculator();
+
+        if (powerCalculator.intoPower(3, 5) == 243) {
+            System.out.println("PowerCalculator 3^5 test is OK");
+        } else {
+            System.out.println("PowerCalculator 3^5 test is FAILED");
+        }
+    }
 
 }
