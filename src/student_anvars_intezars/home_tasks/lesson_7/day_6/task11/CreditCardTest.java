e401722ca5188a7038cb4ebef45e79ce230bdc64
Anvar331 <anvar.intezar@gmail.com>
2020-08-05 20:22:06 +0300

    Lesson7/Day6/Task11(updated)

--- src/student_anvars_intezars/home_tasks/lesson_7/day_6/task11/CreditCardTest.java
+++ src/student_anvars_intezars/home_tasks/lesson_7/day_6/task11/CreditCardTest.java
@@ -1,11 +1,14 @@
 package student_anvars_intezars.home_tasks.lesson_7.day_6.task11;
 
+import java.sql.SQLOutput;
+
 class CreditCardTest {
 
     public static void main(String[] args) {
 
         CreditCardTest creditCardTest = new CreditCardTest();
         creditCardTest.testForCreditCardWithdraw();
+        creditCardTest.testForCreditCardDeposit();
     }
 
     public void testForCreditCardWithdraw() {
@@ -13,9 +16,19 @@ class CreditCardTest {
         int withdrawResult = creditCard.withdraw(8978, 100);
         if (withdrawResult == 900) {
             System.out.println("Test for withdraw = OK");
-        }
-        else {
+            System.out.println();
+        }else {
             System.out.println("Test for withdraw = FAIL");
+        }
+    }
+
+    public void testForCreditCardDeposit() {
+        CreditCard creditCard = new CreditCard(77335591,8978);
+        int depositResult = creditCard.deposit(8978,15);
+        if (depositResult == 15) { System.out.println();
+            System.out.println("Test for deposit = OK");
+        } else {
+            System.out.println("Test for deposit = FAIL");
         }
     }
 }
