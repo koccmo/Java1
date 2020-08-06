9d63d56c895025a8f15c7af52e51ba077c0ae601
Igors <igors.barkevics@gmail.com>
2020-08-06 23:16:41 +0300

    Lesson 8 Level 4 Task_17

---
+++ src/student_igors_bartkevics/lesson_8/homework/level_4/SquareTest.java
@@ -0,0 +1,28 @@
+package student_igors_bartkevics.lesson_8.homework.level_4;
+
+class SquareTest {
+
+    Square square = new Square("Square", 5.0);
+
+    public static void main(String[] args) {
+        SquareTest test = new SquareTest();
+        test.calculateAreaTest();
+        test.calculatePerimeterTest();
+    }
+
+    void calculateAreaTest() {
+        checkResult(square.calculateArea() == 25.0, "Calculate area");
+    }
+
+    void calculatePerimeterTest() {
+        checkResult(square.calculatePerimeter() == 20.0, "Calculate perimeter");
+    }
+
+    public void checkResult(boolean condition, String testName) {
+        if (condition) {
+            System.out.println(testName + " test = OK");
+        } else {
+            System.out.println(testName + " test = FAIL");
+        }
+    }
+}
