dd8beee236ddd6190dfd60c83c6a47a7e9767c82
Igors <igors.barkevics@gmail.com>
2020-08-06 22:50:47 +0300

    Lesson 8 Level 4 Task_15

---
+++ src/student_igors_bartkevics/lesson_8/homework/level_4/Shape.java
@@ -0,0 +1,36 @@
+/*
+Task_15:
+
+Создание иерархии класса Shape (геометрическая фигура).
+
+Дан класс Shape:
+
+abstract class Shape {
+
+    private String title;
+
+    Shape(String title) {
+        this.title = title;
+    }
+
+    abstract double calculateArea();
+
+}
+
+Объявите в классе Shape абстрактный метод
+для подсчёта периметра.
+ */
+package student_igors_bartkevics.lesson_8.homework.level_4;
+
+abstract class Shape {
+
+    private String title;
+
+    Shape(String title) {
+        this.title = title;
+    }
+
+    abstract double calculateArea();
+
+    abstract double calculatePerimeter();
+}
