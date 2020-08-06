d95b66c7f176f0c97a0ebe2cf52750bac9066ca9
Jaroslav <brutan.jaroslav@gmail.com>
2020-08-06 09:41:57 +0300

    Lesson 7 palindrome class

---
+++ src/student_jaroslav_brutan/lesson_7/day_6/Palindrome.java
@@ -0,0 +1,24 @@
+package student_jaroslav_brutan.lesson_7.day_6;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+    }
+    boolean isPalindrome(String text){
+        String mainText = text.toLowerCase().replaceAll("[^A-Za-z^]", "");
+        StringBuffer palText = new StringBuffer(mainText).reverse();
+        boolean result = true;
+        for (int i = 0; i < mainText.length();i++){
+            if (mainText.charAt(i) != palText.charAt(i)){
+                result = false;
+                System.out.println("Word is not a palindrome!");
+                break;
+            }
+        }
+        System.out.println("Word is a palindrome!");
+        return result;
+
+
+    }
+}
