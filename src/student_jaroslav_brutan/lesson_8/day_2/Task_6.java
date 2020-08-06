1d8d60225178b09c672f604f7fe9f3f1db1093ad
Jaroslav <brutan.jaroslav@gmail.com>
2020-08-06 23:50:00 +0300

    Lesson 8 start

---
+++ src/student_jaroslav_brutan/lesson_8/day_2/Task_6.java
@@ -0,0 +1,17 @@
+package student_jaroslav_brutan.lesson_8.day_2;
+
+public class Task_6 {
+
+    class Mother {
+
+    }
+
+    class Father {
+
+    }
+
+    class Child extends Mother {
+        // может наследовать или класс Father или Mother
+
+    }
+}
