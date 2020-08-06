33989a4ef04c7af6cac633fcf7cbe7a408e7ac47
Igors <igors.barkevics@gmail.com>
2020-08-06 23:02:35 +0300

    Lesson 8 Level 4 Task_16

---
+++ src/student_igors_bartkevics/lesson_8/homework/level_4/Circle.java
@@ -0,0 +1,47 @@
+/*
+ССоздание иерархии класса Shape (геометрическая фигура).
+
+Создайте наследника класс Shape - класс Circle.
+Реализуйте в нём абстрактные методы из класса
+Shape: calculateArea() и calculatePerimeter().
+Напишите тесты для класса Circle.
+
+PS: подсказка - необходимые значения для
+расчёта площади и периметра подавайте к
+класс через конструктор.
+
+class Circle extends Shape {
+
+    private double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    // implement here calculateArea()
+
+    // implement here calculatePerimeter()
+
+}
+ */
+package student_igors_bartkevics.lesson_8.homework.level_4;
+import java.lang.Math;
+class Circle extends Shape{
+
+    private double radius;
+
+    public Circle(String title, double radius) {
+        super(title);
+        this.radius = radius;
+    }
+
+    @Override
+    double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
