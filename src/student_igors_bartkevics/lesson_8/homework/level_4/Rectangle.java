43e6377c811dc55bc73e1f24bb386e3133655b92
Igors <igors.barkevics@gmail.com>
2020-08-06 23:25:49 +0300

    Lesson 8 Level 4 Task_18

---
+++ src/student_igors_bartkevics/lesson_8/homework/level_4/Rectangle.java
@@ -0,0 +1,29 @@
+/*
+Создание иерархии класса Shape (геометрическая фигура).
+
+Создайте наследника класс Shape - класс Rectangle.
+Реализуйте в нём абстрактные методы из класса
+Shape: calculateArea() и calculatePerimeter().
+Напишите тесты для класса Rectangle.
+ */
+package student_igors_bartkevics.lesson_8.homework.level_4;
+
+class Rectangle extends Shape{
+    private double width;
+    private double length;
+    Rectangle(String title, double width, double length) {
+        super(title);
+        this.width = width;
+        this.length = length;
+    }
+
+    @Override
+    double calculateArea() {
+        return width * length;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return 2 * (width + length);
+    }
+}
