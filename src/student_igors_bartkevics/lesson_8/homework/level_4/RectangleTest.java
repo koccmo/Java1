9a7a592e3b50720cfdf807cdfba159c291375ab7
Igors <igors.barkevics@gmail.com>
2020-08-09 23:06:39 +0300

    Lesson 8 Level 4 Task_18

--- src/student_igors_bartkevics/lesson_8/homework/level_4/RectangleTest.java
+++ src/student_igors_bartkevics/lesson_8/homework/level_4/RectangleTest.java
@@ -1,6 +1,6 @@
 package student_igors_bartkevics.lesson_8.homework.level_4;
 
-public class RectangleTest {
+class RectangleTest {
 
     Rectangle rectangle = new Rectangle("Rectangle", 2.0, 5.0);
 
