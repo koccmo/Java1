4b091662f575f1314702116d7dedcf3ba22e541d
Jaroslav <brutan.jaroslav@gmail.com>
2020-08-10 22:15:32 +0300

    Lesson 8 update

--- src/student_jaroslav_brutan/lesson_8/day_2/task_8/HumanPlayer.java
+++ src/student_jaroslav_brutan/lesson_8/day_2/task_8/HumanPlayer.java
@@ -1,6 +1,6 @@
 package student_jaroslav_brutan.lesson_8.day_2.task_8;
 
-public class HumanPlayer extends Player {
+class HumanPlayer extends Player {
 
     public HumanPlayer(String name) {
         super (name);
