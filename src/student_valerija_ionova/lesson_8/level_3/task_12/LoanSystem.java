a8bbc8822c6fc99cd85a64bef7e0c3cb6b8e60d8
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-08-10 21:03:51 +0300

    Homework Lesson 8 Level 3

--- src/student_valerija_ionova/lesson_8/level_3/task_12/LoanSystem.java
+++ src/student_valerija_ionova/lesson_8/level_3/task_12/LoanSystem.java
@@ -1,6 +1,6 @@
 package student_valerija_ionova.lesson_8.level_3.task_12;
 
-/*Исправьте код программы так, что бы он компилировался.
+/* Исправьте код программы так, что бы он компилировался.
 Реализовывать метод canReceiveLoan() нельзя.
 
 class LoanSystem {
