d22f5d73085eed646cb439b940fdfa10fb910eed
pavels-s <pavels.sarkels@gmail.com>
2020-08-12 21:44:14 +0300

    Lesson 8, level 2, homework

---
+++ src/student_pavel_sharkel/lesson_8/level_2/task_7/Student.java
@@ -0,0 +1,10 @@
+package student_pavel_sharkel.lesson_8.level_2.task_7;
+
+public class Student extends Course {
+
+    String studentName;
+
+    public Student (String studentName, int courseNumber) {
+        this.studentName = studentName;
+    }
+}
