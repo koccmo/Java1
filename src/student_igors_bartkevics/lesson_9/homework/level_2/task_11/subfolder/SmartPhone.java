4041a8be6cc196bd63568682703c675736b328de
Igors <igors.barkevics@gmail.com>
2020-08-12 23:36:58 +0300

    Lesson 9 Level 2 Task_11 questions

--- src/student_igors_bartkevics/lesson_9/homework/level_2/task_11/subfolder/SmartPhone.java
+++ src/student_igors_bartkevics/lesson_9/homework/level_2/task_11/subfolder/SmartPhone.java
@@ -5,13 +5,12 @@ import student_igors_bartkevics.lesson_9.homework.level_2.task_11.Phone;
 public class SmartPhone extends Phone {
 
     public SmartPhone(int ringVolume) {
-        super(ringVolume);
+        super(ringVolume);   //где зранится и кому пренадлежит?
     }
 
     public static void main(String[] args) {
-        SmartPhone smart = new SmartPhone(3);
-        System.out.println(smart);
-
+        SmartPhone smart = new SmartPhone(3); //для чего нужно поле, если к нему нет доступа?
+        System.out.println(smart);                      //как это можно использовать?
         //Phone phone = new Phone(4); ERROR
     }
 
