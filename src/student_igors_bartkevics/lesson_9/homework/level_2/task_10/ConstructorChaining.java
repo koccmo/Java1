85ac025dbe11dead99cf3547948b7af62f6a71b3
Igors <igors.barkevics@gmail.com>
2020-08-12 20:48:32 +0300

    Lesson 9 Level 1 Task_10

---
+++ src/student_igors_bartkevics/lesson_9/homework/level_2/task_10/ConstructorChaining.java
@@ -0,0 +1,37 @@
+/*
+Дан класс ConstructorChaining.
+
+class ConstructorChaining {
+
+    private String constructorName;
+    private int parameterCount;
+
+    private ConstructorChaining(String constructorName) {
+        this.constructorName = constructorName;
+    }
+
+    // допишите решение тут
+
+}
+
+Создайте в этом классе публичный конструктор с двумя параметрами.
+Внутри этого конструктора первой строкой вызовите приватный конструктор с одним параметром.
+
+Более подробно про этот подход можно прочитать: constructor chaining in Java. */
+package student_igors_bartkevics.lesson_9.homework.level_2.task_10;
+
+class ConstructorChaining {
+
+    private String constructorName;
+    private int parameterCount;
+
+    private ConstructorChaining(String constructorName) {
+        this.constructorName = constructorName;
+    }
+
+    // допишите решение тут
+    public ConstructorChaining(String constructorName, int parameterCount) {
+        this(constructorName);
+        this.parameterCount = parameterCount;
+    }
+}
