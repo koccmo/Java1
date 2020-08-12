a5c53221df52850f420fbaf3042332937f4f9641
Igors <igors.barkevics@gmail.com>
2020-08-12 17:54:03 +0300

    Lesson 9 Level 1 Task_9

---
+++ src/student_igors_bartkevics/lesson_9/homework/level_2/task_9/PrivateConstructorDemo.java
@@ -0,0 +1,8 @@
+package student_igors_bartkevics.lesson_9.homework.level_2.task_9;
+
+class PrivateConstructorDemo {
+    public static void main(String[] args) {
+        //PrivateConstructor constructor = new PrivateConstructor();
+
+    }
+}
