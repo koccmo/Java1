be229fb2cba9cc46230ee7233ce3d0416b7aadcd
robku <rkarrow2@gmail.com>
2020-08-12 13:15:53 -0700

    Lesson 8 level 3 task 11

---
+++ src/student_roberts_kupcs/lesson_8/homework/level_3/LoanSystem.java
@@ -0,0 +1,11 @@
+package student_roberts_kupcs.lesson_8.homework.level_3;
+
+//Исправьте код программы так, что бы он компилировался.
+//
+//abstracts class LoanSystem {
+//
+//}
+
+abstract class LoanSystem {     //abstracts --> abstract
+
+}
