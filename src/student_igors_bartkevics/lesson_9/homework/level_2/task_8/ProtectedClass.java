83344c5181840d6addd6a8f1f1e68646b3be7cfa
Igors <igors.barkevics@gmail.com>
2020-08-12 17:48:21 +0300

    Lesson 9 Level 1 Task_8

---
+++ src/student_igors_bartkevics/lesson_9/homework/level_2/task_8/ProtectedClass.java
@@ -0,0 +1,26 @@
+/*
+Исправьте код класса ProtectedClass так, что бы он компилировался.
+
+protected class ProtectedClass {
+
+    private String title;
+    private String author;
+
+}
+
+Как вы думаете, почему protected классы создавать нельзя?
+Напиши свой ответ в коментарии над исправленным кодом.
+ */
+package student_igors_bartkevics.lesson_9.homework.level_2.task_8;
+/*
+Protected классы создавать нельзя так как в противном случае не было бы возможным
+наследоваться от класса Protected из других пакетов. Таким образом класс с модификатором
+доступа Protected ни чем не отлечался бы от класса с дефолтным модификатором доступа.
+ */
+class ProtectedClass {
+
+    private String title;
+    private String author;
+
+}
+
