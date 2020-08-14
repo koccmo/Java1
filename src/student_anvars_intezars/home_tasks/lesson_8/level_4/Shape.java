be88c6282577fb2961d8f26cb04e042bd533afd6
Anvar331 <anvar.intezar@gmail.com>
2020-08-14 22:22:15 +0300

    lesson8/level4

--- src/student_anvars_intezars/home_tasks/lesson_8/level_4/Shape.java
+++ src/student_anvars_intezars/home_tasks/lesson_8/level_4/Shape.java
@@ -34,3 +34,54 @@ abstract class Shape {
         }
     }
 
+    class Square extends Shape {
+
+        private double length;
+
+        Square(double length) {
+            this.length = length;
+        }
+        double calculateArea () {return length * length;}
+        double calculatePerimeter() { return 4 * length; }
+    }
+
+    class Rectangle extends Shape {
+
+        private double length;
+        private double width;
+
+        Rectangle(double length, double width) {
+            this.length = length;
+            this.width = width;
+        }
+        double calculateArea() {
+            return length * width;
+        }
+        double calculatePerimeter() {
+            return (length + width) * 2;
+        }
+    }
+
+    class Triangle extends Shape {
+
+        private double basis;
+        private double height;
+        private double length1;
+        private double length2;
+        private double length3;
+
+        Triangle(double basis, double height, double length1, double length2, double length3) {
+            this.basis = basis;
+            this.height = height;
+            this.length1 = length1;
+            this.length2 = length2;
+            this.length3 = length3;
+        }
+        double calculateArea() {
+            return (basis * height) / 2;
+        }
+        double calculatePerimeter() {
+            return length1 + length2 + length3;
+        }
+    }
+
