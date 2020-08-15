0b92de29912388c157bb8ce9b5d6d9b4f0b1810a
robku <rkarrow2@gmail.com>
2020-08-14 23:56:52 -0700

    Lesson 9 level 1 task 2

---
+++ src/student_roberts_kupcs/lesson_9/level_1/Task_2/Rocket.java
@@ -0,0 +1,17 @@
+package student_roberts_kupcs.lesson_9.level_1.Task_2;
+
+class Rocket {
+    int firingRange;
+    double flightSpeed;
+    String controlSystem;
+
+    public Rocket(int firingRange, double flightSpeed, String controlSystem) {
+        this.firingRange = firingRange;
+        this.flightSpeed = flightSpeed;
+        this.controlSystem = controlSystem;
+    }
+
+    void displayFiringRange() {
+        System.out.println("Firing range : " + firingRange);
+    }
+}
