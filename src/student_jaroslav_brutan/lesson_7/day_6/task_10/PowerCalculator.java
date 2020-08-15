6942d009fcebeeb4ca97fdfd29d996d861093fa6
Jaroslav <brutan.jaroslav@gmail.com>
2020-08-15 08:24:30 +0300

    Lesson 7 update

---
+++ src/student_jaroslav_brutan/lesson_7/day_6/task_10/PowerCalculator.java
@@ -0,0 +1,14 @@
+package student_jaroslav_brutan.lesson_7.day_6.task_10;
+
+
+
+class PowerCalculator {
+
+        public int powerNumber(int number, int power){
+            int result = 1;
+            for (int i = 1; i <= power; i++){
+                result = result * number;
+            } return result;
+        }
+    }
+
