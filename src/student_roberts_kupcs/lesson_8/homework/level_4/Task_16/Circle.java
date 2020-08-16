65d0a0ec21a4a356a2cd6ac9904c3b35c266f43c
robku <rkarrow2@gmail.com>
2020-08-16 13:19:19 -0700

    correct Lesson 8 level 4  task 16

---
+++ src/student_roberts_kupcs/lesson_8/homework/level_4/Task_16/Circle.java
@@ -0,0 +1,23 @@
+package student_roberts_kupcs.lesson_8.homework.level_4.Task_16;
+
+class Circle extends Shape {
+
+    private double radius;
+
+
+    Circle(String title, double radius) {
+        super(title);
+        this.radius = radius;
+    }
+
+    double calculateArea() {
+        double pi = 3.1415;
+        return pi * radius * radius;
+    }
+
+    double calculatePerimeter() {
+        double pi = 3.1415;
+        return 2 * pi * radius;
+
+    }
+}
