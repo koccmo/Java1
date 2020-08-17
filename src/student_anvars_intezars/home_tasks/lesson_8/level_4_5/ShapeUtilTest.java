234ab245ec300b42655ba42d8d05e82727a3567e
Anvar331 <anvar.intezar@gmail.com>
2020-08-17 20:59:32 +0300

    lesson8/level5/Tasks20,21,22,23,24,25

---
+++ src/student_anvars_intezars/home_tasks/lesson_8/level_4_5/ShapeUtilTest.java
@@ -0,0 +1,18 @@
+package student_anvars_intezars.home_tasks.lesson_8.level_4_5;
+
+class ShapeUtilTest {
+
+    public static void main(String[] args) {
+
+        ShapeUtilTest shapeUtilTest = new ShapeUtilTest();
+        shapeUtilTest.createCircleTest();
+    }
+
+    void createCircleTest() {
+        ShapeUtil shapeUtil = new ShapeUtil();
+        Circle resultOfRandomCircle = shapeUtil.createRandomCircle();
+        double resultOfArea = resultOfRandomCircle.calculateArea();
+        System.out.println(resultOfArea);
+        }
+    }
+
