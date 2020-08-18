2f7e98c3aa1733e43f533dff22dbd54420466511
Anvar331 <anvar.intezar@gmail.com>
2020-08-18 19:00:55 +0300

    lesson8/Task27(updated)

--- src/student_anvars_intezars/home_tasks/lesson_8/level_4_5/ShapeUtilDemo.java
+++ src/student_anvars_intezars/home_tasks/lesson_8/level_4_5/ShapeUtilDemo.java
@@ -10,6 +10,12 @@ class ShapeUtilDemo {
         double circlePerimeter = shapeUtil.calculatePerimeter(circle);
         System.out.println("Area of circle = " + circleArea);
         System.out.println("Perimeter of circle = " + circlePerimeter);
+        System.out.println();
 
+        Shape rectangle = shapeUtil.createRandomRectangle();
+        double rectangleArea = shapeUtil.calculateArea(rectangle);
+        double rectanglePerimeter = shapeUtil.calculatePerimeter(rectangle);
+        System.out.println("Area of rectangle = " + rectangleArea);
+        System.out.println("Perimeter of rectangle = " + rectanglePerimeter);
     }
 }
