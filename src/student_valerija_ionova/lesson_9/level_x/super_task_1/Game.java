135ae002a34c9c77b749f3c62e429112dd623983
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-08-18 20:47:03 +0300

    Homework Lesson 9 ST 1 Game

--- src/student_valerija_ionova/lesson_9/level_x/super_task_1/Game.java
+++ src/student_valerija_ionova/lesson_9/level_x/super_task_1/Game.java
@@ -1,42 +1,38 @@
 package student_valerija_ionova.lesson_9.level_x.super_task_1;
 
-public class Game {
+class Game {
 
     Field field = new Field();
     PlayersMove playersMove = new PlayersMove();
-    VictoryConditions victoryConditions = new VictoryConditions();
 
     void game(){
         field.createNewField();
         System.out.println("Let's start game!\n");
-
-        field.printField(field.getField());
+        field.printField();
 
         while(true){
-
             System.out.println("Please enter coordinates for player 3");
-            Coordinates move3 = playersMove.newMove(field.getField());
+            Coordinates move3 = playersMove.newMove(field);
             field.getField()[move3.getY()][move3.getX()] = 3;
-            field.printField(field.getField());
-            if (victoryConditions.win(field.getField(), 3)){
+            field.printField();
+            if (field.win(3)){
                 System.out.println("Player 3 WON!");
                 break;
             }
-            if (field.fullField(field.getField())){
+            if (field.fullField()){
                 System.out.println("Full field. The end!");
                 break;
             }
 
-
             System.out.println("Please enter coordinates for player 8");
-            Coordinates move8 = playersMove.newMove(field.getField());
+            Coordinates move8 = playersMove.newMove(field);
             field.getField()[move8.getY()][move8.getX()] = 8;
-            field.printField(field.getField());
-            if (victoryConditions.win(field.getField(), 8)){
+            field.printField();
+            if (field.win(8)){
                 System.out.println("Player 8 WON!");
                 break;
             }
-            if (field.fullField(field.getField())){
+            if (field.fullField()){
                 System.out.println("Full field. The end!");
                 break;
             }
