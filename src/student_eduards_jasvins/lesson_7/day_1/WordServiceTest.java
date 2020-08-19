7f6a38a60dcf904c434f6659284fe6f18be5c95c
vsavonins <viktors.savonins@proofit.lv>
2020-08-19 08:06:06 +0300

    CodeReviewComments

--- src/student_eduards_jasvins/lesson_7/day_1/WordServiceTest.java
+++ src/student_eduards_jasvins/lesson_7/day_1/WordServiceTest.java
@@ -1,5 +1,8 @@
 package student_eduards_jasvins.lesson_7.day_1;
 
+import teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
 public class WordServiceTest {
     public static void main(String[] args) {
         WordServiceTest wordServiceTest = new WordServiceTest();
