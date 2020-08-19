aa63e1fd6bc6bf4e9bdec7ecf1c85c3f643ab559
Jaroslav <brutan.jaroslav@gmail.com>
2020-08-19 23:34:26 +0300

    lesson 9 update

---
+++ src/student_jaroslav_brutan/lesson_9/day_2/task_11/ElectricGuitar.java
@@ -0,0 +1,11 @@
+package student_jaroslav_brutan.lesson_9.day_2.task_11;
+
+class ElectricGuitar extends Guitar{
+
+    private int stringAmount;
+
+    protected ElectricGuitar(String color, String stringType, int stringAmount) {
+        super(color, stringType);
+        this.stringAmount = stringAmount;
+    }
+}
---
+++ src/student_jaroslav_brutan/lesson_9/day_2/task_11/Guitar.java
@@ -0,0 +1,12 @@
+package student_jaroslav_brutan.lesson_9.day_2.task_11;
+
+class Guitar {
+
+    private String color;
+    private String stringType;
+
+    protected Guitar(String color, String stringType) {
+        this.color = color;
+        this.stringType = stringType;
+    }
+}
