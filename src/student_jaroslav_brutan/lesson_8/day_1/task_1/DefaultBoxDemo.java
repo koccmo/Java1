df91fe4b3a5ca06b8c32587c640560c4d3093183
Jaroslav <brutan.jaroslav@gmail.com>
2020-08-19 22:28:40 +0300

    lesson 8 update

---
+++ src/student_jaroslav_brutan/lesson_8/day_1/task_1/DefaultBoxDemo.java
@@ -0,0 +1,9 @@
+package student_jaroslav_brutan.lesson_8.day_1.task_1;
+
+public class DefaultBoxDemo {
+
+    public static void main(String[] args) {
+        DefaultBox defaultBox = new DefaultBox(4);
+        System.out.println(defaultBox.getWidth() + " is box width!");
+    }
+}
