ce3ce5ea411558b60cb7e9cb50cf476bd4ba509c
vsavonins <viktors.savonins@proofit.lv>
2020-08-19 09:38:06 +0300

    CodeReviewComments

--- src/student_alexey_tretyakov/lesson_7/level_6/PowerCalculatorTest.java
+++ src/student_alexey_tretyakov/lesson_7/level_6/PowerCalculatorTest.java
@@ -1,5 +1,8 @@
 package student_alexey_tretyakov.lesson_7.level_6;
 
+import teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
 class PowerCalculatorTest {
 
     public static void main(String[] args) {
