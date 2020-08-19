7b0a7966abcaaf80eadd248ec7c38352c8c992da
robku <rkarrow2@gmail.com>
2020-08-18 21:14:22 -0700

    Lesson 9 level 1  task 7

---
+++ src/student_roberts_kupcs/lesson_9/level_1/Task_7/PrivateClass.java
@@ -0,0 +1,24 @@
+package student_roberts_kupcs.lesson_9.level_1.Task_7;
+
+//Исправьте код класса PrivateClass так, что бы он компилировался.
+//
+//private class PrivateClass {
+//
+//    private String title;
+//    private String author;
+//
+//
+//}
+//
+//Как вы думаете, почему private классы создавать нельзя?
+//Напиши свой ответ в коментарии над исправленным кодом.
+
+class PrivateClass {    //Модификатор private здесь запрещен
+
+    //Приватный класс верхнего уровня не имеет большого смысла,
+    // потому что на него нельзя ссылаться ни из одного класса.
+
+    private String title;
+    private String author;
+
+}
