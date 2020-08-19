410a7bb6e4dbab0f66c6d5d33f31f35a864db7fd
Jaroslav <brutan.jaroslav@gmail.com>
2020-08-19 23:38:44 +0300

    lesson 9 update

---
+++ src/student_jaroslav_brutan/lesson_9/day_2/task_12/A.java
@@ -0,0 +1,7 @@
+package student_jaroslav_brutan.lesson_9.day_2.task_12;
+
+class A {
+    public A() {
+        System.out.println("Class A Constructor");
+    }
+}
---
+++ src/student_jaroslav_brutan/lesson_9/day_2/task_12/C.java
@@ -0,0 +1,7 @@
+package student_jaroslav_brutan.lesson_9.day_2.task_12;
+
+class C extends B{
+    public C() {
+        System.out.println("Class C Constructor");
+    }
+}
