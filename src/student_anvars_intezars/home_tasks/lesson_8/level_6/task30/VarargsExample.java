413560e4a8947ef8496e968134b5ac554a3e3924
Anvar331 <anvar.intezar@gmail.com>
2020-08-20 22:56:12 +0300

    Lesson8/level6/Task30

---
+++ src/student_anvars_intezars/home_tasks/lesson_8/level_6/task30/VarargsExample.java
@@ -0,0 +1,18 @@
+package student_anvars_intezars.home_tasks.lesson_8.level_6.task30;
+
+class VarargsExample {
+    /*
+    Varargs — это аргументы переменной длины: фича, которая
+    появилась еще в JDK5. Varargs позволяют нам создавать методы
+    с произвольным количеством аргументов. По большому счету, создавать
+    такие методы можно было и раньше. Правда, делать это было не очень удобно.
+
+    Source:https://javarush.ru/groups/posts/2733-java-varargs
+     */
+
+    void printNumbersOfVarargArray (int... numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + ",");
+        }
+    }
+}
