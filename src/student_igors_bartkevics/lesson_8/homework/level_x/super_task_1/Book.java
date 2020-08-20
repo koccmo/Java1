379cd719697858ef5699bd81c4ce2a5d28ffd9ec
Igors <igors.barkevics@gmail.com>
2020-08-20 10:19:17 +0300

    Lesson 8 Level X Super_task_1

---
+++ src/student_igors_bartkevics/lesson_8/homework/level_x/super_task_1/Book.java
@@ -0,0 +1,11 @@
+package student_igors_bartkevics.lesson_8.homework.level_x.super_task_1;
+
+class Book {
+    private String title;
+    private String author;
+    private int bookID;
+    private boolean isReserved;
+    private boolean isInLibrary;
+    private boolean isAvailable;
+
+}
