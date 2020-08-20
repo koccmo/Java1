f01da81bee14737d705861eb7b95f069608ca716
Jaroslav <brutan.jaroslav@gmail.com>
2020-08-20 22:52:56 +0300

    lesson 9 update

---
+++ src/student_jaroslav_brutan/lesson_9/day_4/Trader.java
@@ -0,0 +1,7 @@
+package student_jaroslav_brutan.lesson_9.day_4;
+
+class Trader {
+
+    private String fullName;
+    private String city;
+}
---
+++ src/student_jaroslav_brutan/lesson_9/day_4/Transaction.java
@@ -0,0 +1,7 @@
+package student_jaroslav_brutan.lesson_9.day_4;
+
+class Transaction {
+
+    private Trader trader;
+    private int amount;
+}
