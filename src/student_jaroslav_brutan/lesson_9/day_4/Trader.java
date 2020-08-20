d84eb976c20bac3c0683d105303b2d99b740a57e
Jaroslav <brutan.jaroslav@gmail.com>
2020-08-20 22:56:14 +0300

    Lesson 9 update

--- src/student_jaroslav_brutan/lesson_9/day_4/Trader.java
+++ src/student_jaroslav_brutan/lesson_9/day_4/Trader.java
@@ -4,4 +4,9 @@ class Trader {
 
     private String fullName;
     private String city;
+
+    public Trader(String fullName, String city) {
+        this.fullName = fullName;
+        this.city = city;
+    }
 }
--- src/student_jaroslav_brutan/lesson_9/day_4/Transaction.java
+++ src/student_jaroslav_brutan/lesson_9/day_4/Transaction.java
@@ -4,4 +4,9 @@ class Transaction {
 
     private Trader trader;
     private int amount;
+
+    public Transaction(Trader trader, int amount) {
+        this.trader = trader;
+        this.amount = amount;
+    }
 }
