7d75fc826881e01d6004ac92a6c0414de6a14481
Anvar331 <anvar.intezar@gmail.com>
2020-08-23 10:57:29 +0300

    Lesson7/task11/corrected/after/code/review

--- src/student_anvars_intezars/home_tasks/lesson_7/day_6/task11/CreditCardTest.java
+++ src/student_anvars_intezars/home_tasks/lesson_7/day_6/task11/CreditCardTest.java
@@ -1,12 +1,6 @@
 package student_anvars_intezars.home_tasks.lesson_7.day_6.task11;
 
-import java.sql.SQLOutput;
-
-import teacher.codereview.CodeReview;
-import teacher.codereview.CodeReviewComment;
-
-@CodeReview(approved = false)
-@CodeReviewComment(teacher = "import java.sql.SQLOutput - не используемый импорт, удалить")
+//не используемый импорт удален
 class CreditCardTest {
 
     public static void main(String[] args) {
@@ -22,17 +16,18 @@ class CreditCardTest {
         if (withdrawResult == 900) {
             System.out.println("Test for withdraw = OK");
             System.out.println();
-        }else {
+        } else {
             System.out.println("Test for withdraw = FAIL");
         }
     }
 
     public void testForCreditCardDeposit() {
-        CreditCard creditCard = new CreditCard(77335591,8978);
-        int depositResult = creditCard.deposit(8978,15);
-        if (depositResult == 15) { System.out.println();
+        CreditCard creditCard = new CreditCard(77335591, 8978);
+        int depositResult = creditCard.deposit(8978, 15);
+        if (depositResult == 15) {
+            System.out.println();
             System.out.println("Test for deposit = OK");
-        }else {
+        } else {
             System.out.println("Test for deposit = FAIL");
         }
     }
