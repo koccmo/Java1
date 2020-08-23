95645a0e4a7241cd647802f528f88e829240897b
Anvar331 <anvar.intezar@gmail.com>
2020-08-23 20:09:55 +0300

    Lesson8/level1/Task4

---
+++ src/student_anvars_intezars/home_tasks/lesson_9/level_1/task2/Rocket.java
@@ -0,0 +1,9 @@
+/*package student_anvars_intezars.home_tasks.lesson_9.level_1.task2;
+
+class Rocket {
+
+    protected Rocket() {
+
+    }
+
+}*/
--- src/student_anvars_intezars/home_tasks/lesson_9/task2/Rocket.java
+++
@@ -1,9 +0,0 @@
-/*package student_anvars_intezars.home_tasks.lesson_9.task2;
-
-class Rocket {
-
-    protected Rocket() {
-
-    }
-
-}*/
