71bfc32079c8ba64acc2f364f3172ec0d9b0d5e3
robku <rkarrow2@gmail.com>
2020-08-23 22:53:56 -0700

    Lesson 9 level 2  task 9

---
+++ src/student_roberts_kupcs/lesson_9/level_2/Task_9/PrivateConstructorDemo.java
@@ -0,0 +1,7 @@
+package student_roberts_kupcs.lesson_9.level_2.Task_9;
+
+class PrivateConstructorDemo {
+    public static void main(String[] args) {
+        //PrivateConstructor privateConstructor = new PrivateConstructor();
+    }
+}
