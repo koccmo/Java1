6895459dd8b3abc5752591aea260fbe9e3989a34
Anvar331 <anvar.intezar@gmail.com>
2020-08-24 20:54:46 +0300

    Lesson8/level1/Task5

---
+++ src/student_anvars_intezars/home_tasks/lesson_9/level_1/task5/NewSafeDemo.java
@@ -0,0 +1,15 @@
+package student_anvars_intezars.home_tasks.lesson_9.level_1.task5;
+
+class NewSafeDemo {
+
+    public static void main(String[] args) {
+
+        NewSafe mySafe = new NewSafe(112233,1000);
+        int moneyTaken = mySafe.getMoney(112233,400);
+        int moneyLeft = mySafe.putMoney(112233,600);
+
+        System.out.println("After entering password I took " + moneyTaken + " from safe");
+        System.out.println("Then I added more money. Now I have " + moneyLeft);
+
+    }
+}
