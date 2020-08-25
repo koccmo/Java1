e472a4d1a53a389d308b4753ec5b3e0e55374ad0
AlexaMaksimovic <lexaferret@inbox.lv>
2020-08-25 10:17:32 +0300

    lesson 9 level_1 task_4

---
+++ src/student_aleksandra_maksimovic/lesson_9/level_1/task_4/SafeDemo.java
@@ -0,0 +1,18 @@
+package student_aleksandra_maksimovic.lesson_9.level_1.task_4;
+
+
+public class SafeDemo {
+    public static void main(String[] args) {
+        Safe safe = new Safe();
+        safe.pinCode = "7891";
+        safe.amount = 100;
+
+        System.out.println("Pin kod - "+ safe.pinCode + "Money amount = " + safe.amount);
+
+        safe.pinCode = "1111";
+        safe.amount = 0;
+
+        System.out.println("Pin kod - "+ safe.pinCode + "Money amount = " + safe.amount);
+
+    }
+}
