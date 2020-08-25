6d7b1e4d95b84dfbd8b5f4c16833629422894492
AlexaMaksimovic <lexaferret@inbox.lv>
2020-08-25 11:33:56 +0300

    lesson 9 level_1 task_7

---
+++ src/student_aleksandra_maksimovic/lesson_9/level_1/task_7/PrivateClass.java
@@ -0,0 +1,27 @@
+package student_aleksandra_maksimovic.lesson_9.level_1.task_7;
+/*
+Исправьте код класса PrivateClass так, что бы он компилировался.
+
+private class PrivateClass {
+
+    private String title;
+    private String author;
+
+
+}
+
+Как вы думаете, почему private классы создавать нельзя?
+Напиши свой ответ в коментарии над исправленным кодом.
+
+Уже говорила ранее...Это не имеет смысла, класс будет не дотупен для использования других классов.
+
+  */
+
+class PrivateClass {
+
+    private String title;
+    private String author;
+
+
+}
+
