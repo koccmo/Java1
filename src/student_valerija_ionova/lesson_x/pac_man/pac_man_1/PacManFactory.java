ad01a302c3be44f9fd0a68ec64944e9fe55f165d
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-08-26 17:31:53 +0300

    Homework Lesson X Pac-Man PacManFactory NotPublic

--- src/student_valerija_ionova/lesson_x/pac_man/pac_man_1/PacManFactory.java
+++ src/student_valerija_ionova/lesson_x/pac_man/pac_man_1/PacManFactory.java
@@ -1,6 +1,6 @@
 package student_valerija_ionova.lesson_x.pac_man.pac_man_1;
 
-public class PacManFactory {
+class PacManFactory {
 
     MoveInformation createPacMan (int x, int y, Direction direction){
         Coordinates coordinates = new Coordinates(x, y);
