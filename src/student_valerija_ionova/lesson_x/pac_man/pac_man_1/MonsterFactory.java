454e6dcec7db95c89e4a5b222e7a2df2d1ee70e3
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-08-26 17:29:43 +0300

    Homework Lesson X Pac-Man MonstersFactory not public

--- src/student_valerija_ionova/lesson_x/pac_man/pac_man_1/MonsterFactory.java
+++ src/student_valerija_ionova/lesson_x/pac_man/pac_man_1/MonsterFactory.java
@@ -1,6 +1,6 @@
 package student_valerija_ionova.lesson_x.pac_man.pac_man_1;
 
-public class MonsterFactory {
+class MonsterFactory {
 
     MoveInformation createMonster (int x, int y, Direction direction){
         Coordinates coordinates = new Coordinates(x, y);
