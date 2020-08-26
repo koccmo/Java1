8974d108ec988babc5435de2df657e32a2db6a00
Igors <igors.barkevics@gmail.com>
2020-08-26 09:54:56 +0300

    Lesson 10 Level 1 Task_3

---
+++ src/student_igors_bartkevics/lesson_10/homework/level_1/task_3/MyList.java
@@ -0,0 +1,11 @@
+package student_igors_bartkevics.lesson_10.homework.level_1.task_3;
+
+interface MyList {
+
+    void add(int value); //add new element to end of the list
+
+    void removeByIndex(int index); //remove element with index from list
+
+    int getElement(int index); //get element from list with index
+
+}
