024d52b05e7cb3fed1cef4e3a6f8a3a00a6d3285
Igors <igors.barkevics@gmail.com>
2020-08-30 12:30:48 +0300

    Lesson 10 Level 6 Task_15

---
+++ src/student_igors_bartkevics/lesson_10/homework/level_6/task_15/BookReader.java
@@ -0,0 +1,7 @@
+package student_igors_bartkevics.lesson_10.homework.level_6.task_15;
+
+interface BookReader {
+
+    boolean addBook(Book book);
+
+}
---
+++ src/student_igors_bartkevics/lesson_10/homework/level_6/task_15/BookReaderImpl.java
@@ -0,0 +1,24 @@
+package student_igors_bartkevics.lesson_10.homework.level_6.task_15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BookReaderImpl implements BookReader {
+
+    private List<Book> library = new ArrayList<>();
+    private BookValidator validator = new BookValidator();
+
+    List<Book> getLibrary() {
+        return library;
+    }
+
+
+    @Override
+    public boolean addBook(Book bookToAdd) {
+        if (validator.isBookValidToAddInReaderLibrary(bookToAdd,this)) {
+            library.add(bookToAdd);
+            return true;
+        }
+        else return false;
+    }
+}
