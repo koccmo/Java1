47366bd9e5ddc24d5aa3f611dd77358dc2d1850d
Igors <igors.barkevics@gmail.com>
2020-08-30 23:06:17 +0300

    Lesson 10 Level 6 Task_16

---
+++ src/student_igors_bartkevics/lesson_10/homework/level_6/task_16/BookReaderImpl.java
@@ -0,0 +1,33 @@
+package student_igors_bartkevics.lesson_10.homework.level_6.task_16;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BookReaderImpl implements BookReader {
+
+    private List<Book> library = new ArrayList<>();
+    private BookValidator validator = new BookValidator();
+
+    List<Book> getLibrary() {
+        return library;
+    }
+
+
+    @Override
+    public boolean addBook(Book book) {
+        if (validator.isBookValidToAddInReaderLibrary(book,this)) {
+            library.add(book);
+            return true;
+        }
+        else return false;
+    }
+
+    @Override
+    public boolean removeBook(Book book) {
+        if (validator.isBookValidToRemoveItFromReaderLibrary(book, this)) {
+            library.remove(book);
+            return true;
+        }
+        else return false;
+    }
+}
---
+++ src/student_igors_bartkevics/lesson_10/homework/level_6/task_16/BookValidator.java
@@ -0,0 +1,38 @@
+package student_igors_bartkevics.lesson_10.homework.level_6.task_16;
+
+class BookValidator {
+
+    boolean isBookValidToAddInReaderLibrary(Book bookToAdd, BookReaderImpl bookReader) {
+        return (bookReader.getLibrary().isEmpty() || !isBookInReaderLibrary(bookToAdd, bookReader)) &&
+                bookFieldsAreFilled(bookToAdd);
+    }
+
+    boolean isBookValidToRemoveItFromReaderLibrary(Book bookToRemove, BookReaderImpl bookReader) {
+        return (isBookInReaderLibrary(bookToRemove, bookReader) && bookFieldsAreFilled(bookToRemove));
+    }
+
+    private boolean bookFieldsAreFilled(Book book) {
+        return isBookTitleFieldFilled(book) && isBookAuthorFieldFilled(book);
+    }
+
+    private boolean isBookTitleFieldFilled(Book book) {
+        return !book.getTitle().equals("") && book.getTitle() != null;
+    }
+
+    private boolean isBookAuthorFieldFilled(Book book) {
+        return !book.getAuthor().equals("") && book.getAuthor() != null;
+    }
+
+    private boolean isBookInReaderLibrary(Book bookToCheck, BookReaderImpl bookReader) {
+        boolean result = false;
+
+        for (Book book : bookReader.getLibrary()) {
+            if (book.getAuthor().equals(bookToCheck.getAuthor()) &&
+                    book.getTitle().equals(bookToCheck.getTitle())) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+}
