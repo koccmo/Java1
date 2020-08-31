36eb6dc6f92ac2f63eeadac9ab0a28b18bcd1145
Igors <igors.barkevics@gmail.com>
2020-08-31 10:18:34 +0300

    Lesson 10 Level 6 Task_18

---
+++ src/student_igors_bartkevics/lesson_10/homework/level_6/task_18/BookReader.java
@@ -0,0 +1,15 @@
+package student_igors_bartkevics.lesson_10.homework.level_6.task_18;
+
+import java.util.List;
+
+interface BookReader {
+
+    boolean addBook(Book book);
+
+    boolean removeBook(Book book);
+
+    String[] bookLibraryToStringList();
+
+    List<Book> findBooksByAuthor(String author);
+
+}
