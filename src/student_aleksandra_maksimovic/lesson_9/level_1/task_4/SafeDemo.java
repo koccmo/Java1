35ff318394abdf73368f626bd4bc14b3b30f6ed7
vsavonins <viktors.savonins@proofit.lv>
2020-09-01 08:50:34 +0300

    Code review lesson 9

--- src/student_aleksandra_maksimovic/lesson_9/level_1/task_4/SafeDemo.java
+++ src/student_aleksandra_maksimovic/lesson_9/level_1/task_4/SafeDemo.java
@@ -1,6 +1,8 @@
 package student_aleksandra_maksimovic.lesson_9.level_1.task_4;
 
+import teacher.codereview.CodeReview;
 
+@CodeReview(approved = true)
 public class SafeDemo {
     public static void main(String[] args) {
         Safe safe = new Safe();
