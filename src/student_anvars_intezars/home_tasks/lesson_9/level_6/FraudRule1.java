c67ac30d727c54634787eecf0d0dba1bbb592294
Anvar331 <anvar.intezar@gmail.com>
2020-09-01 20:52:16 +0300

    Lesson9/level6/FraudMethodCorrected

--- src/student_anvars_intezars/home_tasks/lesson_9/level_6/FraudRule1.java
+++ src/student_anvars_intezars/home_tasks/lesson_9/level_6/FraudRule1.java
@@ -20,7 +20,9 @@ class FraudRule1 extends FraudRule {
     boolean compareTraderName(Trader trader) {
         String traderName = trader.getFullName();
         if (traderName.equals("Pokemon")) {
+            return true;
+        } else {
+            return false;
         }
-        return true;
     }
 }
