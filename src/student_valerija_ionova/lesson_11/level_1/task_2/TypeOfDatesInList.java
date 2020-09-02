dcfde8e534ecec534ff9f158e1504832fcb8266f
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-02 20:47:36 +0300

    Homework Lesson 11 Level 1 Task 2

---
+++ src/student_valerija_ionova/lesson_11/level_1/task_2/TypeOfDatesInList.java
@@ -0,0 +1,19 @@
+package student_valerija_ionova.lesson_11.level_1.task_2;
+
+//Что может хранить java.util.List: примитивные типы данных или ссылки на объекты,
+//или то и другое? Напишите программу, в которой кодом продемонстрируйте ответ на вопрос.
+
+// Ссылки на объекты
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TypeOfDatesInList {
+    public static void main (String [] args) {
+        List<String> listOfIntegers = new ArrayList<>();
+        listOfIntegers.add("It's object String");
+
+        // list <char> listError = new ArrayList<>(); Это будет ошибкой, т.к. char примитивный тип данных
+    }
+
+}
