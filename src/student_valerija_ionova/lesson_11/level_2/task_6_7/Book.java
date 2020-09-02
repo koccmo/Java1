e1c494a448301a8a217506f628ca9b72dcf3ec65
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-02 21:58:21 +0300

    Homework Lesson 11 Level 2 Task 6-7 Book

---
+++ src/student_valerija_ionova/lesson_11/level_2/task_6_7/Book.java
@@ -0,0 +1,43 @@
+package student_valerija_ionova.lesson_11.level_2.task_6_7;
+
+import java.util.Objects;
+
+class Book {
+
+    private String title;
+    private String author;
+
+    Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return title.equals(book.title) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{ title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
