617ca7b6463706f1469522931277ac4bc1a01cb2
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-02 20:47:55 +0300

    Homework Lesson 11 Level 1 Task 4

---
+++ src/student_valerija_ionova/lesson_11/level_1/task_4/IfItIsPossibleToStoreTheSameElements.java
@@ -0,0 +1,26 @@
+package student_valerija_ionova.lesson_11.level_1.task_4;
+
+//Могут ли реализации интерфейса java.util.List хранить одинаковые
+//элементы (дубликаты)? Напишите код программы, который демонстрирует
+//ответ на этот вопрос.
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IfItIsPossibleToStoreTheSameElements {
+
+    public static void main (String [] args) {
+        List<Integer> listForExample = new ArrayList<>();
+
+        listForExample.add(10);
+        listForExample.add(10);
+        listForExample.add(10);
+        listForExample.add(10);
+        listForExample.add(10);
+        listForExample.add(10);
+
+        System.out.println("Yes! It's possible to store the same elements!");
+        listForExample.forEach(System.out :: println);
+    }
+
+}
