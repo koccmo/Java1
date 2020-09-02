3af1164e7460f319271a590dc6b5cf659d65db67
Anvar331 <anvar.intezar@gmail.com>
2020-09-02 23:10:22 +0300

    Lesson9/level2/ConstructorDemoChanged

--- src/student_anvars_intezars/home_tasks/lesson_9/level_2/task9/PrivateConstructorDemo.java
+++ src/student_anvars_intezars/home_tasks/lesson_9/level_2/task9/PrivateConstructorDemo.java
@@ -1,9 +1,15 @@
 /*package student_anvars_intezars.home_tasks.lesson_9.level_2.task9;
 
 @CodeReview(approved = false)
+//Исрпавлено. Код компилится
 class PrivateConstructorDemo {
 
-    PrivateConstructor privateConstructor = new PrivateConstructor();
+    public static void main(String[] args) {
+
+        PrivateConstructorDemo privateConstructorDemo = new PrivateConstructorDemo();
+    }
 }
 
  */
+
+
