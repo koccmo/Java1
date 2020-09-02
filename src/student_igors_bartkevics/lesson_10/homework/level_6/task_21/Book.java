063da4495ca80a2bfbccc61cfa338cb37fc36ed5
Igors <igors.barkevics@gmail.com>
2020-09-02 20:19:36 +0300

    Lesson 10 Level 6 Task_21

--- src/student_igors_bartkevics/lesson_10/homework/level_6/task_21/Book.java
+++ src/student_igors_bartkevics/lesson_10/homework/level_6/task_21/Book.java
@@ -3,10 +3,12 @@ package student_igors_bartkevics.lesson_10.homework.level_6.task_21;
 class Book {
     private String author;
     private String title;
+
     Book(String author, String title) {
         this.author = author;
         this.title = title;
     }
+
 
     public String getAuthor() {
         return author;
