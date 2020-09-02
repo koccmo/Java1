d7a98e7175bbd9aa7adc8036589219865886c211
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-02 21:56:27 +0300

    Homework Lesson 11 Level 1 Task 1 not Public

--- src/student_valerija_ionova/lesson_11/level_1/task_1/RealizationsOfJavaUtilListInterface.java
+++ src/student_valerija_ionova/lesson_11/level_1/task_1/RealizationsOfJavaUtilListInterface.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class RealizationsOfJavaUtilListInterface {
+class RealizationsOfJavaUtilListInterface {
 
     public static void main (String [] args){
 
