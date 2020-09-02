6bc71b258702c3310244e1ae922be35d78f88f66
Igors <igors.barkevics@gmail.com>
2020-09-02 19:12:53 +0300

    Lesson 10 Level 6 Task_20

---
+++ src/student_igors_bartkevics/lesson_10/homework/level_6/task_20/BookValidator.java
@@ -0,0 +1,43 @@
+package student_igors_bartkevics.lesson_10.homework.level_6.task_20;
+
+class BookValidator {
+
+    boolean isBookValidToAddInReaderLibrary(Book bookToAdd, BookReaderImpl bookReader) {
+        return (bookReader.isLibraryEmpty() || !bookReader.isBookInReaderLibrary(bookToAdd)) &&
+                bookFieldsAreFilled(bookToAdd);
+    }
+
+    boolean isBookValidToRemoveItFromReaderLibrary(Book bookToRemove, BookReaderImpl bookReader) {
+        return (bookReader.isBookInReaderLibrary(bookToRemove) && bookFieldsAreFilled(bookToRemove));
+    }
+
+    boolean isBookByAuthor(Book book, String author) {
+        return (book.getAuthor().equals(author));
+    }
+
+    boolean isBookWithTitle(Book book, String title) {
+        return (book.getTitle().equals(title));
+    }
+
+    boolean areLettersMatchingBookFieldValue(String letters, String fieldValue) {
+        if ((letters.length() < fieldValue.length()) && letters.length() > 0) {
+            String substring = fieldValue.substring(0, letters.length());
+            return letters.equals(substring);
+        }
+        else {
+            return false;
+        }
+    }
+
+    private boolean bookFieldsAreFilled(Book book) {
+        return isBookTitleFieldFilled(book) && isBookAuthorFieldFilled(book);
+    }
+
+    private boolean isBookTitleFieldFilled(Book book) {
+        return !book.getTitle().equals("") && book.getTitle() != null;
+    }
+
+    private boolean isBookAuthorFieldFilled(Book book) {
+        return !book.getAuthor().equals("") && book.getAuthor() != null;
+    }
+}
