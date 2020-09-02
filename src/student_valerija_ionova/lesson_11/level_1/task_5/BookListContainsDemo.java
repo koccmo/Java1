61ae3ca2a9a8ebae9680990a539c8d917bdf1f26
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-02 20:48:18 +0300

    Homework Lesson 11 Level 1 Task 5 BookContainsDemo

---
+++ src/student_valerija_ionova/lesson_11/level_1/task_5/BookListContainsDemo.java
@@ -0,0 +1,31 @@
+package student_valerija_ionova.lesson_11.level_1.task_5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookListContainsDemo {
+
+    public static void main (String [] args) {
+
+        List<Book> library = new ArrayList<>();
+
+        Book book1 = new Book("Author1", "Title1");
+        Book book2 = new Book("Author2", "Title2");
+        Book book3 = new Book("Author3", "Title3");
+        Book book4 = new Book("Author4", "Title4");
+        Book book5 = new Book("Author5", "Title5");
+
+        library.add(book1);
+        library.add(book2);
+        library.add(book3);
+        library.add(book4);
+        library.add(book5);
+
+        Book bookToFind1 = new Book("Author5", "Title5");
+        Book bookToFind2 = new Book("Author9", "Title9");
+
+        System.out.println("If is bookToFind1 in library: " + library.contains(bookToFind1));
+        System.out.println("If is bookToFind2 in library: " + library.contains(bookToFind2));
+
+    }
+}
