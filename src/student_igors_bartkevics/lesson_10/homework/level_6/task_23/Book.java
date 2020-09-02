42988415d0564cc57f6b01d870e7e4cfff90e4c6
Igors <igors.barkevics@gmail.com>
2020-09-02 20:19:14 +0300

    Lesson 10 Level 6 Task_23

---
+++ src/student_igors_bartkevics/lesson_10/homework/level_6/task_23/Book.java
@@ -0,0 +1,28 @@
+package student_igors_bartkevics.lesson_10.homework.level_6.task_23;
+
+class Book {
+    private String author;
+    private String title;
+    private boolean isReadState = false;
+
+    Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public void setIsReadState(boolean state) {
+        isReadState = state;
+    }
+
+    public boolean getIsReadState() {
+        return isReadState;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
