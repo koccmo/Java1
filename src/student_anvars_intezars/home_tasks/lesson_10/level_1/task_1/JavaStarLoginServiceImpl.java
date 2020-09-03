a1765abfcb11bbebf1137251540dacf4c3770ab8
Anvar331 <anvar.intezar@gmail.com>
2020-09-03 21:44:09 +0300

    Lesson10/level1/Task1

---
+++ src/student_anvars_intezars/home_tasks/lesson_10/level_1/task_1/JavaStarLoginServiceImpl.java
@@ -0,0 +1,16 @@
+package student_anvars_intezars.home_tasks.lesson_10.level_1.task_1;
+
+class JavaStarLoginServiceImpl implements JavaStarLoginService {
+
+    public boolean canLogin(String login, String password) {
+        return notNull(login)
+                && login.equals("javastar")
+                && notNull(password)
+                && password.equals("pass");
+    }
+
+    private boolean notNull(String text) {
+        return text != null;
+    }
+
+}
