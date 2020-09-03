36282e472b30f8ce16cf498cfb7405dd59589795
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-03 22:21:42 +0300

    Homework Lesson 11 Level 3 Task 15 AuthorSearchCriteria

---
+++ src/student_valerija_ionova/lesson_11/level_3/task_15/AuthorSearchCriteria.java
@@ -0,0 +1,19 @@
+package student_valerija_ionova.lesson_11.level_3.task_15;
+
+class AuthorSearchCriteria {
+
+    private String authorToSearch;
+
+    public AuthorSearchCriteria(String authorToSearch) {
+        this.authorToSearch = authorToSearch;
+    }
+
+    public boolean match(Book book) {
+        if (book.getAuthor().equals(authorToSearch)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
