077ce39e24427596cecff22f47c317f8f661f936
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-03 21:35:17 +0300

    Homework Lesson 11 Level 1 Task 8-9 BookDatabaseImplTest

--- src/student_valerija_ionova/lesson_11/level_2/task_6_7/BookDatabaseImplTest.java
+++ src/student_valerija_ionova/lesson_11/level_2/task_6_7/BookDatabaseImplTest.java
@@ -2,6 +2,7 @@ package student_valerija_ionova.lesson_11.level_2.task_6_7;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 class BookDatabaseImplTest {
 
@@ -15,7 +16,11 @@ class BookDatabaseImplTest {
         bookDatabaseImplTest.testRemoveBookTrue();
         bookDatabaseImplTest.testRemoveBookFalse();
         bookDatabaseImplTest.testRemoveBookListCheck();
-
+        bookDatabaseImplTest.testRemoveBookByBookTrue();
+        bookDatabaseImplTest.testRemoveBookByBookFalse();
+        bookDatabaseImplTest.testRemoveBookByBookListCheck();
+        bookDatabaseImplTest.testFindByIdTrue();
+        bookDatabaseImplTest.testFindByIdFalse();
 
     }
 
@@ -107,6 +112,85 @@ class BookDatabaseImplTest {
         //bookDatabase.getBookDataBase().forEach(System.out :: println);
         //listForCheck.forEach(System.out :: println);
         checkResult("testRemoveBook", bookDatabase.getBookDataBase().equals(listForCheck));
+    }
+
+    void testRemoveBookByBookTrue(){
+
+        Book book1 = new Book ("Author1", "Title1");
+        Book book2 = new Book ("Author2", "Title2");
+        Book book3 = new Book ("Author3", "Title3");
+
+        BookDatabaseImpl bookDatabase = new BookDatabaseImpl();
+        bookDatabase.save(book1);
+        bookDatabase.save(book2);
+        bookDatabase.save(book3);
+
+        Book bookToDelete = new Book ("Author1", "Title1");
+
+
+        checkResult("testRemoveBookByBookTrue", bookDatabase.delete(bookToDelete));
+    }
+
+    void testRemoveBookByBookFalse(){
+
+        BookDatabaseImpl bookDatabase = new BookDatabaseImpl();
+
+        Book bookToDelete = new Book ("Author1", "Title1");
+
+        checkResult("testRemoveBookFalse", !bookDatabase.delete(bookToDelete));
+    }
+
+    void testRemoveBookByBookListCheck(){
+
+        Book book1 = new Book ("Author1", "Title1");
+        Book book2 = new Book ("Author2", "Title2");
+        Book book3 = new Book ("Author3", "Title3");
+
+        BookDatabaseImpl bookDatabase = new BookDatabaseImpl();
+        bookDatabase.save(book1);
+        bookDatabase.save(book2);
+        bookDatabase.save(book3);
+
+        List<BookAndId> listForCheck = new ArrayList<>();
+        BookAndId bookId1 = new BookAndId(book1, 1L);
+        BookAndId bookId3 = new BookAndId(book3, 3L);
+        listForCheck.add(bookId1);
+        listForCheck.add(bookId3);
+
+        Book bookToDelete = new Book ("Author2", "Title2");
+
+        bookDatabase.delete(bookToDelete);
+        checkResult("testRemoveBook", bookDatabase.getBookDataBase().equals(listForCheck));
+    }
+
+    void testFindByIdTrue(){
+
+        Book book1 = new Book ("Author1", "Title1");
+        Book book2 = new Book ("Author2", "Title2");
+        Book book3 = new Book ("Author3", "Title3");
+
+        BookDatabaseImpl bookDatabase = new BookDatabaseImpl();
+        bookDatabase.save(book1);
+        bookDatabase.save(book2);
+        bookDatabase.save(book3);
+
+        Optional<Book> result = Optional.of(book3);
+
+        checkResult("testFindByIdTrue", bookDatabase.findById(3L).equals(result));
+    }
+
+    void testFindByIdFalse(){
+
+        Book book1 = new Book ("Author1", "Title1");
+        Book book2 = new Book ("Author2", "Title2");
+        Book book3 = new Book ("Author3", "Title3");
+
+        BookDatabaseImpl bookDatabase = new BookDatabaseImpl();
+        bookDatabase.save(book1);
+        bookDatabase.save(book2);
+        bookDatabase.save(book3);
+
+        checkResult("testFindByIdFalse", bookDatabase.findById(15L).equals(Optional.empty()));
     }
 
 }
