af5d0d3890a253f9c3943369cc80dcf31c58957c
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-03 23:35:41 +0300

    Homework Lesson 11 Level 3 Task 22 SearchCriteriaTest plus SearchCriteria

--- src/student_valerija_ionova/lesson_11/level_3/task_15/SearchCriteriaTest.java
+++ src/student_valerija_ionova/lesson_11/level_3/task_15/SearchCriteriaTest.java
@@ -1,6 +1,9 @@
 package student_valerija_ionova.lesson_11.level_3.task_15;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 class SearchCriteriaTest {
 
         public static void main (String [] args){
@@ -19,6 +22,9 @@ class SearchCriteriaTest {
 
             authorSearchCriteriaTest.testOrSearchCriteriaTrue();
             authorSearchCriteriaTest.testOrSearchCriteriaFalse();
+
+            authorSearchCriteriaTest.testFindSearchCriteriaOr();
+            authorSearchCriteriaTest.testFindSearchCriteriaAnd();
 
         }
 
@@ -122,6 +128,57 @@ class SearchCriteriaTest {
 
         OrSearchCriteria orSearchCriteria = new OrSearchCriteria(searchCriteria1, searchCriteria2);
         checkResult("testOrSearchCriteriaFalse", !orSearchCriteria.match(book1));
+    }
+
+    void testFindSearchCriteriaOr(){
+        Book book1 = new Book ("Author1", "Title1", "2009");
+        Book book2 = new Book ("Author2", "Title2", "2001");
+        Book book3 = new Book ("Author3", "Title3", "2007");
+        Book book4 = new Book ("Author2", "Title4", "2008");
+
+        BookDatabaseImpl bookDatabase = new BookDatabaseImpl();
+        bookDatabase.save(book1);
+        bookDatabase.save(book2);
+        bookDatabase.save(book3);
+        bookDatabase.save(book4);
+
+
+        List <Book> listForCheck = new ArrayList<>();
+        listForCheck.add(book1);
+        listForCheck.add(book2);
+        listForCheck.add(book4);
+
+        SearchCriteria searchCriteria1 = new TitleSearchCriteria("Title1");
+        SearchCriteria searchCriteria2 = new AuthorSearchCriteria("Author2");
+
+        OrSearchCriteria orSearchCriteria = new OrSearchCriteria(searchCriteria1, searchCriteria2);
+
+        checkResult("testFindSearchCriteriaOr", bookDatabase.find(orSearchCriteria).equals(listForCheck));
+    }
+
+    void testFindSearchCriteriaAnd(){
+
+        Book book1 = new Book ("Author1", "Title1", "2009");
+        Book book2 = new Book ("Author1", "Title2", "2001");
+        Book book3 = new Book ("Author3", "Title3", "2007");
+        Book book4 = new Book ("Author1", "Title4", "2009");
+
+        BookDatabaseImpl bookDatabase = new BookDatabaseImpl();
+        bookDatabase.save(book1);
+        bookDatabase.save(book2);
+        bookDatabase.save(book3);
+        bookDatabase.save(book4);
+
+        List <Book> listForCheck = new ArrayList<>();
+        listForCheck.add(book1);
+        listForCheck.add(book4);
+
+        SearchCriteria searchCriteria1 = new AuthorSearchCriteria("Author1");
+        SearchCriteria searchCriteria2 = new YearOfIssueSearchCriteria("2009");
+
+        AndSearchCriteria andSearchCriteria = new AndSearchCriteria(searchCriteria1, searchCriteria2);
+
+        checkResult("testFindSearchCriteriaAnd", bookDatabase.find(andSearchCriteria).equals(listForCheck));
     }
 
 }
