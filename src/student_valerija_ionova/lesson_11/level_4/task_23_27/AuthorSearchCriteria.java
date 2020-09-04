0431e154e91732b7591a8a9d6f23c5e7065ab390
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-04 17:35:42 +0300

    Homework Lesson 11 Level 4 Task 23-27 Refactoring

--- src/student_valerija_ionova/lesson_11/level_4/task_23_27/AuthorSearchCriteria.java
+++ src/student_valerija_ionova/lesson_11/level_4/task_23_27/AuthorSearchCriteria.java
@@ -1,4 +1,4 @@
-package student_valerija_ionova.lesson_11.level_4;
+package student_valerija_ionova.lesson_11.level_4.task_23_27;
 
 //Task16 Создайте класс AuthorSearchCriteria:
 
--- src/student_valerija_ionova/lesson_11/level_4/task_23_27/SearchCriteria.java
+++ src/student_valerija_ionova/lesson_11/level_4/task_23_27/SearchCriteria.java
@@ -1,4 +1,4 @@
-package student_valerija_ionova.lesson_11.level_4;
+package student_valerija_ionova.lesson_11.level_4.task_23_27;
 
 /*Task15
 Для представления сложных критериев поиска книг в базе данных создайте
