2172924871a2b9e806246e9b4ea97cff61d17abe
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-04 20:35:03 +0300

    Homework Lesson 11 Level 4 Task 29 BookDatabaseImplTest plus getEachAuthorBookCount

--- src/student_valerija_ionova/lesson_11/level_5/task_28_29/BookDatabaseTestTODO.java
+++ src/student_valerija_ionova/lesson_11/level_5/task_28_29/BookDatabaseTestTODO.java
@@ -1,4 +1,4 @@
-package student_valerija_ionova.lesson_11.level_5.task_28;
+package student_valerija_ionova.lesson_11.level_5.task_28_29;
 
 
 import java.util.*;
@@ -9,7 +9,8 @@ class BookDatabaseTestTODO {
 
             BookDatabaseTestTODO authorSearchCriteriaTest = new BookDatabaseTestTODO();
 
-            //authorSearchCriteriaTest.testGetAuthorToBooksMap();
+            authorSearchCriteriaTest.testGetAuthorToBooksMap();
+            authorSearchCriteriaTest.testGetAuthorsBookCount();
 
         }
 
@@ -54,6 +55,35 @@ class BookDatabaseTestTODO {
         listOfBooks.add(book6);
         result.put("Author20", listOfBooks);
 
+        System.out.println(bookDatabase.getAuthorToBooksMap());
+        //Как сравнить map не учитывая порядок??? :(
+        //checkResult("testContainsBookFalse",);
+    }
+
+    void testGetAuthorsBookCount(){
+
+        Book book1 = new Book("Author1", "Title1", "2009");
+        Book book2 = new Book("Author1", "Title8", "2009");
+        Book book3 = new Book("Author3", "Title45", "2007");
+        Book book4 = new Book("Author3", "Title3", "2005");
+        Book book5 = new Book("Author2", "Title4", "2009");
+        Book book6 = new Book("Author20", "Title4", "2009");
+
+        BookDatabaseImpl bookDatabase = new BookDatabaseImpl();
+        bookDatabase.save(book1);
+        bookDatabase.save(book2);
+        bookDatabase.save(book3);
+        bookDatabase.save(book4);
+        bookDatabase.save(book5);
+        bookDatabase.save(book6);
+
+        Map<String, Integer> result = new HashMap<>();
+        result.put("Author1", 2);
+        result.put("Author3", 2);
+        result.put("Author2", 1);
+        result.put("Author20", 1);
+
+        System.out.println(bookDatabase.getEachAuthorBookCount());
         //Как сравнить map не учитывая порядок??? :(
         //checkResult("testContainsBookFalse",);
     }
