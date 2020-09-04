95b972658111dbb1c703f928e5f771e0e0947ae2
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-04 17:34:29 +0300

    Homework Lesson 11 Level 4 Refactoring

---
+++ src/student_valerija_ionova/lesson_11/level_4/SearchCriteria.java
@@ -0,0 +1,13 @@
+package student_valerija_ionova.lesson_11.level_4;
+
+/*Task15
+Для представления сложных критериев поиска книг в базе данных создайте
+интерфейс SearchCriteria со следующим методом:
+*/
+
+
+interface SearchCriteria {
+
+    boolean match(Book book);
+
+}
