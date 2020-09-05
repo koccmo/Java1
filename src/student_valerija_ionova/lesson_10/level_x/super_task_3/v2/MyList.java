df91e996f9f6bb3189dee229c5de893777bae131
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-05 11:56:53 +0300

    Lesson 10 Level x Super Task 3 V2 MyList Interface

---
+++ src/student_valerija_ionova/lesson_10/level_x/super_task_3/v2/MyList.java
@@ -0,0 +1,15 @@
+package student_valerija_ionova.lesson_10.level_x.super_task_3.v2;
+
+interface MyList {
+
+    int sizeOfList();  //Размер списка
+
+    ElementOfLinkedList getElement (int index);  // Получить элемент листа
+
+    void addElement(int number); // Добавляет элемент в массив
+
+    void insertElement(int index, int number); // Вставляет элемент в середину массива
+
+    void deleteElement(int index); //Удаляет ячейку массива, сокращая его длину
+
+}
