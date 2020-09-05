20fc92ae1a8d678022f90333d4bed7eb9dfe68aa
Igors <igors.barkevics@gmail.com>
2020-09-05 14:06:12 +0300

    Lesson 11 Level 3 Task_17

---
+++ src/student_igors_bartkevics/lesson_11/homework/level_3/task_17/SearchCriteria.java
@@ -0,0 +1,7 @@
+package student_igors_bartkevics.lesson_11.homework.level_3.task_17;
+
+public interface SearchCriteria {
+
+    boolean match(Book book);
+
+}
---
+++ src/student_igors_bartkevics/lesson_11/homework/level_3/task_17/TitleSearchCriteriaTest.java
@@ -0,0 +1,33 @@
+package student_igors_bartkevics.lesson_11.homework.level_3.task_17;
+
+class TitleSearchCriteriaTest {
+
+    public static void main(String[] args) {
+        TitleSearchCriteriaTest test = new TitleSearchCriteriaTest();
+        test.matchPositiveTest();
+        test.matchNegativeTest();
+    }
+
+    void matchPositiveTest() {
+        TitleSearchCriteria titleSearchCriteria = new TitleSearchCriteria("Clean Code - A Handbook of Agile Software Craftsmanship");
+        Book book = new Book("Martin R.", "Clean Code - A Handbook of Agile Software Craftsmanship");
+        boolean condition = titleSearchCriteria.match(book);
+        checkResult(condition, "Match positive");
+    }
+
+    void matchNegativeTest() {
+        TitleSearchCriteria titleSearchCriteria = new TitleSearchCriteria("Clean Code - A Handbook of Agile Software Craftsmanship");
+        Book book = new Book("Martin R.", "Clean Code - A Handbook of Agile Software Craftsmanship rev 3");
+        boolean condition = !titleSearchCriteria.match(book);
+        checkResult(condition, "Match negative");
+    }
+
+    public void checkResult(boolean condition, String testName) {
+        if (condition) {
+            System.out.println(testName + " test = OK");
+        } else {
+            System.out.println(testName + " test = FAIL");
+        }
+    }
+
+}
