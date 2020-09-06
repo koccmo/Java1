67199acaa079e24b544dbe4941fcaa3cc1a6a7a3
Igors <igors.barkevics@gmail.com>
2020-09-06 16:33:51 +0300

    Lesson 11 Level 4 task_23

---
+++ src/student_igors_bartkevics/lesson_11/homework/level_4_junior/task_23/BookDatabase.java
@@ -0,0 +1,30 @@
+package student_igors_bartkevics.lesson_11.homework.level_4_junior.task_23;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public interface BookDatabase {
+
+    Long save(Book book);
+
+    boolean delete(Long bookId);
+
+    boolean delete(Book book);
+
+    Optional<Book> findById(Long bookId);
+
+    List<Book> findByAuthor(String author);
+
+    List<Book> findByTitle(String title);
+
+    int countAllBooks();
+
+    void deleteByAuthor(String author);
+
+    void deleteByTitle(String title);
+
+    List<Book> find(SearchCriteria searchCriteria);
+
+    Set<String> findUniqueAuthors();
+}
