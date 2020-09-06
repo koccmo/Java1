a2a495d98da7a962af82413e6cfeb7a89b6fd8c7
Igors <igors.barkevics@gmail.com>
2020-09-06 16:43:48 +0300

    Lesson 11 Level 4 task_24

---
+++ src/student_igors_bartkevics/lesson_11/homework/level_4_junior/task_24/BookValidator.java
@@ -0,0 +1,12 @@
+package student_igors_bartkevics.lesson_11.homework.level_4_junior.task_24;
+
+class BookValidator {
+
+    boolean isBookByAuthor(Book book, String author) {
+        return (book.getAuthor().equals(author));
+    }
+
+    boolean isBookWithTitle(Book book, String title) {
+        return (book.getTitle().equals(title));
+    }
+}
