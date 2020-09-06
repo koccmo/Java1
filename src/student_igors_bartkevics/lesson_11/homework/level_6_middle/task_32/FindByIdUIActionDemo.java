09b9a5f05b4875aa90042cc3a7dc9ed830f51939
Igors <igors.barkevics@gmail.com>
2020-09-07 00:05:54 +0300

    Lesson 11 Level 6 task_32

---
+++ src/student_igors_bartkevics/lesson_11/homework/level_6_middle/task_32/FindByIdUIActionDemo.java
@@ -0,0 +1,25 @@
+package student_igors_bartkevics.lesson_11.homework.level_6_middle.task_32;
+
+class FindByIdUIActionDemo {
+
+    public static void main(String[] args) {
+
+        Book book1 = new Book("Bates B., Sierra K.", "Head First Java");
+        Book book2 = new Book("Martin R.", "Clean Code - A Handbook of Agile Software Craftsmanship");
+        Book book3 = new Book("Hunt A., Thomas D.", "The Pragmatic Programmer. From Journeyman to Master");
+        Book book4 = new Book("Martin R.", "Clean Code - A Handbook of Agile Software Craftsmanship rev 2");
+        Book book5 = new Book("Martin R.", "Clean Code - A Handbook of Agile Software Craftsmanship rev 3");
+
+        BookDatabase bookDatabase = new BookDatabaseImpl();
+        UIAction findByIdUIAction = new FindByIdUIAction(bookDatabase);
+        bookDatabase.save(book1);
+        bookDatabase.save(book2);
+        bookDatabase.save(book3);
+        bookDatabase.save(book4);
+        bookDatabase.save(book5);
+
+        findByIdUIAction.execute();
+
+    }
+
+}
---
+++ src/student_igors_bartkevics/lesson_11/homework/level_6_middle/task_32/SearchCriteria.java
@@ -0,0 +1,7 @@
+package student_igors_bartkevics.lesson_11.homework.level_6_middle.task_32;
+
+interface SearchCriteria {
+
+    boolean match(Book book);
+
+}
