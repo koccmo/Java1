b79c7bde5a8e94ae8b920834dcda5faf591384f8
Igors <igors.barkevics@gmail.com>
2020-09-06 21:03:04 +0300

    Lesson 11 Level 4 task_29

---
+++ src/student_igors_bartkevics/lesson_11/homework/level_5_middle/task_29/AndSearchCriteria.java
@@ -0,0 +1,19 @@
+package student_igors_bartkevics.lesson_11.homework.level_5_middle.task_29;
+
+class AndSearchCriteria implements SearchCriteria {
+
+    private SearchCriteria leftCondition;
+    private SearchCriteria rightCondition;
+
+    public AndSearchCriteria(SearchCriteria leftCondition,
+                             SearchCriteria rightCondition) {
+        this.leftCondition = leftCondition;
+        this.rightCondition = rightCondition;
+    }
+
+    @Override
+    public boolean match(Book book) {
+        return leftCondition.match(book) && rightCondition.match(book);
+    }
+
+}
---
+++ src/student_igors_bartkevics/lesson_11/homework/level_5_middle/task_29/TitleSearchCriteria.java
@@ -0,0 +1,15 @@
+package student_igors_bartkevics.lesson_11.homework.level_5_middle.task_29;
+
+class TitleSearchCriteria implements SearchCriteria {
+
+    private String titleToSearch;
+
+    public TitleSearchCriteria(String titleToSearch) {
+        this.titleToSearch = titleToSearch;
+    }
+
+    public boolean match(Book book) {
+        return book.getTitle().equals(titleToSearch);
+    }
+
+}
