1a866acf57987b2419315f9a8110a08587be6aeb
Dmitryyyyo <dmitrijs.ja@gmail.com>
2020-09-08 21:47:57 +0300

    Lesson_10_homework

---
+++ src/student_dmitrijs_jasvins/lesson_10/day_6/BookReaderTest.java
@@ -0,0 +1,42 @@
+package student_dmitrijs_jasvins.lesson_10.day_6;
+
+public class BookReaderTest {
+    public static void main(String[] args) {
+        BookReaderTest test = new BookReaderTest();
+        test.shouldReturnTrueIfBookIsInELibraryAndMarkedAsNotRead();
+        test.shouldReturnTrueIfBookIsInELibraryAndMarkedAsRead();
+        test.shouldReturnFalseIfBookIsNotInELibrary();
+    }
+
+    public void shouldReturnTrueIfBookIsInELibraryAndMarkedAsNotRead() {
+        BookReaderImpl bookReader = new BookReaderImpl();
+        Book book = new Book("Dmitry", "Dmitry Jasvins");
+        bookReader.addBook(book);
+        if (bookReader.MarkBookAsNotRead(book)) {
+            System.out.println("Test = OK");
+        } else {
+            System.out.println("Test = FAIL");
+        }
+    }
+    public void shouldReturnTrueIfBookIsInELibraryAndMarkedAsRead () {
+        BookReaderImpl bookReader = new BookReaderImpl();
+        Book book = new Book("Dmitry", "Dmity Jasvins");
+        bookReader.addBook(book);
+        if (bookReader.MarkBookAsAlreadyRead(book)){
+            System.out.println("Test = OK");
+        } else {
+            System.out.println("Test = FAIL");
+        }
+    }
+    public void shouldReturnFalseIfBookIsNotInELibrary () {
+        BookReaderImpl bookReader = new BookReaderImpl();
+        Book book = new Book("Dmitry", "Dmity Jasvins");
+        bookReader.addBook(book);
+        if (bookReader.MarkBookAsAlreadyRead(new Book("A", "B"))) {
+            System.out.println("Test = OK");
+        } else {
+            System.out.println("Test = FAIL");
+        }
+    }
+
+}
