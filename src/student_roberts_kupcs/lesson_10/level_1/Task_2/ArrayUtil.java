3eeee8ca0210fdbea7f4fb65fed5f869d1d9e459
Roberts@Rob <39665997+robku@users.noreply.github.com>
2020-09-07 22:34:54 -0700

    Lesson level 1 task 2

---
+++ src/student_roberts_kupcs/lesson_10/level_1/Task_2/ArrayUtil.java
@@ -0,0 +1,15 @@
+package student_roberts_kupcs.lesson_10.level_1.Task_2;
+
+interface ArrayUtil {
+
+    int[] createArray(int arrayLength);
+
+    void fillArrayWithRandomNumbers(int[] array);
+
+    void printArrayToConsole(int[] array);
+
+    int findMaxNumber(int[] array);
+
+    int findMinNumber(int[] array);
+
+}
