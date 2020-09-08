47546c41b1aead4628eef8a53882109924ced344
Alexey777555 <eiroartlv@inbox.lv>
2020-09-08 18:46:10 +0300

    Lesson 9,level 1

---
+++ src/student_alexey_tretyakov/lesson_9/homework/level_1/SafeBoxDemo.java
@@ -0,0 +1,12 @@
+package student_alexey_tretyakov.lesson_9.homework.level_1;
+
+class SafeBoxDemo {
+
+    public static void main(String[] args) {
+        SafeBox safeBox = new SafeBox();
+        System.out.println("Pin code is " + safeBox.pinCode);
+        System.out.println("Money in safe =  " + safeBox.boxMoney);
+
+    }
+
+}
