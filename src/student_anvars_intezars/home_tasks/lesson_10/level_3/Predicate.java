2c2d6415c8df0c041f92e74388cd3d0902342ab6
Anvar331 <anvar.intezar@gmail.com>
2020-09-08 10:37:44 +0300

    Lesson10/level3/Task6

---
+++ src/student_anvars_intezars/home_tasks/lesson_10/level_3/Predicate.java
@@ -0,0 +1,10 @@
+package student_anvars_intezars.home_tasks.lesson_10.level_3;
+
+interface Predicate<Number> {
+
+    boolean test (Number number);
+}
+
+/*Функциональный интерфейс - является интерфейсом у которого в наличии
+только один абстрактный метод
+*/
