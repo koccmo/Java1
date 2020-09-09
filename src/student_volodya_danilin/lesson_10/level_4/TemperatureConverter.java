76abb3f66ec44a2b67d671ceec39a81af046bc13
binco <vladimirs.danilins@gmail.com>
2020-09-09 14:40:11 +0300

    Домашние задания 10-4

---
+++ src/student_volodya_danilin/lesson_10/level_4/TemperatureConverter.java
@@ -0,0 +1,7 @@
+package student_volodya_danilin.lesson_10.level_4;
+
+interface TemperatureConverter {
+
+    double convert(double celsiusTemperature);
+
+}
---
+++ src/student_volodya_danilin/lesson_10/level_4/Test.java
@@ -0,0 +1,42 @@
+package student_volodya_danilin.lesson_10.level_4;
+
+import java.text.DecimalFormat;
+
+class Test {
+
+    public static void main(String[] args) {
+
+        double celsiusTemperature = 100;
+        double kelvinTemperature;
+        double fahrenheitTemperature;
+
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        KelvinConverter kelvin = new KelvinConverter();
+        kelvinTemperature = kelvin.convert(celsiusTemperature);
+
+        FahrenheitConverter fahrenheit = new FahrenheitConverter();
+        fahrenheitTemperature = fahrenheit.convert(celsiusTemperature);
+
+        System.out.println("Celsius : " + celsiusTemperature);
+        System.out.println("Kelvin : " + kelvinTemperature);
+        System.out.println("Fahrenheit : " + fahrenheitTemperature);
+
+        System.out.println();
+
+        if (celsiusTemperature + 273.15 == kelvinTemperature) {
+            System.out.println("Kelvin test OK");
+        }
+        else {
+            System.out.println("Kelvin test FAIL");
+        }
+
+        if (1.8 * celsiusTemperature + 32 == fahrenheitTemperature) {
+            System.out.println("Fahrenheit test OK");
+        }
+        else {
+            System.out.println("Fahrenheit test FAIL");
+        }
+    }
+
+}
