cc85f897e6bc5a09efd0c7e1d70ab472dce73aed
Igors <igors.barkevics@gmail.com>
2020-09-09 11:45:27 +0300

    Lesson 11 Level 6 task_34

---
+++ src/student_igors_bartkevics/lesson_11/homework/level_6_middle/task_34/Book.java
@@ -0,0 +1,54 @@
+package student_igors_bartkevics.lesson_11.homework.level_6_middle.task_34;
+
+import java.util.Objects;
+
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private String yearOfIssue;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    Book(String author, String title, String yearOfIssue) {
+        this.author = author;
+        this.title = title;
+        this.yearOfIssue = yearOfIssue;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getYearOfIssue() {
+        return yearOfIssue;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return title.equals(book.title) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author);
+    }
+}
---
+++ src/student_igors_bartkevics/lesson_11/homework/level_6_middle/task_34/UI/FindByAuthorUIAction.java
@@ -0,0 +1,34 @@
+package student_igors_bartkevics.lesson_11.homework.level_6_middle.task_34.UI;
+
+import student_igors_bartkevics.lesson_11.homework.level_6_middle.task_34.Book;
+import student_igors_bartkevics.lesson_11.homework.level_6_middle.task_34.BookDatabase;
+import student_igors_bartkevics.lesson_11.homework.level_6_middle.task_34.UIAction;
+
+import java.util.List;
+import java.util.Scanner;
+
+class FindByAuthorUIAction implements UIAction {
+
+    private BookDatabase bookDatabase;
+
+    public FindByAuthorUIAction(BookDatabase bookDatabase) {
+        this.bookDatabase = bookDatabase;
+    }
+
+    @Override
+    public void execute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter author");
+        String author = scanner.nextLine();
+        List<Book> booksByAuthor = bookDatabase.findByAuthor(author);
+        if (booksByAuthor.size() == 0) {
+            System.out.println("Books by author: '" + author + "' not found");
+        }
+        else {
+            System.out.println("Books by " + author + " :");
+            for (Book book : booksByAuthor) {
+                System.out.println(book.getTitle());
+            }
+        }
+    }
+}
