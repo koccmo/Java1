85c1331eea26c86d96b7095b2061118f858e4481
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-10 13:34:45 +0300

    Lesson 12 Level 3 Junior Task 18 NullPointerExceptionDemo

---
+++ src/student_valerija_ionova/lesson_12/level_3_junior/task_18/NullPointerExceptionDemo.java
@@ -0,0 +1,24 @@
+package student_valerija_ionova.lesson_12.level_3_junior.task_18;
+
+/*Создайте класс NullPointerExceptionDemo
+и в main() методе этого класса напишите код,
+который приводит к появлению NullPointerException.
+В комментариях напишите, когда происходит NullPointerException.
+
+NullPointerException
+Возникает при попытке обращения к полю, методу или объекту по ссылке,
+равной null. Также исключение выбрасывается, когда метод, не допускающий
+передачи аргумента null, был вызван с заданием значения null.
+В последнем случае может быть сгенерировано и исключение типа IllegalArgumentException.
+
+Класс NullPointerException унаследован от RuntimeException.*/
+
+class NullPointerExceptionDemo {
+
+    public static void main (String [] args) {
+
+        int [] test = null;
+
+        System.out.println(test[0]);
+    }
+}
