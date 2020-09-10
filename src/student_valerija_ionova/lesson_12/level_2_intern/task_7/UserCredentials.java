17a9bc07b4fa500821bf4ff4e12085a994fbd46c
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-10 12:28:17 +0300

    Lesson 12 Level 2 Intern Task 8 UserCredentials

---
+++ src/student_valerija_ionova/lesson_12/level_2_intern/task_7/UserCredentials.java
@@ -0,0 +1,24 @@
+package student_valerija_ionova.lesson_12.level_2_intern.task_7;
+
+/*Task8
+В теории и на практике у клиентов могут быть разные права
+и у одного клиента может быть много разных ролей.
+
+Для описания прав доступа, которые есть у того кто делает
+запрос к банковской системе создайте класс UserCredentials:*/
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class UserCredentials {
+
+    private List<Role> roles;
+
+    UserCredentials (Role...roles){
+        this.roles = new ArrayList<>(Arrays.asList(roles));
+    }
+    // созжайте конструктор класса, в котором вы получаете список ролей
+
+}
