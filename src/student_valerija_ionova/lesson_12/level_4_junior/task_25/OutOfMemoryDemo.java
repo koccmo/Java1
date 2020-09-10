aea6f7727e3a58d915275e5e583a9e4b63470b10
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-10 15:06:37 +0300

    Lesson 12 Level 4 Junior Task 25 OutOfMemoryDemo

---
+++ src/student_valerija_ionova/lesson_12/level_4_junior/task_25/OutOfMemoryDemo.java
@@ -0,0 +1,23 @@
+package student_valerija_ionova.lesson_12.level_4_junior.task_25;
+
+/*Напишите программу, которая вылетает с OutOfMemoryError.
+
+Подсказка: создайте много объектов, только не теряйте ссылки на них.
+Например положите много объектов в список.
+
+Память JVM не безгранична!*/
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class OutOfMemoryDemo {
+
+    public static void main (String [] args) {
+
+        List<Long> numbers = new ArrayList<>();
+        while (true) {
+            numbers.add(1258L);
+        }
+    }
+}
