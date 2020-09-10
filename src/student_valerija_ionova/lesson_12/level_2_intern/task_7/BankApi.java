e3080d7e5f059eee90300400b7bfc42d08de11ba
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-10 12:35:51 +0300

    Lesson 12 Level 2 Intern Task 10 BankApi

---
+++ src/student_valerija_ionova/lesson_12/level_2_intern/task_7/BankApi.java
@@ -0,0 +1,21 @@
+package student_valerija_ionova.lesson_12.level_2_intern.task_7;
+
+/*Task10
+В интерфейсе мы сможем чётко обозначить
+какую функциональность мы предоставляем (названия методов),
+какие входные параметры мы принимаем и что отдаём в качестве результата.
+Создайте интерфейс BankApi:
+
+
+Метод findByUid() должен возвращать ("кидать") ошибку
+если в переданных credentials нет роли Role.CAN_SEARCH_CLIENTS.
+
+*/
+
+import java.util.Optional;
+
+interface BankApi {
+
+    Optional<BankClient> findByUid(UserCredentials credentials, String uid) throws AccessDeniedException;
+
+}
