6c066f2a3e3e977fe6190844a60a8e591c22122a
Jaroslav <brutan.jaroslav@gmail.com>
2020-09-12 01:45:15 +0300

    Lesson 10 task 4 update

--- src/student_jaroslav_brutan/lesson_10/day_2/task_4/DayOfTheWeekDetectorArrayVersion.java
+++ src/student_jaroslav_brutan/lesson_10/day_2/task_4/DayOfTheWeekDetectorArrayVersion.java
@@ -2,7 +2,7 @@ package student_jaroslav_brutan.lesson_10.day_2.task_4;
 
 public class DayOfTheWeekDetectorArrayVersion implements DayOfTheWeekDetector{
 
-    public String detectDayName(int number){
+    public String detectDayName(int number) {
         String[] dayOfTheWeek = new String[7];
         dayOfTheWeek[0] = "Monday";
         dayOfTheWeek[1] = "Tuesday";
@@ -11,6 +11,10 @@ public class DayOfTheWeekDetectorArrayVersion implements DayOfTheWeekDetector{
         dayOfTheWeek[4] = "Friday";
         dayOfTheWeek[5] = "Saturday";
         dayOfTheWeek[6] = "Sunday";
-        return dayOfTheWeek[number - 1];
+        if (number < 1 || number > 7) {
+           return "Enter number from 1 to 7!";
+        } else {
+            return dayOfTheWeek[number - 1];
+        }
     }
 }
