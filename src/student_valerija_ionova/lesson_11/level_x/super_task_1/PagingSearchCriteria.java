7d1515b220283c0c50734012f97a812fc7d3c247
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-11 14:26:41 +0300

    Lesson11 Level x SuperTask1 rrr deleted

--- src/student_valerija_ionova/lesson_11/level_x/super_task_1/PagingSearchCriteria.java
+++
@@ -1,8 +0,0 @@
-package student_valerija_ionova.lesson_11.level_x.super_task_1;
-
-public class PagingSearchCriteria implements SearchCriteria{
-    @Override
-    public boolean match(Book book) {
-        return false;
-    }
-}
--- src/student_valerija_ionova/lesson_11/level_x/super_task_1/UIAction/UIAction.java
+++
@@ -1,9 +0,0 @@
-package student_valerija_ionova.lesson_11.level_x.super_task_1.UIAction;
-
-//Task 30
-
-interface UIAction {
-
-    void execute();
-
-}
