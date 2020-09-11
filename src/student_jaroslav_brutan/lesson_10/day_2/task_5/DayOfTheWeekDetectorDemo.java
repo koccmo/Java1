65784884973ed67286e198c9092aa05820c111b8
Jaroslav <brutan.jaroslav@gmail.com>
2020-09-12 01:38:55 +0300

    Lesson 10 task 5 update

--- src/student_jaroslav_brutan/lesson_10/day_2/task_5/DayOfTheWeekDetectorDemo.java
+++ src/student_jaroslav_brutan/lesson_10/day_2/task_5/DayOfTheWeekDetectorDemo.java
@@ -1,7 +1,9 @@
 package student_jaroslav_brutan.lesson_10.day_2.task_5;
 
 import student_jaroslav_brutan.lesson_10.day_2.task_4.DayOfTheWeekDetector;
+import student_jaroslav_brutan.lesson_10.day_2.task_4.DayOfTheWeekDetectorArrayVersion;
 import student_jaroslav_brutan.lesson_10.day_2.task_4.DayOfTheWeekDetectorIfVersion;
+import student_jaroslav_brutan.lesson_10.day_2.task_4.DayOfTheWeekDetectorSwitchVersion;
 
 import java.util.Scanner;
 
@@ -21,11 +23,17 @@ public class DayOfTheWeekDetectorDemo {
     }
 
     public static void main(String[] args) {
-        DayOfTheWeekDetector dayOfTheWeekDetector = new DayOfTheWeekDetectorIfVersion();
+        DayOfTheWeekDetector ifDetector = new DayOfTheWeekDetectorIfVersion();
+        DayOfTheWeekDetector switchDetector = new DayOfTheWeekDetectorSwitchVersion();
+        DayOfTheWeekDetector arrayDetector = new DayOfTheWeekDetectorArrayVersion();
 
-        DayOfTheWeekDetectorDemo dayOfTheWeekDetectorIf = new DayOfTheWeekDetectorDemo(dayOfTheWeekDetector);
+        DayOfTheWeekDetectorDemo dayOfTheWeekDetectorIf = new DayOfTheWeekDetectorDemo(ifDetector);
+        DayOfTheWeekDetectorDemo dayOfTheWeekDetectorSwitch = new DayOfTheWeekDetectorDemo(switchDetector);
+        DayOfTheWeekDetectorDemo dayOfTheWeekDetectorArray = new DayOfTheWeekDetectorDemo(arrayDetector);
 
         dayOfTheWeekDetectorIf.run();
+        dayOfTheWeekDetectorSwitch.run();
+        dayOfTheWeekDetectorArray.run();
     }
 
 
