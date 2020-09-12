ceb6e3742de347fae86c1c37ef5a9c181039965d
Jaroslav <brutan.jaroslav@gmail.com>
2020-09-13 00:32:57 +0300

    Lesson 10 task 6

---
+++ src/student_jaroslav_brutan/lesson_10/day_3/task_6/Example.java
@@ -0,0 +1,8 @@
+package student_jaroslav_brutan.lesson_10.day_3.task_6;
+
+public class Example {
+
+    public static void main(String[] args) {
+        Car bmw330 = new Car("BMW330", true, true);
+    }
+}
