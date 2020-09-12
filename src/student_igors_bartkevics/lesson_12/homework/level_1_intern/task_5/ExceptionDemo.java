916262e3d8a9e462b65d5f58676f462dd654beda
Igors <igors.barkevics@gmail.com>
2020-09-12 23:26:25 +0300

    Lesson 12 Level 1 Task_5

---
+++ src/student_igors_bartkevics/lesson_12/homework/level_1_intern/task_5/ExceptionDemo.java
@@ -0,0 +1,11 @@
+package student_igors_bartkevics.lesson_12.homework.level_1_intern.task_5;
+
+class ExceptionDemo {
+    public static void main(String[] args) {
+        Exception exception1 = new Exception();
+        Exception exception2 = new Exception("Specified detail message");
+        Throwable cause = new Throwable();
+        Exception exception3 = new Exception("Specified detail message", cause);
+        Exception exception4 = new Exception(cause);
+    }
+}
