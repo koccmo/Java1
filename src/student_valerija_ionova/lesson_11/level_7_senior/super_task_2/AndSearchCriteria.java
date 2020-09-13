b58e5da14f116f987274bb53c35486b49c050402
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-13 23:29:31 +0300

    Lesson 11 Level 7 Senior SuperTask 2 refactoring

---
+++ src/student_valerija_ionova/lesson_11/level_7_senior/super_task_2/AndSearchCriteria.java
@@ -0,0 +1,24 @@
+package student_valerija_ionova.lesson_11.level_7_senior.super_task_2;
+
+//Task19 Создайте класс AndSearchCriteria:
+
+public class AndSearchCriteria implements SearchCriteria {
+
+    private SearchCriteria leftCondition;
+    private SearchCriteria rightCondition;
+
+    public AndSearchCriteria(SearchCriteria leftCondition,
+                             SearchCriteria rightCondition) {
+        this.leftCondition = leftCondition;
+        this.rightCondition = rightCondition;
+    }
+
+    public boolean match(Book book) {
+        if (leftCondition.match(book) && rightCondition.match(book)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
