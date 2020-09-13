babd74ce760b7092c0f10b78fa756ae530ac16bb
Igors <igors.barkevics@gmail.com>
2020-09-13 08:40:14 +0300

    Lesson 12 Level 2 Task_8

---
+++ src/student_igors_bartkevics/lesson_12/homework/level_2_intern/UserCredentials.java
@@ -0,0 +1,13 @@
+package student_igors_bartkevics.lesson_12.homework.level_2_intern;
+
+import java.util.List;
+
+class UserCredentials {
+
+    private List<Role> roles;
+
+    // созжайте конструктор класса, в котором вы получаете список ролей
+    UserCredentials(List<Role> roles) {
+        this.roles = roles;
+    }
+}
