b4137aa9ba4f5cfb6dd6a5c7b8284f0dfd3d2216
Igors <igors.barkevics@gmail.com>
2020-09-13 18:56:00 +0300

    Lesson 12 Level 5 Task_34

--- src/student_igors_bartkevics/lesson_12/homework/level_5_middle/Product.java
+++ src/student_igors_bartkevics/lesson_12/homework/level_5_middle/Product.java
@@ -26,25 +26,25 @@ import java.util.Objects;
 class Product {
 
     // создайте нужные свойства класса
-    private String name;
+    private String title;
     private Integer price;
     private String description;
 
     // создайте конструктор класса
-    public Product(String name, Integer price, String description) {
-        this.name = name;
+    public Product(String title, Integer price, String description) {
+        this.title = title;
         this.price = price;
         this.description = description;
     }
 
     // создайте get() set() методы
 
-    public String getName() {
-        return name;
+    public String getTitle() {
+        return title;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public Integer getPrice() {
@@ -72,21 +72,21 @@ class Product {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Product product = (Product) o;
-        return Objects.equals(name, product.name) &&
+        return Objects.equals(title, product.title) &&
                 Objects.equals(price, product.price) &&
                 Objects.equals(description, product.description);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, price, description);
+        return Objects.hash(title, price, description);
     }
 
     //      toString()
     @Override
     public String toString() {
         return "Product{" +
-                "name='" + name + '\'' +
+                "name='" + title + '\'' +
                 ", price=" + price +
                 ", description='" + description + '\'' +
                 '}';
