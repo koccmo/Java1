fb69c79d2a162364fc4cd2eb3c5fab75bc0d6081
Igors <igors.barkevics@gmail.com>
2020-09-13 08:38:20 +0300

    Lesson 12 Level 2 Task_7

---
+++ src/student_igors_bartkevics/lesson_12/homework/level_2_intern/Role.java
@@ -0,0 +1,9 @@
+package student_igors_bartkevics.lesson_12.homework.level_2_intern;
+
+enum Role {
+
+    CAN_SEARCH_CLIENTS  // эта роль должна быть у клиента, для того
+    // что бы он имел возможность делать поиск клиентов
+    // в базе данных банка
+
+}
