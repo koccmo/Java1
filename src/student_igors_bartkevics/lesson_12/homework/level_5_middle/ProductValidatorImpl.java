1f13d992b951136dcf39dcfd8248b27bebb19113
Igors <igors.barkevics@gmail.com>
2020-09-13 15:03:17 +0300

    Lesson 12 Level 5 Task_34

---
+++ src/student_igors_bartkevics/lesson_12/homework/level_5_middle/ProductValidatorImpl.java
@@ -0,0 +1,10 @@
+package student_igors_bartkevics.lesson_12.homework.level_5_middle;
+
+import java.util.List;
+
+class ProductValidatorImpl implements ProductValidator{
+    @Override
+    public List<ValidationException> validate(Product product) {
+        return null;
+    }
+}
