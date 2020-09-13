e788efc22a1e8aa760dd7a8bd4218f7c41c880a5
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-13 22:08:36 +0300

    Lesson 11 Level 7 Senior SuperTask1 refactoring

---
+++ src/student_valerija_ionova/lesson_11/level_7_senior/super_task_1/UIAction/FindUniqueTitlesUIAction.java
@@ -0,0 +1,18 @@
+package student_valerija_ionova.lesson_11.level_7_senior.super_task_1.UIAction;
+
+import student_valerija_ionova.lesson_11.level_7_senior.super_task_1.BookDatabase;
+
+public class FindUniqueTitlesUIAction implements UIAction {
+
+    BookDatabase bookDatabase;
+
+    FindUniqueTitlesUIAction(BookDatabase bookDatabase){
+        this.bookDatabase = bookDatabase;
+    }
+
+
+    @Override
+    public void execute() {
+        System.out.println("We have books with following titles in database: \n" + bookDatabase.findUniqueTitles());
+    }
+}
---
+++ src/student_valerija_ionova/lesson_11/level_7_senior/super_task_1/UIAction/PagingSearchCriteria.java
@@ -0,0 +1,13 @@
+package student_valerija_ionova.lesson_11.level_7_senior.super_task_1.UIAction;
+
+import student_valerija_ionova.lesson_11.level_7_senior.super_task_1.Book;
+import student_valerija_ionova.lesson_11.level_7_senior.super_task_1.SearchCriteria;
+
+public class PagingSearchCriteria implements SearchCriteria {
+
+    @Override
+    public boolean match(Book book) {
+        return true;
+    }
+}
+
