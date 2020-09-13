ddb76c8fe9c51da39695938cd64cd30012004668
Igors <igors.barkevics@gmail.com>
2020-09-13 14:39:36 +0300

    Lesson 12 Level 5 Task_28

---
+++ src/student_igors_bartkevics/lesson_12/homework/level_5_middle/Product.java
@@ -0,0 +1,95 @@
+/*/
+Разработку функциональности можно начать с создания доменной модели.
+Доменная модель - это классы, которые описывают данные с которыми
+будет работать компьютерная программа.
+
+В нашем случае речь идёт о продукте.
+
+Поэтому создаём класс Product:
+
+class Product {
+
+    // создайте нужные свойства класса
+    // создайте конструктор класса
+    // создайте get() set() методы
+    // переопределите методы:
+    //      equals()
+    //      hashCode()
+    //      toString()
+
+}
+ */
+package student_igors_bartkevics.lesson_12.homework.level_5_middle;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+class Product {
+
+    // создайте нужные свойства класса
+    private String name;
+    private BigDecimal price;
+    private String description;
+
+    // создайте конструктор класса
+    public Product(String name, BigDecimal price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    // создайте get() set() методы
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // переопределите методы:
+    //      equals()
+    //      hashCode()
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name) &&
+                Objects.equals(price, product.price) &&
+                Objects.equals(description, product.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, description);
+    }
+
+    //      toString()
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
