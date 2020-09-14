d5a618bd0ace3dc4e221836e3bc2d712f66fbd91
Jaroslav <brutan.jaroslav@gmail.com>
2020-09-14 23:01:45 +0300

    Lesson 10 task 12

---
+++ src/student_jaroslav_brutan/lesson_10/day_4/CelsiusConverter.java
@@ -0,0 +1,9 @@
+package student_jaroslav_brutan.lesson_10.day_4;
+
+public class CelsiusConverter implements TemperatureConverter{
+
+    @Override
+    public double convert(double celsiusTemperature){
+        return celsiusTemperature;
+    }
+}
