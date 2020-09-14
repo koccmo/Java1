75c5f0e44a8f9d6b8e6a2f8be0b4045596cc1f90
pavels-s <pavels.sarkels@gmail.com>
2020-09-14 21:09:24 +0300

    Lesson 10, level 1, task 2

---
+++ src/student_pavel_sharkel/lesson_10/level_1/task_2/ArrayUtil.java
@@ -0,0 +1,14 @@
+package student_pavel_sharkel.lesson_10.level_1.task_2;
+
+public interface ArrayUtil {
+
+    int[] createArray(int arrayLength);
+
+    void fillArrayWithRandomNumbers(int[] array);
+
+    void printArrayToConsole(int[] array);
+
+    int findMaxNumber(int[] array);
+
+    int findMinNumber(int[] array);
+}
