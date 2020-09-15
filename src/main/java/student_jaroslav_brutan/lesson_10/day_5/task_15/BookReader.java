b5edd519ea1cf5afe82a95c38ebbc76679af446a
Jaroslav <brutan.jaroslav@gmail.com>
2020-09-15 23:05:35 +0300

    Lesson 10 day 6 update

--- src/main/java/student_jaroslav_brutan/lesson_10/day_5/task_15/BookReader.java
+++
@@ -1,8 +0,0 @@
-package student_jaroslav_brutan.lesson_10.day_5.task_15;
-
-import student_jaroslav_brutan.lesson_10.day_5.task_14.Book;
-
-public interface BookReader {
-
-    boolean addBook(Book book);
-}
