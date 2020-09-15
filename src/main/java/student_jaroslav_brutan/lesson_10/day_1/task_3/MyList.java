5884aede91b63f105f59ca5ab0ac0b928a64eac3
Jaroslav <brutan.jaroslav@gmail.com>
2020-09-15 22:19:33 +0300

    Lesson 10 task 3

---
+++ src/main/java/student_jaroslav_brutan/lesson_10/day_1/task_3/MyList.java
@@ -0,0 +1,10 @@
+package student_jaroslav_brutan.lesson_10.day_1.task_3;
+
+public interface MyList {
+
+    void addElement(int element); // создает новый элемент в листе
+
+    void removeElement(int element); // удаляет какой-то элемент из листа
+
+    boolean contains(int element); // проверяет, есть ли заданный эдемент в листе
+}
