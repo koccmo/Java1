ee26872917d1180fefd62d21ded6fb06df21e65b
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-15 22:49:29 +0300

    TaxCalculatorImplTest

---
+++ src/test/java/student_valerija_ionova/lesson_13/level_1_intern/TaxCalculatorImplTest.java
@@ -0,0 +1,41 @@
+package student_valerija_ionova.lesson_13.level_1_intern;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+public class TaxCalculatorImplTest {
+
+
+    private TaxCalculator taxCalculator = new TaxCalculatorImpl();
+
+    @Test
+    public void testFor0Income() {
+        var income = BigDecimal.ZERO;
+        var taxToPay = taxCalculator.calculateTax(income);
+        assertEquals(taxToPay, new BigDecimal("0.00"));
+    }
+
+    @Test
+    public void lowTaxWhenIncomeUnder20000() {
+        var income = new BigDecimal(10000);
+        var taxToPay = taxCalculator.calculateTax(income);
+        assertEquals(taxToPay, new BigDecimal("2500.00"));
+    }
+
+    @Test
+    public void lowTaxWhenIncome20000() {
+        var income = new BigDecimal(20000);
+        var taxToPay = taxCalculator.calculateTax(income);
+        assertEquals(taxToPay, new BigDecimal("5000.00"));
+    }
+
+    @Test
+    public void highTaxWhenIncomeBiggerThen20000() {
+        var income = new BigDecimal(30000);
+        var taxToPay = taxCalculator.calculateTax(income);
+        assertEquals(taxToPay, new BigDecimal("9000.00"));
+    }
+}
