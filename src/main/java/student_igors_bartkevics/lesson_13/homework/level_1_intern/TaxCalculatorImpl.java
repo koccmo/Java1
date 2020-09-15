7683d21e6beaa86e0359efacf9774267fb3a1eda
Igors <igors.barkevics@gmail.com>
2020-09-15 22:59:47 +0300

    Lesson 13 Lesson 1 Task_2

---
+++ src/main/java/student_igors_bartkevics/lesson_13/homework/level_1_intern/TaxCalculatorImpl.java
@@ -0,0 +1,12 @@
+package student_igors_bartkevics.lesson_13.homework.level_1_intern;
+
+import java.math.BigDecimal;
+
+class TaxCalculatorImpl implements TaxCalculator{
+
+
+    @Override
+    public BigDecimal calculateTax(BigDecimal income) {
+        return null;
+    }
+}
