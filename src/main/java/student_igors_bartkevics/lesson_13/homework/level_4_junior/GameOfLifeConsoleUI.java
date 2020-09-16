82bb4d623b265663e156bbb13d8305f50c5250b9
Igors <igors.barkevics@gmail.com>
2020-09-16 23:53:58 +0300

    Lesson 13 Level 4 Task_24

--- src/main/java/student_igors_bartkevics/lesson_13/homework/level_4_junior/GameOfLifeConsoleUI.java
+++ src/main/java/student_igors_bartkevics/lesson_13/homework/level_4_junior/GameOfLifeConsoleUI.java
@@ -1,9 +1,19 @@
 package student_igors_bartkevics.lesson_13.homework.level_4_junior;
 
+
+
 class GameOfLifeConsoleUI {
 
     public void show(boolean[][] field) {
 
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j ++) {
+                if (field[i][j]) {
+                    System.out.print("+  ");
+                } else System.out.print("o  ");
+            }
+            System.out.print("\n");
+        }
     }
 
 }
