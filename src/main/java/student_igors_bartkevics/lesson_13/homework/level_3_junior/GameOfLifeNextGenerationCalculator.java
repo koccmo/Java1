8cedf7e3194d76095337a628d7cf37449899a6cb
Igors <igors.barkevics@gmail.com>
2020-09-16 13:53:15 +0300

    Lesson 13 Level 2 Task_10

---
+++ src/main/java/student_igors_bartkevics/lesson_13/homework/level_3_junior/GameOfLifeNextGenerationCalculator.java
@@ -0,0 +1,9 @@
+package student_igors_bartkevics.lesson_13.homework.level_3_junior;
+
+class GameOfLifeNextGenerationCalculator {
+
+    public boolean[][] calculate(boolean[][] currentGeneration) {
+        return null; // реализацию напишем тут чуть позже
+    }
+
+}
