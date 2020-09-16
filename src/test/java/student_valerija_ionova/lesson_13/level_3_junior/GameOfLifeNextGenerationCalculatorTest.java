7921def109909cce048d198285b31ab3f6eef26d
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-16 16:45:44 +0300

    Lesson13 Level3 Task13 Test

--- src/test/java/student_valerija_ionova/lesson_13/level_3_junior/GameOfLifeNextGenerationCalculatorTest.java
+++ src/test/java/student_valerija_ionova/lesson_13/level_3_junior/GameOfLifeNextGenerationCalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 //Task11 Правило 1: Любая живая клетка с меньше чем двумя живыми соседями умирает в следующем поколении.
+//Правило 2: Любая живая клетка с больше чем тремя живыми соседями умирает в следующем поколении.
 
 public class GameOfLifeNextGenerationCalculatorTest {
 
@@ -11,22 +12,31 @@ public class GameOfLifeNextGenerationCalculatorTest {
 
     boolean[][] currentGeneration = {{false, true, false, true, false},
                                      {false, false, false,true, false},
-                                     {true, false, false, false, false},
-                                     {true, false, false, false, false},
+                                     {true, true, false, false, false},
+                                     {true, true, false, false, false},
                                      {false, true, false, true, false}};
 
     @Test
     public void rule1FalseIfLessThanTwoAroundAreAlive1(){
-        boolean [][] newGenerationField = gameOfLifeNextGenerationCalculator.calculate(currentGeneration);
+        boolean [][] newGenerationField;
         newGenerationField = gameOfLifeNextGenerationCalculator.calculate(currentGeneration);
         assertEquals(false, newGenerationField[1][3]);
     }
 
     @Test
     public void rule1FalseIfLessThanTwoAroundAreAlive0(){
-        boolean [][] newGenerationField = gameOfLifeNextGenerationCalculator.calculate(currentGeneration);
+        boolean [][] newGenerationField;
         newGenerationField = gameOfLifeNextGenerationCalculator.calculate(currentGeneration);
         assertEquals(false, newGenerationField[4][3]);
     }
+
+    @Test
+    public void rule2FalseIfMoreThanThreeAroundAreAlive4(){
+        boolean [][] newGenerationField;;
+        newGenerationField = gameOfLifeNextGenerationCalculator.calculate(currentGeneration);
+        assertEquals(false, newGenerationField[3][0]);
+    }
+
+
 
 }
