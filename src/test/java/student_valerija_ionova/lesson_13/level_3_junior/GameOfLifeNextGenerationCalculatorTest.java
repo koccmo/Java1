ec446082ed81035d7638e922881a5ff952b2d41c
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-16 16:49:04 +0300

    Lesson13 Level3 Task15 Rule3 Test

--- src/test/java/student_valerija_ionova/lesson_13/level_3_junior/GameOfLifeNextGenerationCalculatorTest.java
+++ src/test/java/student_valerija_ionova/lesson_13/level_3_junior/GameOfLifeNextGenerationCalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 //Task11 Правило 1: Любая живая клетка с меньше чем двумя живыми соседями умирает в следующем поколении.
 //Правило 2: Любая живая клетка с больше чем тремя живыми соседями умирает в следующем поколении.
+//Task3 Правило 3: Любая живая клетка с двумя или тремя живыми соседями остаётся живой в следующем поколении.
 
 public class GameOfLifeNextGenerationCalculatorTest {
 
@@ -15,6 +16,20 @@ public class GameOfLifeNextGenerationCalculatorTest {
                                      {true, true, false, false, false},
                                      {true, true, false, false, false},
                                      {false, true, false, true, false}};
+
+    @Test
+    public void rule3TrueIfTwoAliveAround(){
+        boolean [][] newGenerationField;;
+        newGenerationField = gameOfLifeNextGenerationCalculator.calculate(currentGeneration);
+        assertEquals(true, newGenerationField[4][1]);
+    }
+
+    @Test
+    public void rule3TrueIfTThreeAliveAround(){
+        boolean [][] newGenerationField;;
+        newGenerationField = gameOfLifeNextGenerationCalculator.calculate(currentGeneration);
+        assertEquals(true, newGenerationField[2][1]);
+    }
 
     @Test
     public void rule1FalseIfLessThanTwoAroundAreAlive1(){
