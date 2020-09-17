743b87e14e36f8f02539097c0d0c93a0af2aee57
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-17 14:39:48 +0300

    Lesson11 Level 2-7 V2 After code Review Corrected BookDataBaseImplTest Tasks 6-13

--- src/test/java/student_valerija_ionova/lesson_11/level_2_7_v2/BookDatabaseImplTest.java
+++ src/test/java/student_valerija_ionova/lesson_11/level_2_7_v2/BookDatabaseImplTest.java
@@ -3,6 +3,8 @@ package student_valerija_ionova.lesson_11.level_2_7_v2;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
@@ -16,6 +18,7 @@ public class BookDatabaseImplTest {
     Book book3 = new Book("Author3", "Title3");
     Book book4 = new Book("Author4", "Title4");
     Book book5 = new Book("Author5", "Title5");
+    Book book6 = new Book("Author5", "Title4");
 
     @Before
     public void init (){
@@ -24,6 +27,7 @@ public class BookDatabaseImplTest {
         bookDatabase.save(book3);
         bookDatabase.save(book4);
         bookDatabase.save(book5);
+        bookDatabase.save(book6);
 
     }
 
@@ -31,7 +35,7 @@ public class BookDatabaseImplTest {
     public void testMethodDeleteById(){
         assertEquals(true, bookDatabase.delete(2L));
         assertEquals(false, bookDatabase.getListOfBooks().contains(book2));
-        assertEquals(4, bookDatabase.getListOfBooks().size());
+        assertEquals(5, bookDatabase.getListOfBooks().size());
         assertEquals(false, bookDatabase.delete(2L));
     }
 
@@ -39,14 +43,61 @@ public class BookDatabaseImplTest {
     public void testMethodDeleteByBook(){
         assertEquals(true, bookDatabase.delete(book1));
         assertEquals(false, bookDatabase.getListOfBooks().contains(book1));
-        assertEquals(4, bookDatabase.getListOfBooks().size());
+        assertEquals(5, bookDatabase.getListOfBooks().size());
     }
 
     @Test
     public void testMethodFindById(){
         assertEquals(Optional.of(book3), bookDatabase.findById(3L));
         assertEquals(Optional.empty(), bookDatabase.findById(55L));
-        assertEquals(5, bookDatabase.getListOfBooks().size());
+        assertEquals(6, bookDatabase.getListOfBooks().size());
+    }
+
+    @Test
+    public void testMethodFindByAuthor(){
+        List<Book> expectedResult = new ArrayList<>();
+        expectedResult.add(book5);
+        expectedResult.add(book6);
+        List <Book> result;
+        result = bookDatabase.findByAuthor("Author5");
+        assertEquals(expectedResult, result);
+        assertEquals(6, bookDatabase.getListOfBooks().size());
+
+    }
+
+    @Test
+    public void testMethodFindByAuthorNothingFound(){
+        List<Book> expectedResult = new ArrayList<>();
+        List <Book> result;
+        result = bookDatabase.findByAuthor("Author555");
+        assertEquals(expectedResult, result);
+        assertEquals(6, bookDatabase.getListOfBooks().size());
+    }
+
+    @Test
+    public void testMethodFindByTitle(){
+        List<Book> expectedResult = new ArrayList<>();
+        expectedResult.add(book4);
+        expectedResult.add(book6);
+        List <Book> result;
+        result = bookDatabase.findByTitle("Title4");
+        assertEquals(expectedResult, result);
+        assertEquals(6, bookDatabase.getListOfBooks().size());
+
+    }
+
+    @Test
+    public void testMethodFindByTitleNothingFound(){
+        List<Book> expectedResult = new ArrayList<>();
+        List <Book> result;
+        result = bookDatabase.findByTitle("Title555");
+        assertEquals(expectedResult, result);
+        assertEquals(6, bookDatabase.getListOfBooks().size());
+    }
+
+    @Test
+    public void testMethodCountAllBooks(){
+        assertEquals(6, bookDatabase.countAllBooks());
     }
 
 }
