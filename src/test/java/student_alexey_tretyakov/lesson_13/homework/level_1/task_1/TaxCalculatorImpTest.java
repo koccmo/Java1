fc8ea914795ffe70dec18370cdd97b40d7e67747
Alexey777555 <eiroartlv@inbox.lv>
2020-09-17 12:43:47 +0300

    Lesson 13, task 3

---
+++ src/test/java/student_alexey_tretyakov/lesson_13/homework/level_1/task_1/TaxCalculatorImpTest.java
@@ -0,0 +1,32 @@
+package student_alexey_tretyakov.lesson_13.homework.level_1.task_1;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+public class TaxCalculatorImpTest {
+    TaxCalculatorImp taxCalculatorImp = new TaxCalculatorImp();
+
+    @Test
+    public void calculateTax() {
+        BigDecimal tax = taxCalculatorImp.calculateTax(BigDecimal.valueOf(10000));
+        assertEquals(0,tax.compareTo(BigDecimal.valueOf(2500)));
+    }
+    @Test
+    public void calculateTaxMax() {
+        BigDecimal tax = taxCalculatorImp.calculateTax(BigDecimal.valueOf(20000));
+        assertEquals(0,tax.compareTo(BigDecimal.valueOf(5000)));
+    }
+    @Test
+    public void calculateTaxBig() {
+        BigDecimal tax = taxCalculatorImp.calculateTax(BigDecimal.valueOf(40000));
+        assertNull(tax);
+    }
+    @Test
+    public void calculateTaxZero() {
+        BigDecimal tax = taxCalculatorImp.calculateTax(BigDecimal.valueOf( 0 ));
+        assertNull(tax);
+    }
+}
