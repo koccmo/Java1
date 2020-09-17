bffc10fb0c80abacc39b79fdf8f001358cd8b6f2
Roberts@Rob <39665997+robku@users.noreply.github.com>
2020-09-17 12:33:36 -0700

    Lesson 10 level 3 task 8

---
+++ src/main/java/student_roberts_kupcs/lesson_10/level_3/Task_8/Database.java
@@ -0,0 +1,22 @@
+package student_roberts_kupcs.lesson_10.level_3.Task_8;
+
+interface Database {
+    void save(Product product);
+
+    Product findByTitle(String productTitle);
+
+}
+
+
+class Product {
+
+    private String title;
+
+    public Product(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+}
