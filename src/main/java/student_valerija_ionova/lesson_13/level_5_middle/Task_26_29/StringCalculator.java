a8c5e732b523eb309d53b71647b907b330cb78bd
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-17 10:07:54 +0300

    Lesson13 Level 5 Middle Task 26-29 StringCalculator only correct input

--- src/main/java/student_valerija_ionova/lesson_13/level_5_middle/Task_26_29/StringCalculator.java
+++ src/main/java/student_valerija_ionova/lesson_13/level_5_middle/Task_26_29/StringCalculator.java
@@ -4,22 +4,21 @@ package student_valerija_ionova.lesson_13.level_5_middle.Task_26_29;
 //Task 26 Метод может принимать до двух чисел разделённые запятой и возвращает их сумму.
 //    Пример входной строки: “” or “1” or “1,2”. Для пустой строки сумма равна 0.
 
-import java.util.Arrays;
 
 class StringCalculator {
 
     int add (String numbers){
         String [] arrayOfNumber = numbers.split(",");
-        //System.out.println(Arrays.toString(arrayOfNumber));
         int sum = 0;
-        for (String number : arrayOfNumber){
-            try {
+        if (numbers.equals("")){
+            return 0;
+        }
+        else {
+            for (String number : arrayOfNumber) {
                 sum += Integer.parseInt(number);
-            }catch (NumberFormatException e){
 
             }
-
-            }
-        return sum;
+            return sum;
+        }
     }
 }
