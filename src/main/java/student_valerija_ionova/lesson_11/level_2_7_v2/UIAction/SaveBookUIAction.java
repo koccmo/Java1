d2f6bc9fbb811b2c21cd2976a1f0da806b652532
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-17 16:40:14 +0300

    Lesson11 Level 2-7 V2 Correct variant BookDataBase

---
+++ src/main/java/student_valerija_ionova/lesson_11/level_2_7_v2/UIAction/SaveBookUIAction.java
@@ -0,0 +1,33 @@
+package student_valerija_ionova.lesson_11.level_2_7_v2.UIAction;
+
+import student_valerija_ionova.lesson_11.level_7_senior.super_task_2.Book;
+import student_valerija_ionova.lesson_11.level_7_senior.super_task_2.BookDatabase;
+
+import java.util.Scanner;
+
+//Task 31 Создайте и реализуйте класс SaveBookUIAction:
+
+class SaveBookUIAction implements UIAction {
+
+    private BookDatabase bookDatabase;
+
+    public SaveBookUIAction(BookDatabase bookDatabase) {
+        this.bookDatabase = bookDatabase;
+    }
+
+    public void execute() {
+        System.out.println("Please enter book's author: ");
+        Scanner in = new Scanner(System.in);
+        String author = in.nextLine();
+
+        System.out.println("Please enter book's title: ");
+        String title = in.nextLine();
+
+        System.out.println("Please enter book's year of issue: ");
+        String yearOfIssue = in.nextLine();
+
+        Book newBook = new Book(author, title, yearOfIssue);
+        bookDatabase.save(newBook);
+    }
+
+}
