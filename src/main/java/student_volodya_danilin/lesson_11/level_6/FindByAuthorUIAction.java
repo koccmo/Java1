be1a931352a26c4306823dbc5e5c2f500add33a7
binco <vladimirs.danilins@gmail.com>
2020-09-18 19:50:15 +0300

    Домашние задания 11 урока

---
+++ src/main/java/student_volodya_danilin/lesson_11/level_6/FindByAuthorUIAction.java
@@ -0,0 +1,32 @@
+package student_volodya_danilin.lesson_11.level_6;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+class FindByAuthorUIAction implements UIAction{
+
+    private BookDatabase bookDatabase;
+
+    public FindByAuthorUIAction(BookDatabase bookDatabase) {
+        this.bookDatabase = bookDatabase;
+    }
+
+    public void execute() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter book's author : ");
+        String author = sc.nextLine();
+
+        List<Book> books = bookDatabase.findByAuthor(author);
+
+        if (!books.isEmpty()) {
+            for (Book value : books) {
+                value.printBookInfo();
+            }
+        }
+        else {
+            System.out.println("Book not found!");
+        }
+    }
+}
