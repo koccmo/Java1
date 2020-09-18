2566e44c2ccda00b9c091f8735701c05b153ffb0
pavels-s <pavels.sarkels@gmail.com>
2020-09-18 07:28:51 +0300

    Lesson 10, level , task 5

--- src/main/java/student_pavel_sharkel/lesson_10/level_2/task_4/DayOfTheWeekDetectorTest.java
+++
@@ -1,5 +0,0 @@
-package student_pavel_sharkel.lesson_10.level_2.task_4;
-
-public class DayOfTheWeekDetectorTest {
-
-}
--- src/test/java/student_pavel_sharkel/lesson_10/level_2/task_4_5/DayOfTheWeekDetectorTestTest.java
+++ src/test/java/student_pavel_sharkel/lesson_10/level_2/task_4_5/DayOfTheWeekDetectorTestTest.java
@@ -1,4 +1,4 @@
-package student_pavel_sharkel.lesson_10.level_2.task_4;
+package student_pavel_sharkel.lesson_10.level_2.task_4_5;
 
 import org.junit.Test;
 
