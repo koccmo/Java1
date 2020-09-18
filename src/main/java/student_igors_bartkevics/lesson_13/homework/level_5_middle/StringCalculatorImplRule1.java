30131967bf8c07bb00f49d86fbefe75925d51434
Igors <igors.barkevics@gmail.com>
2020-09-18 23:53:07 +0300

    Lesson 13 Level 5 Task_27

--- src/main/java/student_igors_bartkevics/lesson_13/homework/level_5_middle/StringCalculatorImplRule1.java
+++
@@ -1,9 +0,0 @@
-package student_igors_bartkevics.lesson_13.homework.level_5_middle;
-
-class StringCalculatorImplRule1 implements StringCalculator {
-
-    @Override
-    public int add(String numbers) {
-        return 0;
-    }
-}
---
+++ src/test/java/student_igors_bartkevics/lesson_13/homework/level_5_middle/StringCalculatorTest.java
@@ -0,0 +1,27 @@
+package student_igors_bartkevics.lesson_13.homework.level_5_middle;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void shouldReturnZeroInComplianceWithRule1() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(calculator.add(""), 0);
+    }
+
+    @Test
+    public void shouldReturnFiveInComplianceWithRule2() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(calculator.add("5"), 5);
+    }
+
+    @Test
+    public void shouldReturnElevenInComplianceWithRule3() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(calculator.add("2,9"), 11);
+    }
+
+}
