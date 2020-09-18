d5f2ec55841e1cd81ee4af5182f023c167c51115
Igors <igors.barkevics@gmail.com>
2020-09-18 12:52:32 +0300

    Lesson 13 Level 5 Task_26 rule 1

---
+++ src/main/java/student_igors_bartkevics/lesson_13/homework/level_5_middle/StringCalculatorImplRule1.java
@@ -0,0 +1,9 @@
+package student_igors_bartkevics.lesson_13.homework.level_5_middle;
+
+class StringCalculatorImplRule1 implements StringCalculator {
+
+    @Override
+    public int add(String numbers) {
+        return 0;
+    }
+}
