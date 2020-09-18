b54c4d1e05aa531b415f0ac22e7b007c3dcc6a2b
Igors <igors.barkevics@gmail.com>
2020-09-18 10:25:55 +0300

    Lesson 13 Level 4 update

--- src/main/java/student_igors_bartkevics/lesson_13/homework/level_4_junior/Cell.java
+++ src/main/java/student_igors_bartkevics/lesson_13/homework/level_4_junior/Cell.java
@@ -3,7 +3,6 @@ package student_igors_bartkevics.lesson_13.homework.level_4_junior;
 class Cell {
     private int coordinateX;
     private int getCoordinateY;
-    private boolean state;
 
     public Cell(int coordinateX, int getCoordinateY) {
         this.coordinateX = coordinateX;
@@ -18,7 +17,4 @@ class Cell {
         return getCoordinateY;
     }
 
-    public boolean isState() {
-        return state;
-    }
 }
