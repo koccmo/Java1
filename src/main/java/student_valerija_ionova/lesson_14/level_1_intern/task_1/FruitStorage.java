9016a00733ca221cd04ab86d426a82beab318e3a
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-19 17:29:09 +0300

    Lesson 14 Level 1 Task 1 FruitStorage

---
+++ src/main/java/student_valerija_ionova/lesson_14/level_1_intern/task_1/FruitStorage.java
@@ -0,0 +1,30 @@
+package student_valerija_ionova.lesson_14.level_1_intern.task_1;
+
+//Task1 найти все зеленые яблоки в своем складе.
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class FruitStorage {
+
+    public List<Apple> getAllApples() {
+        List<Apple> apples = new ArrayList<>();
+        apples.add(new Apple("red", 100));
+        apples.add(new Apple("red", 160));
+        apples.add(new Apple("red", 200));
+        apples.add(new Apple("green", 200));
+        apples.add(new Apple("green", 90));
+        apples.add(new Apple("green", 50));
+        apples.add(new Apple("yellow", 50));
+        apples.add(new Apple("yellow", 170));
+        return apples;
+    }
+
+    List <Apple> getAllApplesByColor(String color){
+        return getAllApples().stream()
+                             .filter(apple -> apple.getColor().equals(color))
+                             .collect(Collectors.toList());
+    }
+
+}
