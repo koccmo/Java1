9ba6e1959f504aa70ce15f009fdc3c77e7577228
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-19 18:23:45 +0300

    Lesson 14 Level 2 Task 16-17 Lambda

---
+++ src/main/java/student_valerija_ionova/lesson_14/level_2_intern/task_16_17/Apple.java
@@ -0,0 +1,23 @@
+package student_valerija_ionova.lesson_14.level_2_intern.task_16_17;
+
+//Task 1
+
+class Apple {
+
+    private String color;
+    private int weight;
+
+    public Apple (String color, int weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+}
---
+++ src/main/java/student_valerija_ionova/lesson_14/level_2_intern/task_16_17/ApplePredicate.java
@@ -0,0 +1,10 @@
+package student_valerija_ionova.lesson_14.level_2_intern.task_16_17;
+
+//Task7
+
+@FunctionalInterface
+interface ApplePredicate {
+
+    boolean test (Apple apple);
+
+}
