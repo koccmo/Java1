910a2b8cfaf53b56888c244092376e4218f61c92
Jaroslav <brutan.jaroslav@gmail.com>
2020-09-20 08:15:35 +0300

    Lesson 11 task 9

--- src/main/java/student_jaroslav_brutan/lesson_11/day_2/task_6/BookDatabase.java
+++ src/main/java/student_jaroslav_brutan/lesson_11/day_2/task_6/BookDatabase.java
@@ -1,5 +1,7 @@
 package student_jaroslav_brutan.lesson_11.day_2.task_6;
 
+import java.util.Optional;
+
 public interface BookDatabase {
 
     Long save(Book book);
@@ -7,4 +9,6 @@ public interface BookDatabase {
     boolean delete(Long bookId);
 
     boolean delete(Book book);
+
+    Optional<Book> findById(Long bookId);
 }
