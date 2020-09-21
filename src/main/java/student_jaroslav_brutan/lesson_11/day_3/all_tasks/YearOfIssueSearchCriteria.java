cec4c34a14d5ef5b409bac4b02bd634e40ac2670
Jaroslav <brutan.jaroslav@gmail.com>
2020-09-21 23:07:43 +0300

    Lesson 11 day 3 task 18

---
+++ src/main/java/student_jaroslav_brutan/lesson_11/day_3/all_tasks/YearOfIssueSearchCriteria.java
@@ -0,0 +1,16 @@
+package student_jaroslav_brutan.lesson_11.day_3.all_tasks;
+
+public class YearOfIssueSearchCriteria implements SearchCriteria{
+
+    private String yearOfIssueToSearch;
+
+    public YearOfIssueSearchCriteria(String yearOfIssueToSearch) {
+        this.yearOfIssueToSearch = yearOfIssueToSearch;
+    }
+
+    public boolean match(Book book){
+        if (book.getYearOfIssue().equals(yearOfIssueToSearch)){
+            return true;
+        } return false;
+    }
+}
