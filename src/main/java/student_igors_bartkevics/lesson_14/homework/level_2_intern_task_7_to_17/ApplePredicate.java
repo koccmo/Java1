1c32078989921142a60548a6ba9d62375c8d2078
Igors <igors.barkevics@gmail.com>
2020-09-22 00:19:19 +0300

    Lesson 14 Level 2 Task_17

--- src/main/java/student_igors_bartkevics/lesson_14/homework/level_2_intern_task_7_to_17/ApplePredicate.java
+++ src/main/java/student_igors_bartkevics/lesson_14/homework/level_2_intern_task_7_to_17/ApplePredicate.java
@@ -1,4 +1,4 @@
-package student_igors_bartkevics.lesson_14.homework.level_2_intern;
+package student_igors_bartkevics.lesson_14.homework.level_2_intern_task_7_to_17;
 
 @FunctionalInterface
 interface ApplePredicate {
