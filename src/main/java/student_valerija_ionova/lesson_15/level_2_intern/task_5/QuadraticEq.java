747ee6cd7692c9b35abcc2595d6cf845ee0e7f42
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-22 22:06:53 +0300

    Lesson 15 Level 2 Intern Task 7

--- src/main/java/student_valerija_ionova/lesson_15/level_2_intern/task_5/QuadraticEq.java
+++ src/main/java/student_valerija_ionova/lesson_15/level_2_intern/task_5/QuadraticEq.java
@@ -2,34 +2,34 @@ package student_valerija_ionova.lesson_15.level_2_intern.task_5;
 
 class QuadraticEq {
 
-    public void calc (double a, double b, double c) {
+    public String calc (double a, double b, double c) {
         double discriminant = b * b - 4 * a * c;
         if (discriminant > 0) {
-            calculateRootsIfDiscriminantPositive (a, b, discriminant);
+            return calculateRootsIfDiscriminantPositive (a, b, discriminant);
         }
         else if (discriminant == 0) {
-            calculateRootsIfDeterminantIsNull(a, b);
+            return calculateRootsIfDeterminantIsNull(a, b);
         }
         else {
-            calculateRootsIfDiscriminantNegative();
+            return calculateRootsIfDiscriminantNegative();
         }
     }
 
-    void calculateRootsIfDiscriminantPositive (double a, double b, double discriminant){
+    String calculateRootsIfDiscriminantPositive (double a, double b, double discriminant){
         double x1, x2;
         x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
         x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
-        System.out.println("x1 = " + x1 + ", x2 = " + x2);
+        return "x1 = " + x1 + ", x2 = " + x2;
     }
 
-    void calculateRootsIfDeterminantIsNull(double a, double b){
+    String calculateRootsIfDeterminantIsNull(double a, double b){
         double x;
         x = -b / (2 * a);
-        System.out.println("x = " + x);
+        return "x = " + x;
     }
 
-    void calculateRootsIfDiscriminantNegative(){
-        System.out.println("Equation has no roots");
+    String calculateRootsIfDiscriminantNegative(){
+        return "Equation has no roots";
     }
 
 }
