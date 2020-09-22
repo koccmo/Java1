0fc008f58b1c7dfeb762985e07487db17579e136
Igors <igors.barkevics@gmail.com>
2020-09-22 09:12:43 +0300

    Lesson 14 Level 3 Task_19

--- src/main/java/student_igors_bartkevics/lesson_14/homework/level_3_junior/TransactionTestData.java
+++ src/main/java/student_igors_bartkevics/lesson_14/homework/level_3_junior/TransactionTestData.java
@@ -11,7 +11,7 @@ class TransactionTestData {
         Trader alan = new Trader("Alan","Cambridge");
         Trader brian = new Trader("Brian","Cambridge");
 
-        List<Transaction> transactions = Arrays.asList(
+        return Arrays.asList(
                 new Transaction(brian, 2011, 300),
                 new Transaction(raoul, 2012, 1000),
                 new Transaction(raoul, 2011, 400),
@@ -19,6 +19,5 @@ class TransactionTestData {
                 new Transaction(mario, 2012, 700),
                 new Transaction(alan, 2012, 950)
         );
-        return transactions;
     }
 }
