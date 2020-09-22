d3f004cd82d67ca88fc5c883200710bd4f52da1d
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-22 21:56:06 +0300

    Lesson 15 Level 2 Intern Task 5 corrected

--- src/main/java/student_valerija_ionova/lesson_15/level_2_intern/task_5/QuadraticEq.java
+++ src/main/java/student_valerija_ionova/lesson_15/level_2_intern/task_5/QuadraticEq.java
@@ -5,24 +5,24 @@ class QuadraticEq {
     public void calc (double a, double b, double c) {
         double discriminant = b * b - 4 * a * c;
         if (discriminant > 0) {
-            calculateRootsIfDiscriminantPositive (a, b, c, discriminant);
+            calculateRootsIfDiscriminantPositive (a, b, discriminant);
         }
         else if (discriminant == 0) {
-            calculateRootsIfDeterminantIsNull(a, b, c, discriminant);
+            calculateRootsIfDeterminantIsNull(a, b);
         }
         else {
             calculateRootsIfDiscriminantNegative();
         }
     }
 
-    void calculateRootsIfDiscriminantPositive (double a, double b, double c, double discriminant){
+    void calculateRootsIfDiscriminantPositive (double a, double b, double discriminant){
         double x1, x2;
         x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
         x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
         System.out.println("x1 = " + x1 + ", x2 = " + x2);
     }
 
-    void calculateRootsIfDeterminantIsNull(double a, double b, double c, double discriminant){
+    void calculateRootsIfDeterminantIsNull(double a, double b){
         double x;
         x = -b / (2 * a);
         System.out.println("x = " + x);
