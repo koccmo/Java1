824ee762edf095c57f2365c2acc6a658a41150be
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-22 22:10:22 +0300

    Lesson 15 Level 2 Intern Task 8 Test

--- src/main/java/student_valerija_ionova/lesson_15/level_2_intern/task_5/QuadraticEq.java
+++ src/main/java/student_valerija_ionova/lesson_15/level_2_intern/task_5/QuadraticEq.java
@@ -2,7 +2,7 @@ package student_valerija_ionova.lesson_15.level_2_intern.task_5;
 
 class QuadraticEq {
 
-    public String calc (double a, double b, double c) {
+    String calc (double a, double b, double c) {
         double discriminant = b * b - 4 * a * c;
         if (discriminant > 0) {
             return calculateRootsIfDiscriminantPositive (a, b, discriminant);
@@ -15,20 +15,20 @@ class QuadraticEq {
         }
     }
 
-    String calculateRootsIfDiscriminantPositive (double a, double b, double discriminant){
+    private String calculateRootsIfDiscriminantPositive (double a, double b, double discriminant){
         double x1, x2;
         x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
         x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
         return "x1 = " + x1 + ", x2 = " + x2;
     }
 
-    String calculateRootsIfDeterminantIsNull(double a, double b){
+    private String calculateRootsIfDeterminantIsNull(double a, double b){
         double x;
         x = -b / (2 * a);
         return "x = " + x;
     }
 
-    String calculateRootsIfDiscriminantNegative(){
+    private String calculateRootsIfDiscriminantNegative(){
         return "Equation has no roots";
     }
 
