cafe883b0f93be460731df0b40f22a66ea113722
Jaroslav <brutan.jaroslav@gmail.com>
2020-09-24 00:11:36 +0300

    Lesson 11 day 6

--- src/main/java/student_jaroslav_brutan/lesson_11/day_6/all_tasks/BookDatabase.java
+++ src/main/java/student_jaroslav_brutan/lesson_11/day_6/all_tasks/BookDatabase.java
@@ -16,7 +16,7 @@ public interface BookDatabase {
 
     boolean delete(Book book);
 
-    Optional<Book> findById(Long bookId);
+    Optional<student_jaroslav_brutan.lesson_11.day_6.all_tasks.Book> findById(Long bookId);
 
     List<Book> findByAuthor(String author);
 
