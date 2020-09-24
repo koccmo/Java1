b834917f4b0435ca0552cbf6110eb811f52165d1
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-24 12:39:34 +0300

    Lesson 15 Level 5 Middle Task 16 interface AgedBriItem

---
+++ src/main/java/student_valerija_ionova/lesson_15/level_5_middle/gildedrose/AgedBrieItem.java
@@ -0,0 +1,18 @@
+package student_valerija_ionova.lesson_15.level_5_middle.gildedrose;
+
+class AgedBrieItem implements  ProductType{
+
+    @Override
+    public Item itemParametersNextDay(Item item) {
+        Item updatedItem = item;
+
+        if (item.getSellIn() > 0){
+            updatedItem.setQuality(Math.min(50, item.getQuality() + 1));
+        }else{
+            updatedItem.setQuality(Math.min(50, item.getQuality() + 2));
+        }
+        updatedItem.setSellIn(item.getSellIn()-1);
+
+        return updatedItem;
+    }
+}
