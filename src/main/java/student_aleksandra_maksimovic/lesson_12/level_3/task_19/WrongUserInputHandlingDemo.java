dbb9ba1b7660d93c7dc8e41d294818fecf266ad6
AlexaMaksimovic <lexaferret@inbox.lv>
2020-09-24 15:15:52 +0300

    lesson_11 level2 task 19

---
+++ src/main/java/student_aleksandra_maksimovic/lesson_12/level_3/task_19/WrongUserInputHandlingDemo.java
@@ -0,0 +1,31 @@
+package student_aleksandra_maksimovic.lesson_12.level_3.task_19;
+
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/*
+Создайте класс WrongUserInputHandlingDemo
+и в main() методе этого класса напишите код, который
+- запрашивает у пользователя целое число
+- если пользователь введёт не число а строку программа должна сообщить
+  пользователю о допущенной ошибке и запросить ввести целое число еще раз.
+ */
+public class WrongUserInputHandlingDemo {
+    public static void main(String[] args) {
+
+        while(true) {
+            try {
+                Scanner scan = new Scanner(System.in);
+                System.out.println("Please, enter integer!");
+                scan.nextInt();
+                break;
+            }catch (InputMismatchException exception){
+                System.out.println("wrong type! Please ,enter INTEGER!!!");
+            }
+
+        }
+
+    }
+
+}
