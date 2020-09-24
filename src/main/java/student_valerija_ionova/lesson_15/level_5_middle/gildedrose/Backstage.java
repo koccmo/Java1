029b01dd39db6effce3c838482bd7f6da4b6fd01
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-24 12:39:16 +0300

    Lesson 15 Level 5 Middle Task 16 interface Backstage

---
+++ src/main/java/student_valerija_ionova/lesson_15/level_5_middle/gildedrose/Backstage.java
@@ -0,0 +1,22 @@
+package student_valerija_ionova.lesson_15.level_5_middle.gildedrose;
+
+class Backstage implements ProductType{
+
+    @Override
+    public Item itemParametersNextDay(Item item) {
+        Item updatedItem = item;
+
+        if (item.getSellIn() <= 0){
+            updatedItem.setQuality(0);
+        }else if (item.getSellIn() <= 5){
+            updatedItem.setQuality(Math.min(50, item.getQuality() + 3));
+        }else if (item.getSellIn() <= 10){
+            updatedItem.setQuality(Math.min(50, item.getQuality() + 2));
+        }else {
+            updatedItem.setQuality(Math.min(50, item.getQuality() + 1));
+        }
+        updatedItem.setSellIn(item.getSellIn()-1);
+        return updatedItem;
+    }
+
+}
