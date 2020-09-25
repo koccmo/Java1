a9514f4e3c4b43a12a833159d4720156846cd702
Jaroslav <brutan.jaroslav@gmail.com>
2020-09-26 00:12:01 +0300

    Lesson 12 task 8

---
+++ src/main/java/student_jaroslav_brutan/lesson_12/day_2/UserCredentials.java
@@ -0,0 +1,16 @@
+package student_jaroslav_brutan.lesson_12.day_2;
+
+import java.util.List;
+
+public class UserCredentials {
+
+    private List<Role> roles;
+
+    public UserCredentials(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    boolean myRole(Role role){
+        return roles.contains(role);
+    }
+}
