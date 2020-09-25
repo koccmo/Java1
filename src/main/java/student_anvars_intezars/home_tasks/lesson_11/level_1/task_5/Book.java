685f267a635cbbd75cc67f498f7b246c724584a8
Anvar331 <anvar.intezar@gmail.com>
2020-09-25 22:41:47 +0300

    Lesson11/level1/task5

---
+++ src/main/java/student_anvars_intezars/home_tasks/lesson_11/level_1/task_5/Book.java
@@ -0,0 +1,21 @@
+package student_anvars_intezars.home_tasks.lesson_11.level_1.task_5;
+
+class Book {
+
+    private String title;
+    private String author;
+
+    Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+}
