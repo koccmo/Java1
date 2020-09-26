84f0f59e951ef7f06b3beee0e10a59284c8988a1
Roberts@Rob <39665997+robku@users.noreply.github.com>
2020-09-26 11:25:25 -0700

    Lesson 12 level 2 task 7

---
+++ src/main/java/student_roberts_kupcs/lesson_12/homework/level_2_intern/Role.java
@@ -0,0 +1,7 @@
+package student_roberts_kupcs.lesson_12.homework.level_2_intern;
+
+enum Role {
+    CAN_SEARCH_CLIENTS  // эта роль должна быть у клиента, для того
+                        // что бы он имел возможность делать поиск клиентов
+                        // в базе данных банка
+}
