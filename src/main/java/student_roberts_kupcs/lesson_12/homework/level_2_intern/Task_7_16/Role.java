7a55cb9c4b1978c1bb07fd7e9186bcae600f1a69
Roberts@Rob <39665997+robku@users.noreply.github.com>
2020-09-26 11:43:37 -0700

    Lesson 12 level 2 task 8

--- src/main/java/student_roberts_kupcs/lesson_12/homework/level_2_intern/Task_7_16/Role.java
+++ src/main/java/student_roberts_kupcs/lesson_12/homework/level_2_intern/Task_7_16/Role.java
@@ -1,4 +1,4 @@
-package student_roberts_kupcs.lesson_12.homework.level_2_intern;
+package student_roberts_kupcs.lesson_12.homework.level_2_intern.Task_7_16;
 
 enum Role {
     CAN_SEARCH_CLIENTS  // эта роль должна быть у клиента, для того
---
+++ src/main/java/student_roberts_kupcs/lesson_12/homework/level_2_intern/Task_7_16/UserCredentials.java
@@ -0,0 +1,13 @@
+package student_roberts_kupcs.lesson_12.homework.level_2_intern.Task_7_16;
+
+import java.util.List;
+
+class UserCredentials {
+    private List<Role> roles;
+
+    // созжайте конструктор класса, в котором вы получаете список ролей
+
+    public UserCredentials(List<Role> roles) {
+        this.roles = roles;
+    }
+}
