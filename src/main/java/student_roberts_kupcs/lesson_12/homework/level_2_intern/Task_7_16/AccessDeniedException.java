b1de933e1e26a056f2fb3d46e3d58d3b09849072
Roberts@Rob <39665997+robku@users.noreply.github.com>
2020-09-26 12:49:11 -0700

    Lesson 12 level 2 task 11

---
+++ src/main/java/student_roberts_kupcs/lesson_12/homework/level_2_intern/Task_7_16/AccessDeniedException.java
@@ -0,0 +1,4 @@
+package student_roberts_kupcs.lesson_12.homework.level_2_intern.Task_7_16;
+
+class AccessDeniedException extends Exception{
+}
--- src/main/java/student_roberts_kupcs/lesson_12/homework/level_2_intern/Task_7_16/BankApi.java
+++ src/main/java/student_roberts_kupcs/lesson_12/homework/level_2_intern/Task_7_16/BankApi.java
@@ -1,6 +1,5 @@
 package student_roberts_kupcs.lesson_12.homework.level_2_intern.Task_7_16;
 
-import java.nio.file.AccessDeniedException;
 import java.util.Optional;
 
 interface BankApi {
