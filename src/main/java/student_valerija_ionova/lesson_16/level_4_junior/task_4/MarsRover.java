4faa871085201568c72b697bb95062f9d36f202a
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-28 18:58:52 +0300

    Lesson 16 Level 4 Junior Task 4 MarsRover Refactoring 1

--- src/main/java/student_valerija_ionova/lesson_16/level_4_junior/task_4/MarsRover.java
+++ src/main/java/student_valerija_ionova/lesson_16/level_4_junior/task_4/MarsRover.java
@@ -6,37 +6,45 @@ public class MarsRover {
         if (!instructions.isEmpty()) {
             char instruction = instructions.charAt(0);
             if (instruction == 'L') {
-                if (direction == 'N') {
-                    return move(x, y, 'W', instructions.substring(1));
-                } else if (direction == 'W') {
-                    return move(x, y, 'S', instructions.substring(1));
-                } else if (direction == 'S') {
-                    return move(x, y, 'E', instructions.substring(1));
-                } else if (direction == 'E') {
-                    return move(x, y, 'N', instructions.substring(1));
-                }
+                return turnLeft(instructions, direction, x, y);
             } else if (instruction == 'R') {
-                if (direction == 'N') {
-                    return move(x, y, 'E', instructions.substring(1));
-                } else if (direction == 'W') {
-                    return move(x, y, 'N', instructions.substring(1));
-                } else if (direction == 'S') {
-                    return move(x, y, 'W', instructions.substring(1));
-                } else if (direction == 'E') {
-                    return move(x, y, 'S', instructions.substring(1));
-                }
+                return turnRight(instructions, direction, x, y);
             } else if (instruction == 'M') {
                 if (direction == 'N') {
-                    return move(x, y + 1, 'N', instructions.substring(1));
+                    return move(x, y + 1, 'N', instructions.substring(1, instructions.length()));
                 } else if (direction == 'S') {
-                    return move(x, y - 1, 'S', instructions.substring(1));
+                    return move(x, y - 1, 'S', instructions.substring(1, instructions.length()));
                 } else if (direction == 'W') {
-                    return move(x - 1, y, 'W', instructions.substring(1));
+                    return move(x - 1, y, 'W', instructions.substring(1, instructions.length()));
                 } else if (direction == 'E') {
-                    return move(x + 1, y, 'E', instructions.substring(1));
+                    return move(x + 1, y, 'E', instructions.substring(1, instructions.length()));
                 }
             }
         }
         return x + " " + y + " " + direction;
     }
+
+    private static String turnLeft (String instructions, char direction, int x, int y){
+        if (direction == 'N') {
+            return move(x, y, 'W', instructions.substring(1, instructions.length()));
+        } else if (direction == 'W') {
+            return move(x, y, 'S', instructions.substring(1, instructions.length()));
+        } else if (direction == 'S') {
+            return move(x, y, 'E', instructions.substring(1, instructions.length()));
+        } else
+            return move(x, y, 'N', instructions.substring(1, instructions.length()));
+    }
+
+    private static String turnRight (String instructions, char direction, int x, int y){
+        if (direction == 'N') {
+            return move(x, y, 'E', instructions.substring(1, instructions.length()));
+        } else if (direction == 'W') {
+            return move(x, y, 'N', instructions.substring(1, instructions.length()));
+        } else if (direction == 'S') {
+            return move(x, y, 'W', instructions.substring(1, instructions.length()));
+        } else
+            return move(x, y, 'S', instructions.substring(1, instructions.length()));
+    }
+
+
 }
