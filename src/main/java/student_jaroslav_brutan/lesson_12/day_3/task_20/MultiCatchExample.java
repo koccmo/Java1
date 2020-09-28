a813b49849c6dd817fda7e6903664ccbf92f1171
Jaroslav <brutan.jaroslav@gmail.com>
2020-09-28 23:16:33 +0300

    Lesson 12 day 3 task 20

---
+++ src/main/java/student_jaroslav_brutan/lesson_12/day_3/task_20/MultiCatchExample.java
@@ -0,0 +1,15 @@
+package student_jaroslav_brutan.lesson_12.day_3.task_20;
+
+public class MultiCatchExample {
+
+    public static void main(String[] args) {
+
+        try {
+
+        } catch (ArithmeticException a) {
+
+        } catch (Exception e) {
+
+        } // тк ArithmeticException является наследником Exception, его надо ловить в начале.
+    }
+}
