ab9e729e5bd22eed8cb7ce8b380186da66d2a4f5
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-09-28 16:22:43 +0300

    AA

--- src/main/java/student_valerija_ionova/lesson_11/level_5/task_28_29/AndSearchCriteria.java
+++ src/main/java/student_valerija_ionova/lesson_11/level_5/task_28_29/AndSearchCriteria.java
@@ -14,11 +14,7 @@ class AndSearchCriteria implements SearchCriteria {
     }
 
     public boolean match(Book book) {
-        if (leftCondition.match(book) && rightCondition.match(book)){
-            return true;
-        }else{
-            return false;
-        }
+        return leftCondition.match(book) && rightCondition.match(book);
     }
 
 }
