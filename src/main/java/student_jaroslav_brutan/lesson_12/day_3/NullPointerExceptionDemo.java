e3a27f0a8d6324814b9f6061dc8b594f80d9b243
Jaroslav <brutan.jaroslav@gmail.com>
2020-09-28 22:57:31 +0300

    Lesson 12 day 3 task 18

---
+++ src/main/java/student_jaroslav_brutan/lesson_12/day_3/NullPointerExceptionDemo.java
@@ -0,0 +1,10 @@
+package student_jaroslav_brutan.lesson_12.day_3;
+
+public class NullPointerExceptionDemo {
+
+    public static void main(String[] args) {
+
+        int firstNumber = Integer.parseInt(null);
+        System.out.println(firstNumber);
+    }
+}
