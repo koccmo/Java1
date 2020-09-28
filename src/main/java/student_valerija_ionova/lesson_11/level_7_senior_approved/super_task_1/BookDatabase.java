003cd03e0d35177149468a1dc8d83e89f19a2de3
vsavonins <viktors.savonins@proofit.lv>
2020-09-28 21:39:59 +0300

    code review

---
+++ src/main/java/student_valerija_ionova/lesson_11/level_7_senior_approved/super_task_1/BookDatabase.java
@@ -0,0 +1,43 @@
+package student_valerija_ionova.lesson_11.level_7_senior_approved.super_task_1;
+
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public interface BookDatabase {
+
+    Long save(Book book);
+
+    boolean delete(Long bookId);
+
+    boolean delete(Book book);
+
+    Optional<Book> findById(Long bookId);
+
+    List<Book> findByAuthor(String author);
+
+    List<Book> findByTitle(String title);
+
+    int countAllBooks();
+
+    void deleteByAuthor(String author);
+
+    void deleteByTitle(String title);
+
+    List<Book> find(List <SearchCriteria>searchCriteria, Paging paging);
+
+    Set<String> findUniqueAuthors();
+
+    Set<String> findUniqueTitles();
+
+    Set<Book> findUniqueBooks();
+
+    boolean contains(Book book);
+
+    Map<String, List<Book>> getAuthorToBooksMap();
+
+    Map<String, Integer> getEachAuthorBookCount();
+
+}
---
+++ src/main/java/student_valerija_ionova/lesson_11/level_7_senior_approved/super_task_1/Paging.java
@@ -0,0 +1,67 @@
+package student_valerija_ionova.lesson_11.level_7_senior_approved.super_task_1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Paging {
+
+    private int booksOnOnePage;
+    private int pageNumber;
+    private boolean pagingIndicated;
+
+    public boolean getPagingIndicated(){
+        return pagingIndicated;
+    }
+
+    List<Book> getCorrectPage(List <Book> books){
+        List <Book> booksOnCorrectPage = new ArrayList<>();
+
+        if (Math.ceil(books.size() / (double)booksOnOnePage) < pageNumber){
+            return booksOnCorrectPage;
+        }
+
+        if (Math.floor((books.size() / (double)booksOnOnePage)) < pageNumber){
+            booksOnCorrectPage = books;
+            for (int i = (books.size()-2); i >=0; i--){
+                booksOnCorrectPage.remove(i);
+            }
+            return booksOnCorrectPage;
+        }
+
+        booksOnCorrectPage = books.subList((pageNumber-1) * booksOnOnePage,
+                ((pageNumber-1) * booksOnOnePage) + booksOnOnePage);
+            return booksOnCorrectPage;
+    }
+
+    public void createPagingCriteria (){
+        System.out.println("Please enter number of books on one page");
+        this.booksOnOnePage = enterNumber();
+
+        System.out.println("Please enter pages number to show");
+        this.pageNumber = enterNumber();
+        pagingIndicated = true;
+    }
+
+    private int enterNumber() {
+        Scanner sc = new Scanner(System.in);
+        int userSelectedNumber;
+        do{
+            while (true) {
+                try {
+                    userSelectedNumber = Integer.parseInt(sc.nextLine());
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("It's not number!");
+                    System.out.println("Please enter number:\n");
+                }
+            }
+            if (userSelectedNumber <0){
+                System.out.println("It's not valid number");
+                System.out.println("Please enter valid number:\n");
+            }
+        }while(userSelectedNumber <0);
+        return userSelectedNumber;
+    }
+
+}
