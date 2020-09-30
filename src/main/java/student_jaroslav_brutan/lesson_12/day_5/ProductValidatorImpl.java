644fc30363ba4270fde0a4a3045e8342c0d98c9d
Jaroslav <brutan.jaroslav@gmail.com>
2020-09-30 22:47:45 +0300

    Lesson 12 day 5

---
+++ src/main/java/student_jaroslav_brutan/lesson_12/day_5/ProductValidatorImpl.java
@@ -0,0 +1,11 @@
+package student_jaroslav_brutan.lesson_12.day_5;
+
+import java.util.List;
+
+public class ProductValidatorImpl implements ProductValidator{
+
+    @Override
+    public List<ValidationException> validate(Product product){
+        return null;
+    }
+}
