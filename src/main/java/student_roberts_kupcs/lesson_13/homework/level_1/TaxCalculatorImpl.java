bd0aa7fc607572f28d5b339a1389dd580a7e90e1
Roberts@Rob <39665997+robku@users.noreply.github.com>
2020-09-30 23:41:33 -0700

    Lesson 13 level 1 task 2

---
+++ src/main/java/student_roberts_kupcs/lesson_13/homework/level_1/TaxCalculatorImpl.java
@@ -0,0 +1,11 @@
+package student_roberts_kupcs.lesson_13.homework.level_1;
+
+import java.math.BigDecimal;
+
+class TaxCalculatorImpl implements TaxCalculatetor{
+
+    @Override
+    public BigDecimal calculateTax(BigDecimal income) {
+        return null;
+    }
+}
