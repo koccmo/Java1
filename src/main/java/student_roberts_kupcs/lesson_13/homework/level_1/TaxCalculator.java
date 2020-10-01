8282720d828557c747b5583abc8d9d10bfbad88a
Roberts@Rob <39665997+robku@users.noreply.github.com>
2020-10-01 03:33:17 -0700

    Lesson 13 level 1 task 3 4 5 6 7 8

--- src/main/java/student_roberts_kupcs/lesson_13/homework/level_1/TaxCalculator.java
+++ src/main/java/student_roberts_kupcs/lesson_13/homework/level_1/TaxCalculator.java
@@ -2,7 +2,7 @@ package student_roberts_kupcs.lesson_13.homework.level_1;
 
 import java.math.BigDecimal;
 
-interface TaxCalculatetor {
+interface TaxCalculator {
 
     BigDecimal calculateTax(BigDecimal income);
 
