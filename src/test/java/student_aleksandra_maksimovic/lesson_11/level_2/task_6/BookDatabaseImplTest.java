12ea3015926cb77144e133a41760ecedfb1d47df
AlexaMaksimovic <lexaferret@inbox.lv>
2020-10-03 11:05:54 +0300

    lesson_11 tests

--- src/test/java/student_aleksandra_maksimovic/lesson_11/level_2/task_6/BookDatabaseImplTest.java
+++ src/test/java/student_aleksandra_maksimovic/lesson_11/level_2/task_6/BookDatabaseImplTest.java
@@ -18,6 +18,9 @@ public class BookDatabaseImplTest {
     Book book1 = new Book("Author1", "Title1");
     Book book2 = new Book("Author2", "Title2");
     Book book3 = new Book("Author3", "Title3");
+    Book book4 = new Book("Author3", "Title4");
+    Book book5 = new Book("Author4", "Title4");
+
 
  private BookDatabaseImpl bookDatabase;
 
@@ -34,7 +37,7 @@ public class BookDatabaseImplTest {
         Long id = bookDatabase.save(new Book("Author1", "Title1"));
 
         boolean condition = id == 2;
-        assertEquals(condition, true);
+        assertTrue(condition);
     }
 
    @Test
@@ -44,7 +47,7 @@ public class BookDatabaseImplTest {
        bookDatabase.save(book2);
        bookDatabase.save(book3);
        boolean condition = bookDatabase.delete(2L);
-       assertEquals(condition,true);
+       assertTrue(condition);
     }
   @Test
     public void deleteNegativeTest() {
@@ -85,8 +88,9 @@ public class BookDatabaseImplTest {
         bookDatabase.save(book1);
         bookDatabase.save(book2);
         bookDatabase.save(book3);
-        Book findBook = bookDatabase.findById(1L).get();
-        boolean condition = findBook.equals(book1);
+        Optional<Book> findBook = bookDatabase.findById(1L);
+
+        boolean condition = findBook.isPresent() && findBook.get().equals(book1);
         assertTrue(condition);
     }
 
@@ -106,42 +110,76 @@ public class BookDatabaseImplTest {
         bookDatabase.save(book1);
         bookDatabase.save(book2);
         bookDatabase.save(book3);
+        bookDatabase.save(book4);
 
         List<Book> booksByAuthor = new ArrayList<>();
-        booksByAuthor.add(book2);
-        booksByAuthor.add(book1);
         booksByAuthor.add(book3);
+        booksByAuthor.add(book4);
 
 
-        boolean condition = bookDatabase.findByAuthor("Author2").equals(booksByAuthor);
-        assertFalse(condition);//TODO
+        boolean condition = bookDatabase.findByAuthor("Author3").equals(booksByAuthor);
+        assertTrue(condition);
     }
 
     @Test
     public void findByTitle() {
-        //TODO
+        bookDatabase.save(book1);
+        bookDatabase.save(book2);
+        bookDatabase.save(book3);
+        bookDatabase.save(book4);
+        bookDatabase.save(book5);
+
+        List<Book> booksByTitle = new ArrayList<>();
+        booksByTitle.add(book4);
+        booksByTitle.add(book5);
+
+        boolean condition = bookDatabase.findByTitle("Title4").equals(booksByTitle);
+        assertTrue(condition);
     }
 
 
     @Test
     public void countAllBooksTest() {
-
         bookDatabase.save(book1);
         bookDatabase.save(book2);
         bookDatabase.save(book3);
-        boolean condition = bookDatabase.countAllBooks() == 3;
-        assertEquals(condition, true);
+
+        assertEquals(bookDatabase.countAllBooks(), 3);
     }
 
     @Test
     public void deleteBooksByAuthorTest() {
-        //TODO
+        bookDatabase.save(book1);
+        bookDatabase.save(book2);
+        bookDatabase.save(book3);
+        bookDatabase.save(book4);
+        bookDatabase.save(book5);
+
+        bookDatabase.deleteByAuthor("Author3");
+
+        assertEquals(bookDatabase.countAllBooks(), 3);
+
+        assertNotNull(bookDatabase.findById(book1.getId()));
+        assertNotNull(bookDatabase.findById(book2.getId()));
+        assertNotNull(bookDatabase.findById(book5.getId()));
 
     }
 
     @Test
     public void deleteBooksByTitleTest() {
-        //TODO
+        bookDatabase.save(book1);
+        bookDatabase.save(book2);
+        bookDatabase.save(book3);
+        bookDatabase.save(book4);
+        bookDatabase.save(book5);
+
+        bookDatabase.deleteByTitle("Title4");
+
+        assertEquals(bookDatabase.countAllBooks(), 3);
+
+        assertNotNull(bookDatabase.findById(book1.getId()));
+        assertNotNull(bookDatabase.findById(book2.getId()));
+        assertNotNull(bookDatabase.findById(book3.getId()));
     }
 
 }
