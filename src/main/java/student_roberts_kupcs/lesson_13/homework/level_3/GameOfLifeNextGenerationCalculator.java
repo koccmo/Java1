cffdb0c2add31bc00bb185e54892c9304b1c532b
Roberts@Rob <39665997+robku@users.noreply.github.com>
2020-10-04 04:19:43 -0700

    Lesson 13 level 3 task 10

---
+++ src/main/java/student_roberts_kupcs/lesson_13/homework/level_3/GameOfLifeNextGenerationCalculator.java
@@ -0,0 +1,7 @@
+package student_roberts_kupcs.lesson_13.homework.level_3;
+
+class GameOfLifeNextGenerationCalculator {
+    public boolean[][] calculate(boolean[][] currentGeneration) {
+        return null; // реализацию напишем тут чуть позже
+    }
+}
