1865fb1a7c02aeb2f6af065e2280578aa672ded4
AlexaMaksimovic <lexaferret@inbox.lv>
2020-10-04 13:15:13 +0300

    lesson_12 level_5

---
+++ src/main/java/student_aleksandra_maksimovic/lesson_12/level_5_6/ValidationException.java
@@ -0,0 +1,57 @@
+package student_aleksandra_maksimovic.lesson_12.level_5_6;
+
+class ValidationException extends Exception {
+
+    // название валидационного правила, которое создало эту ошибку
+    private String ruleName;
+
+    // описание ошибки, которое можно показать на UI
+    private String description;
+
+    // название поля при проверке которого произошла эта ошибка
+    // (Product: title, price, description),
+    // UI сможет по этому названию показать ошибку рядом с нужным полем
+    private String fieldName;
+
+
+    public ValidationException(String ruleName, String description, String fieldName)
+    {
+        this.ruleName = ruleName;
+        this.description = description;
+        this.fieldName = fieldName;
+    }
+
+
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+
+
+
+
+    // создайте конструктор
+
+    // создайте только get
+
+}
