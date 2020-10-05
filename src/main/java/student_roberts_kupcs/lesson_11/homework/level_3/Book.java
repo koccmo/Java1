709466ec759cb67636c95d0e764f3abfed4a8311
Roberts@Rob <39665997+robku@users.noreply.github.com>
2020-10-05 05:44:45 -0700

    Lesson 11 level 3 task 16

--- src/main/java/student_roberts_kupcs/lesson_11/homework/level_3/Book.java
+++ src/main/java/student_roberts_kupcs/lesson_11/homework/level_3/Book.java
@@ -26,4 +26,14 @@ class Book {
         return this.author;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (obj == null || getClass() !=obj.getClass()) {
+            return false;
+        }
+        Book book = (Book) obj;
+        return title.equals(book.title) && author.equals(book.author);
+    }
 }
---
+++ src/test/java/student_roberts_kupcs/lesson_11/homework/level_3/AuthorSearchCriteriaTest.java
@@ -0,0 +1,13 @@
+package student_roberts_kupcs.lesson_11.homework.level_3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AuthorSearchCriteriaTest {
+    //AuthorSearchCriteria authorSearchCriteria = new AuthorSearchCriteria();
+
+    //@Test
+    //public void
+
+}
