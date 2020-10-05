9a995ba040487c26ee77ad19b90c64242297c928
Anvar331 <anvar.intezar@gmail.com>
2020-10-05 13:45:24 +0300

    Lesson12/level1/task4

---
+++ src/main/java/student_anvars_intezars/home_tasks/lesson_12/task_4/MyException.java
@@ -0,0 +1,11 @@
+package student_anvars_intezars.home_tasks.lesson_12.task_4;
+
+class MyException extends Exception {
+
+    private String message;
+
+    public MyException(String message) {
+        this.message = message;
+    }
+
+}
