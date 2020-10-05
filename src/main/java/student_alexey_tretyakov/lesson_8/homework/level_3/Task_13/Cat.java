74cec393498674d6dd559899dc9de5fd77ed39c1
Alexey777555 <eiroartlv@inbox.lv>
2020-10-05 23:08:11 +0300

    Lesson 8, level 2,task 13

---
+++ src/main/java/student_alexey_tretyakov/lesson_8/homework/level_3/Task_13/Cat.java
@@ -0,0 +1,9 @@
+package student_alexey_tretyakov.lesson_8.homework.level_3.Task_13;
+
+abstract class Cat {
+    protected String name;
+    public Cat(String name) {
+        this.name = name;
+    }
+    abstract void voice();
+}
---
+++ src/main/java/student_alexey_tretyakov/lesson_8/homework/level_3/Task_13/CatMain.java
@@ -0,0 +1,12 @@
+package student_alexey_tretyakov.lesson_8.homework.level_3.Task_13;
+
+
+class CatMain {
+
+    public static void main(String[] args) {
+        Cat homeCat = new HomeCat("Archibald");
+        homeCat.voice();
+        Cat streetCat = new StreetCat("Vasjka");
+        streetCat.voice();
+    }
+}
