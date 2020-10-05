467edb501c84d4461bf3514af3c0cf3b0cbb663e
Anvar331 <anvar.intezar@gmail.com>
2020-10-05 21:43:04 +0300

    Lesson12/level1/task8

---
+++ src/main/java/student_anvars_intezars/home_tasks/lesson_12/level_2/UserCredentials.java
@@ -0,0 +1,13 @@
+package student_anvars_intezars.home_tasks.lesson_12.level_2;
+
+import java.util.List;
+
+class UserCredentials {
+
+    private List<Role> roles;
+
+
+    public UserCredentials(List<Role> roles) {
+        this.roles = roles;
+    }
+}
