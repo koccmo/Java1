dc7277c42a14c676a74c4bcac315733a563ec43c
Roberts@Rob <39665997+robku@users.noreply.github.com>
2020-10-06 02:42:24 -0700

    7Lesson 11 level 3 task 17

--- src/test/java/student_roberts_kupcs/lesson_11/homework/level_3/AuthorSearchCriteriaTest.java
+++ src/test/java/student_roberts_kupcs/lesson_11/homework/level_3/AuthorSearchCriteriaTest.java
@@ -2,12 +2,9 @@ package student_roberts_kupcs.lesson_11.homework.level_3;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class AuthorSearchCriteriaTest {
-    //AuthorSearchCriteria authorSearchCriteria = new AuthorSearchCriteria();
+
 
     //@Test
-    //public void
 
 }
