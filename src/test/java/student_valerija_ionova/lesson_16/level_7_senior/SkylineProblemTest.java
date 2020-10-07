94641abc4f0665164f29553dc1797ca23a126f60
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-10-07 14:15:07 +0300

    Lesson 16 Level Senior Skyline problem Interface SilhouetteOfSkylines Test

--- src/test/java/student_valerija_ionova/lesson_16/level_7_senior/SkylineProblemTest.java
+++ src/test/java/student_valerija_ionova/lesson_16/level_7_senior/SkylineProblemTest.java
@@ -12,7 +12,7 @@ public class SkylineProblemTest {
     @Test
     public void testOneBuilding(){
         int [][] coordinates = {{1, 5, 11}};
-        SkylineProblem skylineProblem = new SkylineProblem(coordinates);
+        SilhouetteOfSkylines skylineProblem = new SkylineProblem(coordinates);
         List<ResultCoordinates> expectedResult = new ArrayList<>();
         expectedResult.add(new ResultCoordinates(1, 11));
         expectedResult.add(new ResultCoordinates(5, 0));
@@ -22,7 +22,7 @@ public class SkylineProblemTest {
     @Test
     public void testOneBuilding2(){
         int [][] coordinates = {{3, 9, 9}};
-        SkylineProblem skylineProblem = new SkylineProblem(coordinates);
+        SilhouetteOfSkylines skylineProblem = new SkylineProblem(coordinates);
         List<ResultCoordinates> expectedResult = new ArrayList<>();
         expectedResult.add(new ResultCoordinates(3, 9));
         expectedResult.add(new ResultCoordinates(9, 0));
@@ -32,7 +32,7 @@ public class SkylineProblemTest {
     @Test
     public void testTwoBuildings(){
         int [][] coordinates = {{1, 7, 2}, {3, 5, 6}};
-        SkylineProblem skylineProblem = new SkylineProblem(coordinates);
+        SilhouetteOfSkylines skylineProblem = new SkylineProblem(coordinates);
         List<ResultCoordinates> expectedResult = new ArrayList<>();
         expectedResult.add(new ResultCoordinates(1, 2));
         expectedResult.add(new ResultCoordinates(3, 6));
@@ -44,7 +44,7 @@ public class SkylineProblemTest {
     @Test
     public void testTwoBuildingsTwoSilhouettes(){
         int [][] coordinates = {{1, 5, 11}, {6, 7, 11}};
-        SkylineProblem skylineProblem = new SkylineProblem(coordinates);
+        SilhouetteOfSkylines skylineProblem = new SkylineProblem(coordinates);
         List<ResultCoordinates> expectedResult = new ArrayList<>();
         expectedResult.add(new ResultCoordinates(1, 11));
         expectedResult.add(new ResultCoordinates(5, 0));
@@ -56,7 +56,7 @@ public class SkylineProblemTest {
     @Test
     public void testTwoBuildingsOneSilhouette(){
         int [][] coordinates = {{1, 5, 11}, {5, 7, 11}};
-        SkylineProblem skylineProblem = new SkylineProblem(coordinates);
+        SilhouetteOfSkylines skylineProblem = new SkylineProblem(coordinates);
         List<ResultCoordinates> expectedResult = new ArrayList<>();
         expectedResult.add(new ResultCoordinates(1, 11));
         expectedResult.add(new ResultCoordinates(7, 0));
@@ -66,7 +66,7 @@ public class SkylineProblemTest {
     @Test
     public void testFourBuildings(){
         int [][] coordinates = {{1, 3, 8}, {2, 4, 6}, {4, 6, 4}, {8, 9, 1}};
-        SkylineProblem skylineProblem = new SkylineProblem(coordinates);
+        SilhouetteOfSkylines skylineProblem = new SkylineProblem(coordinates);
         List<ResultCoordinates> expectedResult = new ArrayList<>();
         expectedResult.add(new ResultCoordinates(1, 8));
         expectedResult.add(new ResultCoordinates(3, 6));
@@ -80,7 +80,7 @@ public class SkylineProblemTest {
     @Test
     public void testOneBigBuildingAndLotsSmall(){
         int [][] coordinates = {{1, 20, 8}, {2, 4, 6}, {4, 6, 4}, {8, 9, 1}, {10, 7, 8}, {15, 16, 2}, {19, 20, 5}};
-        SkylineProblem skylineProblem = new SkylineProblem(coordinates);
+        SilhouetteOfSkylines skylineProblem = new SkylineProblem(coordinates);
         List<ResultCoordinates> expectedResult = new ArrayList<>();
         expectedResult.add(new ResultCoordinates(1, 8));
         expectedResult.add(new ResultCoordinates(20, 0));
@@ -91,7 +91,7 @@ public class SkylineProblemTest {
     @Test
     public void testManyBuildings1(){
         int [][] coordinates = {{2, 9, 10}, {3, 7, 15}, {5, 12, 12}, {15, 20, 10}, {19, 24, 8}};
-        SkylineProblem skylineProblem = new SkylineProblem(coordinates);
+        SilhouetteOfSkylines skylineProblem = new SkylineProblem(coordinates);
         List<ResultCoordinates> expectedResult = new ArrayList<>();
         expectedResult.add(new ResultCoordinates(2, 10));
         expectedResult.add(new ResultCoordinates(3, 15));
@@ -107,7 +107,7 @@ public class SkylineProblemTest {
     public void testManyBuildings2(){
         int [][] coordinates = {{1, 5, 11}, {2, 7, 6}, {3, 9, 13}, {12, 16, 7}, {14, 25, 3},
                 {19, 22, 18}, {23, 13, 29}, {24, 4, 28}};
-        SkylineProblem skylineProblem = new SkylineProblem(coordinates);
+        SilhouetteOfSkylines skylineProblem = new SkylineProblem(coordinates);
         List<ResultCoordinates> expectedResult = new ArrayList<>();
         expectedResult.add(new ResultCoordinates(1, 11));
         expectedResult.add(new ResultCoordinates(3, 13));
