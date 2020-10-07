a2d7a1e2c809fe1d9d67bde083060a836d4de661
ValerijaI <55547022+ValerijaI@users.noreply.github.com>
2020-10-07 12:02:28 +0300

    Lesson 16 Level Senior Skyline problem Input coordinates

---
+++ src/main/java/student_valerija_ionova/lesson_16/level_7_senior/InputCoordinates.java
@@ -0,0 +1,39 @@
+package student_valerija_ionova.lesson_16.level_7_senior;
+
+import java.util.Arrays;
+
+class InputCoordinates {
+
+    int [] coordinates = new int [3];
+
+    InputCoordinates (int xL, int xR, int H){
+        this.coordinates[0] = xL;
+        this.coordinates[1] = xR;
+        this.coordinates[2] = H;
+    }
+
+    int getXLeft (){
+        return coordinates[0];
+    }
+
+    int getXRight (){
+        return coordinates[1];
+    }
+
+    int getHeight (){
+        return coordinates[2];
+    }
+
+    void update (int xL, int xR, int H){
+        this.coordinates[0] = xL;
+        this.coordinates[1] = xR;
+        this.coordinates[2] = H;
+    }
+
+    @Override
+    public String toString() {
+        return "InputCoordinates{" +
+                "coordinates=" + Arrays.toString(coordinates) +
+                '}';
+    }
+}
