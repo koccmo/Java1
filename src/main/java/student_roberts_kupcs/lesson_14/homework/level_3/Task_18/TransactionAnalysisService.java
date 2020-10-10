39287047c6704aeb0847e7aab2c9f692db1d2bac
Roberts@Rob <39665997+robku@users.noreply.github.com>
2020-10-10 00:18:35 -0700

    Lesson 15 level 1 task 3

--- src/main/java/student_roberts_kupcs/lesson_14/homework/level_3/Task_18/TransactionAnalysisService.java
+++ src/main/java/student_roberts_kupcs/lesson_14/homework/level_3/Task_18/TransactionAnalysisService.java
@@ -1,4 +1,20 @@
 package student_roberts_kupcs.lesson_14.homework.level_3.Task_18;
 
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 class TransactionAnalysisService {
+
+    public List<Transaction> findTransactionsIn2011(List<Transaction> transactions) {
+        return transactions.stream()
+                .filter(t -> t.getYear() == 2011)
+                .collect(toList());
+    }
+
+    public List<Transaction> findTransactionsByYear(List<Transaction> transactions, int year) {
+        return transactions.stream()
+                .filter(t -> t.getYear() == year)
+                .collect(toList());
+    }
 }
