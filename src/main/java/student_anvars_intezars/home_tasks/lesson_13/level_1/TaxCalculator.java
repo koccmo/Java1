8f7b6f8aa43ac006f231ae01bd958333b31509be
Anvar331 <anvar.intezar@gmail.com>
2020-10-10 12:41:37 +0300

    Lesson13/level1/Task1,2

---
+++ src/main/java/student_anvars_intezars/home_tasks/lesson_13/level_1/TaxCalculator.java
@@ -0,0 +1,8 @@
+package student_anvars_intezars.home_tasks.lesson_13.level_1;
+
+import java.math.BigDecimal;
+
+interface TaxCalculator {
+
+        BigDecimal calculateTax(BigDecimal income);
+}
---
+++ src/main/java/student_anvars_intezars/home_tasks/lesson_13/level_1/TaxCalculatorImpl.java
@@ -0,0 +1,11 @@
+package student_anvars_intezars.home_tasks.lesson_13.level_1;
+
+import java.math.BigDecimal;
+
+class TaxCalculatorImpl implements TaxCalculator {
+
+    public BigDecimal calculateTax(BigDecimal income) {
+        return null;
+    }
+
+}
