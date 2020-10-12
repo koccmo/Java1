7dbaf2f7995e9e618b6d6ecb4470162a3c42b65f
binco <vladimirs.danilins@gmail.com>
2020-10-12 22:24:33 +0300

    Домашние задания 14 урока

---
+++ src/main/java/student_volodya_danilin/lesson_15/level_1/WordsService.java
@@ -0,0 +1,15 @@
+package student_volodya_danilin.lesson_15.level_1;
+
+class WordsService {
+
+    public int countRepetitions(String[] words, String repeatingWord) {
+        int repetitions = 0;
+        for (String word : words) {
+            if (word.equals(repeatingWord)) {
+                repetitions++;
+            }
+        }
+        return repetitions;
+    }
+
+}
