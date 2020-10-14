eae14502ffbe0a0e196c30e5e4740cb076c557e5
pavels-s <pavels.sarkels@gmail.com>
2020-10-14 17:56:01 +0300

    Lesson 11, level 1, task 2

---
+++ src/main/java/student_pavel_sharkel/lesson_11/task_2/User.java
@@ -0,0 +1,12 @@
+package student_pavel_sharkel.lesson_11.task_2;
+
+class User {
+
+    private String name;
+    private int age;
+
+    User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/main/java/student_pavel_sharkel/lesson_11/task_2/UserList.java
@@ -0,0 +1,14 @@
+package student_pavel_sharkel.lesson_11.task_2;
+
+import java.util.ArrayList;
+
+class UserList {
+
+    public static void main(String[] args) {
+
+        User firstUser = new User("John", 27);
+        ArrayList<User> userArrayList = new ArrayList<>();
+        userArrayList.add(firstUser);
+
+    }
+}
