1d18b1080e040c8f99ea221070c1022e3ad317bd
pavels-s <pavels.sarkels@gmail.com>
2020-10-15 11:55:26 +0300

    Lesson 11, level 1, task 5

---
+++ src/main/java/student_pavel_sharkel/lesson_11/level_1/task_5/Book.java
@@ -0,0 +1,20 @@
+package student_pavel_sharkel.lesson_11.level_1.task_5;
+
+class Book {
+    private String title;
+    private String author;
+
+    Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+}
