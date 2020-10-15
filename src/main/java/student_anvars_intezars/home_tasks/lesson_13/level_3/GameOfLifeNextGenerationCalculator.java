558d2def84c20a9aecbc9d0442eacb4172e972b5
Anvar331 <anvar.intezar@gmail.com>
2020-10-15 23:32:48 +0300

    Lesson13/level3/Task11,12(updated code)

--- src/main/java/student_anvars_intezars/home_tasks/lesson_13/level_3/GameOfLifeNextGenerationCalculator.java
+++ src/main/java/student_anvars_intezars/home_tasks/lesson_13/level_3/GameOfLifeNextGenerationCalculator.java
@@ -2,14 +2,32 @@ package student_anvars_intezars.home_tasks.lesson_13.level_3;
 
 class GameOfLifeNextGenerationCalculator {
 
-    public boolean[][] calculate(boolean[][] currentGeneration) {
-        for (boolean[] booleans : currentGeneration) {
-            if ((booleans[0]) && (booleans[1]) && (booleans[2])) {
+    boolean[][] field;
+    int rows = field.length;
+    int columns = field[0].length;
+    boolean[][] newField = new boolean[rows][columns];
 
-            }
-        }
+    public boolean[][] calculate(boolean[][] currentGeneration) {
         return null;
     }
 
+   /* public boolean isOneCellHavingTwoLiveNeighbours(boolean[][] array) {
+        boolean [][] copyOfArray = createArrayCopy(array);
+        for(int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array)
+            }
+        }
+        return false;
+    }*/
 
-}
+    public boolean[][] createArrayCopy(boolean[][] array) {
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < columns; col++) {
+                newField[row][col] = array[row][col];
+            }
+        }
+        return newField;
+    }
+
+  }
