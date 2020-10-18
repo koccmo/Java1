27bd2854b6fc989380a44347126a8f340daeb5dc
Roberts@Rob <39665997+robku@users.noreply.github.com>
2020-10-18 10:35:38 -0700

    Correct Lesson 12 level 5 task 33

---
+++ src/main/java/student_roberts_kupcs/lesson_12/homework/level_5/task_27_34/ProductValidator.java
@@ -0,0 +1,9 @@
+package student_roberts_kupcs.lesson_12.homework.level_5.task_27_34;
+
+import java.util.List;
+
+interface ProductValidator {
+
+    List<ValidationException> validate(Product product);
+
+}
